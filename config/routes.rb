084272202dfe688d1f030b4ca1002f5f6c4a9dbf
Theodor Vararu <theo@vararu.org>
2022-05-11 10:27:35 +0200

    Add GOV.UK styled error pages
    This changeset was written by running the template:
    
    https://github.com/DFE-Digital/rails-template/pull/6

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  scope via: :all do
+    get '/404', to: 'errors#not_found'
+    get '/422', to: 'errors#unprocessable_entity'
+    get '/500', to: 'errors#internal_server_error'
+  end
 end
