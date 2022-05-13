1512db3bdd3d00ff24d219a11244127aea22faf6
Felix Clack <felixclack@gmail.com>
2022-05-13 10:50:57 +0100

    Add shoulda-matchers
    This library adds some common matchers that make unit testing
    validations simpler.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,3 +64,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.before(:each, type: :system) { driven_by(:cuprite) }
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
