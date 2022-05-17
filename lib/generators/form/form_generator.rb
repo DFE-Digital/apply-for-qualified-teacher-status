a95143815542c2c8ab2d405d6209840f0bb1b73e
Felix Clack <felixclack@gmail.com>
2022-05-17 11:48:03 +0100

    Add a generator to automate a new question
    We use form objects to manage adding a new question, validate the user
    input and store on the eligibility check.
    
    This generator automates creating the files we need for that process.

---
+++ lib/generators/form/form_generator.rb
@@ -0,0 +1,14 @@
+class FormGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("templates", __dir__)
+
+  def copy_form_files
+    template "form.erb", "app/forms/#{file_name}_form.rb"
+    template "form_spec.erb", "spec/forms/#{file_name}_form_spec.rb"
+    template "form_controller.erb", "app/controllers/teacher_interface/#{plural_name}_controller.rb"
+    template "new_form.html.erb", "app/views/teacher_interface/#{plural_name}/new.html.erb"
+
+    insert_into_file "config/routes.rb", after: "namespace :teacher_interface, path: '/teacher' do\n" do
+      "    get '#{plural_name}', to: '#{plural_name}#new'\n    post '#{plural_name}', to: '#{plural_name}#create'\n"
+    end
+  end
+end
