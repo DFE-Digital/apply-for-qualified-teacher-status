69a9d178455aaf3a1bf97c1fb8b4b2dd80df1c81
Theodor Vararu <theo@vararu.org>
2022-05-18 17:51:34 +0200

    Configure cookies store
    Use same settings as Find.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+Rails.application.config.session_store :cookie_store,
+                                       key: "_session_id",
+                                       secure: Rails.env.production?,
+                                       expire_after: 2.hours
