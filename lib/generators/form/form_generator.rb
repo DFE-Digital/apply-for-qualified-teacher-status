01a3b96a509c3a4bac1faebd0fea1d749be085b9
Felix Clack <felixclack@gmail.com>
2022-05-18 12:07:55 +0100

    Extend the form generator to add attributes
    We want the form generator to write more of the boilerplate for us.
    
    In this change, I have co-opted the convention for defining attributes
    on a model, eg. attribute:type, as a way of defining attributes for the
    form object and a related one on the EligiblityCheck.
    
    Currently, this only supports boolean types. These are the easiest to
    output the HTML components for.
    
    Running `rails generate form FormName attribute:type` results in
    creating the relevant form object, adding the attribute to both it and
    the `EligibilityCheck`.
    
    Also, the component for the question HTML gets added.
    
    This will reduce the amount of custom code we need to write to add a new
    question to the flow.

--- lib/generators/form/form_generator.rb
+++ lib/generators/form/form_generator.rb
@@ -1,6 +1,11 @@
 class FormGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
+  argument :attributes,
+           type: :array,
+           default: [],
+           banner: "field:type field:type"
+
   def copy_form_files
     template "form.erb", "app/forms/#{file_name}_form.rb"
     template "form_spec.erb", "spec/forms/#{file_name}_form_spec.rb"
@@ -14,5 +19,11 @@ class FormGenerator < Rails::Generators::NamedBase
                        "namespace :teacher_interface, path: \"/teacher\" do\n" do
       "    get \"#{plural_name}\", to: \"#{plural_name}#new\"\n    post \"#{plural_name}\", to: \"#{plural_name}#create\"\n"
     end
+    generate :migration,
+             "Add#{class_name}ToEligibilityCheck #{
+               attributes
+                 .map { |attribute| [attribute.name, attribute.type].join(":") }
+                 .join(" ")
+             }"
   end
 end
