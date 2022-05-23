d85d871fb68d15ddd848eb0377e99ba6241ac474
Thomas Leese <thomas@leese.io>
2022-05-23 12:21:07 +0100

    Configure Sentry
    This initialises Sentry for Rails with a few extra options.
    
    As recommended by the Sentry docs, this configures the breadcrumbs to be
    picked up from the Active Support and HTTP loggers, this enhances the
    breadcrumbs we'll see in Sentry issues.
    
    Certain URL parameters (ones containing sensitive information such as
    auth tokens) are filtered out.
    
    Some exceptions have been included, specifically ones where there has
    been a client error when making the request and we wouldn't be able to
    action.
    
    We don't need to specify the DSN or the environment as these are picked
    up automatically from environment variables, `SENTRY_DSN` and
    `SENTRY_ENVIRONMENT`. If the environment is not set, it defaults to
    `development`
    (https://github.com/getsentry/sentry-ruby/blob/eb8c59861cc3dc29a17282d432309edd86db8286/sentry-ruby/lib/sentry/configuration.rb#L467-L469).

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "active_support/parameter_filter"
+
+Sentry.init do |config|
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  filter =
+    ActiveSupport::ParameterFilter.new(
+      Rails.application.config.filter_parameters
+    )
+  config.before_send = lambda { |event, _hint| filter.filter(event.to_hash) }
+
+  config.inspect_exception_causes_for_exclusion = true
+  config.excluded_exceptions += [
+    # The following exceptions are user-errors that aren't actionable, and can
+    # be safely ignored.
+    "ActionController::BadRequest",
+    "ActionController::UnknownFormat",
+    "ActionController::UnknownHttpMethod",
+    "ActionDispatch::Http::Parameters::ParseError",
+    "Mime::Type::InvalidMimeType"
+  ]
+end
