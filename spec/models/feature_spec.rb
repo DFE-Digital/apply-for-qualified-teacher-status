3a24d2c7fb6d162e1bf5e74fcb636d5995a7512b
Thomas Leese <thomas@leese.io>
2022-05-24 14:52:08 +0100

    Annotate feature model spec
    This is done automatically when running `rails db:migrate`.

--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: features
+#
+#  id         :bigint           not null, primary key
+#  active     :boolean          default(FALSE), not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_features_on_name  (name) UNIQUE
+#
 require "rails_helper"
 
 RSpec.describe Feature, type: :model do
