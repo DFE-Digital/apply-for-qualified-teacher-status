fd9b80f0ad9236f074c5e31dae613c96edfbe98d
Felix Clack <felixclack@gmail.com>
2022-05-24 10:05:01 +0100

    Add a feature flag for :service_open
    We want to control access to the service for the general public.
    
    I have introduced feature flags, as per our approach in [Find a lost TRN](https://github.com/DFE-Digital/find-a-lost-trn/pull/57).
    
    I chose to defer the way we manage our ENV variables in development so
    that we can have a dedicated PR for that. For the time being, the
    default values are test/test.
    
    Also, the support interface to manage the feature flags will be
    implemented in a feature change.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
+
+  http_basic_authenticate_with name: ENV.fetch("SUPPORT_USERNAME", "test"),
+                               password: ENV.fetch("SUPPORT_PASSWORD", "test"),
+                               unless: -> { FeatureFlag.active?(:service_open) }
 end
---
+++ spec/models/feature_flag_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe FeatureFlag do
+  let(:feature) { Feature.create_or_find_by(name: feature_name) }
+  let(:feature_name) { :service_open }
+
+  describe ".activate" do
+    it "activates a feature" do
+      expect { described_class.activate(feature_name) }.to change {
+        described_class.active?(feature_name)
+      }.from(false).to(true)
+    end
+
+    it "records the change in the database" do
+      feature.update!(active: false)
+      expect { described_class.activate(feature_name) }.to change {
+        feature.reload.active
+      }.from(false).to(true)
+    end
+  end
+
+  describe ".deactivate" do
+    it "deactivates a feature" do
+      # To avoid flakey tests where activation/deactivation happens at the same time
+      travel_to(5.minutes.ago) { described_class.activate(feature_name) }
+      expect { described_class.deactivate(feature_name) }.to change {
+        described_class.active?(feature_name)
+      }.from(true).to(false)
+    end
+
+    it "records the change in the database" do
+      feature.update!(active: true)
+      expect { described_class.deactivate(feature_name) }.to change {
+        feature.reload.active
+      }.from(true).to(false)
+    end
+  end
+end
