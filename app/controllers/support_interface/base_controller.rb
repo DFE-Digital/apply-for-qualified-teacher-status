db6b924346aa0756e7b4e11cf939a33e00ddcec3
Felix Clack <felixclack@gmail.com>
2022-05-25 16:31:26 +0100

    Add basic_auth to the support interface
    We want access to the support interface to be restricted with basic
    authentication.

--- app/controllers/support_interface/base_controller.rb
+++ app/controllers/support_interface/base_controller.rb
@@ -1,4 +1,6 @@
 module SupportInterface
   class BaseController < ApplicationController
+    http_basic_authenticate_with name: ENV.fetch("SUPPORT_USERNAME", "test"),
+                                 password: ENV.fetch("SUPPORT_PASSWORD", "test")
   end
 end
