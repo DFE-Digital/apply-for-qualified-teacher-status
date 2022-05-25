683e9b0906b9cd6a3f7a004a941eb11891dc83ea
Thomas Leese <thomas@leese.io>
2022-05-25 10:16:34 +0100

    Add a basic healthcheck for the service
    This uses the `okcomputer` Gem and I've included a small test just to
    make sure the route is registered and working, without testing the
    `okcomputer` Gem itself.

---
+++ spec/requests/healthcheck_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "Healthcheck", type: :request do
+  it "responds successfully" do
+    get "/healthcheck"
+
+    expect(response).to have_http_status(:ok)
+  end
+end
