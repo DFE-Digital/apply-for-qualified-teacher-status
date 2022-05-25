d0e6cdb7451cb1cb4706dce1583cd4dc542c0cb6
Thomas Leese <thomas@leese.io>
2022-05-25 12:54:09 +0100

    Send only certain countries through checker
    Other "legacy" countries will be sent to the old service for now until
    we roll the feature out to all countries.

--- spec/forms/country_form_spec.rb
+++ spec/forms/country_form_spec.rb
@@ -30,4 +30,35 @@ RSpec.describe CountryForm, type: :model do
       expect(eligibility_check.country_code).to eq("GB")
     end
   end
+
+  describe "#success_url" do
+    subject(:success_url) { form.success_url }
+
+    let(:eligibility_check) { EligibilityCheck.new }
+    let(:form) { described_class.new(eligibility_check:) }
+
+    before { eligibility_check.country_code = country_code }
+
+    context "with an eligible country" do
+      let(:country_code) { "GB" }
+
+      it { is_expected.to eq("/teacher/degree") }
+    end
+
+    context "with a legacy country" do
+      let(:country_code) { "FR" }
+
+      it do
+        is_expected.to eq(
+          "https://teacherservices.education.gov.uk/MutualRecognition/"
+        )
+      end
+    end
+
+    context "with a non-eligible country" do
+      let(:country_code) { "ES" }
+
+      it { is_expected.to eq("/teacher/ineligible") }
+    end
+  end
 end
