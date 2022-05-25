27d97323d855a5f523c98f01895ea8a7d6ec23ca
Thomas Leese <thomas@leese.io>
2022-05-25 16:16:30 +0100

    Support multiple ineligible reasons
    This changes the eligibility check model to support multiple reasons why
    the person could be ineligible to apply for QTS. This is the precursor
    to changing the flow so the user gets to answer every question.

--- spec/models/eligibility_check_spec.rb
+++ spec/models/eligibility_check_spec.rb
@@ -17,79 +17,100 @@ require "rails_helper"
 RSpec.describe EligibilityCheck, type: :model do
   let(:eligibility_check) { EligibilityCheck.new }
 
-  describe "#ineligible_reason" do
-    subject(:ineligible_reason) { eligibility_check.ineligible_reason }
+  describe "#ineligible_reasons" do
+    subject(:ineligible_reasons) { eligibility_check.ineligible_reasons }
 
     context "when free_of_sanctions is true" do
       before { eligibility_check.free_of_sanctions = true }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to_not include(:misconduct) }
     end
 
     context "when free_of_sanctions is false" do
       before { eligibility_check.free_of_sanctions = false }
 
-      it { is_expected.to eq(:misconduct) }
+      it { is_expected.to include(:misconduct) }
     end
 
     context "when recognised is true" do
       before { eligibility_check.recognised = true }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to_not include(:recognised) }
     end
 
     context "when recognised is false" do
       before { eligibility_check.recognised = false }
 
-      it { is_expected.to eq(:recognised) }
+      it { is_expected.to include(:recognised) }
     end
 
     context "when teach_children is true" do
       before { eligibility_check.teach_children = true }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to_not include(:teach_children) }
     end
 
     context "when teach_children is false" do
       before { eligibility_check.teach_children = false }
 
-      it { is_expected.to eq(:teach_children) }
+      it { is_expected.to include(:teach_children) }
     end
 
     context "when qualification is true" do
       before { eligibility_check.qualification = true }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to_not include(:qualification) }
     end
 
     context "when qualification is false" do
       before { eligibility_check.qualification = false }
 
-      it { is_expected.to eq(:qualification) }
+      it { is_expected.to include(:qualification) }
     end
 
     context "when degree is true" do
       before { eligibility_check.degree = true }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to_not include(:degree) }
     end
 
     context "when degree is false" do
       before { eligibility_check.degree = false }
 
-      it { is_expected.to eq(:degree) }
+      it { is_expected.to include(:degree) }
     end
 
     context "when country_code is eligible" do
       before { eligibility_check.country_code = "GB" }
 
-      it { is_expected.to be_nil }
+      it { is_expected.to_not include(:country) }
     end
 
     context "when country_code is ineligible" do
       before { eligibility_check.country_code = "INELIGIBLE" }
 
-      it { is_expected.to eq(:country) }
+      it { is_expected.to include(:country) }
+    end
+  end
+
+  describe "#eligible?" do
+    subject(:eligible?) { eligibility_check.eligible? }
+
+    context "when not eligible" do
+      it { is_expected.to be false }
+    end
+
+    context "when eligible" do
+      before do
+        eligibility_check.free_of_sanctions = true
+        eligibility_check.recognised = true
+        eligibility_check.teach_children = true
+        eligibility_check.qualification = true
+        eligibility_check.degree = true
+        eligibility_check.country_code = "GB"
+      end
+
+      it { is_expected.to be true }
     end
   end
 
