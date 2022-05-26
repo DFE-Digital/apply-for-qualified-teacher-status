982fda1c4e27410ce68abd75b0c61abdfd1e9213
Thomas Leese <thomas@leese.io>
2022-05-26 14:04:22 +0100

    Set SENTRY_ENVIRONMENT env var
    This will match the environment name, and ensures that any issues appear in Sentry in the right environment.

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,5 +1,5 @@
 locals {
-  app_environment_variables = try(local.application_environment_variables, null)
+  app_environment_variables = try(local.application_environment_variables, {})
 }
 
 resource "cloudfoundry_route" "apply_qts_public" {
@@ -8,22 +8,28 @@ resource "cloudfoundry_route" "apply_qts_public" {
   space    = data.cloudfoundry_space.space.id
 }
 resource "cloudfoundry_app" "app" {
-  name                       = local.apply_qts_app_name
-  space                      = data.cloudfoundry_space.space.id
-  instances                  = var.apply_qts_instances
-  memory                     = var.apply_qts_memory
-  disk_quota                 = var.apply_qts_disk_quota
-  docker_image               = var.apply_qts_docker_image
-  strategy                   = "blue-green"
-  environment                = local.app_environment_variables
+  name         = local.apply_qts_app_name
+  space        = data.cloudfoundry_space.space.id
+  instances    = var.apply_qts_instances
+  memory       = var.apply_qts_memory
+  disk_quota   = var.apply_qts_disk_quota
+  docker_image = var.apply_qts_docker_image
+  strategy     = "blue-green"
+
+  environment = merge(local.app_environment_variables, {
+    SENTRY_ENVIRONMENT = var.environment_name,
+  })
+
   health_check_type          = "http"
   health_check_http_endpoint = "/healthcheck"
+
   dynamic "routes" {
     for_each = local.apply_qts_routes
     content {
       route = routes.value.id
     }
   }
+
   service_binding {
     service_instance = cloudfoundry_service_instance.postgres.id
   }
