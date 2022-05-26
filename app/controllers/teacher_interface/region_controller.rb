749417471a4c84b47b027c8f502f963fbdafec4b
Thomas Leese <thomas@leese.io>
2022-05-26 21:22:40 +0100

    Allow users to select a region
    This allows users to select a region if the country they selected has
    more than one region. There hasn't been any design input on this and
    I've decided to go with a radio button, but we could easily change it to
    a drop down if that works better.

---
+++ app/controllers/teacher_interface/region_controller.rb
@@ -0,0 +1,29 @@
+module TeacherInterface
+  class RegionController < BaseController
+    before_action :load_regions
+
+    def new
+      @region_form = RegionForm.new
+    end
+
+    def create
+      @region_form =
+        RegionForm.new(region_form_params.merge(eligibility_check:))
+      if @region_form.save
+        redirect_to @region_form.success_url
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def region_form_params
+      params.require(:region_form).permit(:region_id)
+    end
+
+    def load_regions
+      @regions = eligibility_check.country_regions
+    end
+  end
+end
