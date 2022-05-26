f6867d35577bb5db95ef2597784f36e6011ecbd3
Thomas Leese <thomas@leese.io>
2022-05-26 12:36:42 +0100

    Add useful database seed
    This prepares the database in a way that reflects reality. It's not
    intended to be the source of truth for the data, and I don't expect us
    to update this over time, but for now it provides the checker with
    enough information to develop locally with a good representation of
    production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,188 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-sct = Country.create!(code: "GB-SCT")
-sct.regions.create!
+COUNTRIES = {
+  AT: {
+  },
+  BE: {
+    regions: ["Flemish region", "French region", "German region"]
+  },
+  BG: {
+  },
+  HR: {
+  },
+  CY: {
+  },
+  CZ: {
+  },
+  DK: {
+  },
+  EE: {
+  },
+  FI: {
+  },
+  FR: {
+  },
+  DE: {
+    regions: [
+      "Baden-Wurttemberg",
+      "Bavaria",
+      "Berlin",
+      "Brandenburg",
+      "Bremen",
+      "Hamburg",
+      "Hessen",
+      "Lower Saxony",
+      "Mecklenburg-Vorpommern",
+      "Saxony-Anhalt",
+      "Schleswig-Holstein",
+      "Thuringia",
+      "North Rhine-Westphalia",
+      "Saxony",
+      "Rhineland-Palatinate",
+      "Saarland"
+    ]
+  },
+  GR: {
+  },
+  HU: {
+  },
+  IS: {
+  },
+  IT: {
+  },
+  LV: {
+  },
+  LI: {
+  },
+  LT: {
+  },
+  LU: {
+  },
+  MT: {
+  },
+  NL: {
+  },
+  NO: {
+  },
+  PL: {
+  },
+  IE: {
+  },
+  RO: {
+  },
+  SK: {
+  },
+  SI: {
+  },
+  ES: {
+  },
+  SE: {
+  },
+  CH: {
+  },
+  AU: {
+    regions: [
+      "Victoria",
+      "Queensland",
+      "Northern Territory",
+      "Western Australia",
+      "Tasmania",
+      "New South Wales",
+      "South Australia",
+      "Australian Capital Territory"
+    ]
+  },
+  CA: {
+    regions: [
+      "Ontario",
+      "Alberta",
+      "British Columbia",
+      "Manitoba",
+      "Nunavut",
+      "New Brunswick",
+      "Newfoundland and Labrador",
+      "Northwest Territories",
+      "Nova Scotia",
+      "Prince Edward Island",
+      "Quebec",
+      "Saskatchewan",
+      "Yukon"
+    ]
+  },
+  NZ: {
+  },
+  US: {
+    regions: [
+      "Alabama",
+      "Alaska",
+      "Arizona",
+      "Arkansas",
+      "California",
+      "Colorado",
+      "Connecticut",
+      "Delaware",
+      "Florida",
+      "Georgia",
+      "Hawaii",
+      "Idaho",
+      "Illinois",
+      "Indiana",
+      "Kentucky",
+      "Kansas",
+      "Louisiana",
+      "Maine",
+      "Iowa",
+      "Maryland",
+      "Massachusetts",
+      "Montana",
+      "Michigan",
+      "Minnesota",
+      "Mississippi",
+      "Missouri",
+      "Nebraska",
+      "Nevada",
+      "New Hampshire",
+      "New Jersey",
+      "New Mexico",
+      "New York",
+      "North Carolina",
+      "North Dakota",
+      "Ohio",
+      "Oklahoma",
+      "Oregon",
+      "Pennsylvania",
+      "Rhode Island",
+      "South Carolina",
+      "Tennessee",
+      "Texas",
+      "Utah",
+      "Vermont",
+      "Virginia",
+      "Washington",
+      "Washington DC",
+      "West Virginia",
+      "Wisonsin",
+      "Wyoming"
+    ]
+  },
+  GI: {
+  },
+  "GB-SCT": {
+  },
+  "GB-NIR": {
+  }
+}.freeze
 
-fr = Country.create!(code: "FR", legacy: true)
-fr.regions.create!
+COUNTRIES.each do |code, attributes|
+  legacy = attributes.fetch(:legacy, false)
+  regions = attributes.fetch(:regions, [])
+
+  country = Country.create!(code:, legacy:)
+
+  if regions.empty?
+    country.regions.create!
+  else
+    regions.each { |name| country.regions.create!(name:) }
+  end
+end
