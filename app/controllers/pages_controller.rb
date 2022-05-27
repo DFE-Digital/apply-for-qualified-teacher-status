74c746b1dee94470d2a50e1e16e902f4cc561eb1
Thomas Leese <thomas@leese.io>
2022-05-27 12:20:40 +0100

    Allow seeing all performance stats ?since_launch
    Adds a `?since_launch` query string param to the performance dashboard
    that allows querying for the full data since May the 4th.
    
    This page will likely get slow over time, but we can worry about that
    later. Making it a query string param makes it slightly more hidden than
    the regular performance dashboard.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,32 @@
 class PagesController < ApplicationController
   def performance
+    since = 1.week.ago.beginning_of_day
+    until_days = 6
+    @since_text = "over the last 7 days"
+
+    if params.key? :since_launch
+      launch_date = Date.new(2022, 6, 1).beginning_of_day
+      since = launch_date
+      until_days = ((Time.zone.now.beginning_of_day - since) / (3600 * 24)).to_i
+      @since_text = "since launch"
+    end
+
     eligibility_checks =
-      EligibilityCheck.where(
-        created_at: 1.week.ago.beginning_of_day..Time.zone.now
-      ).group("date_trunc('day', created_at)")
+      EligibilityCheck.where(created_at: since..Time.zone.now).group(
+        "date_trunc('day', created_at)"
+      )
 
     eligibility_checks_total = eligibility_checks.count
 
     eligibility_checks_eligible = eligibility_checks.eligible.count
 
-    last_7_days = (0..6).map { |n| n.days.ago.beginning_of_day.utc }
+    last_n_days = (0..until_days).map { |n| n.days.ago.beginning_of_day.utc }
 
-    @checks_over_last_7_days = eligibility_checks_total.values.sum
+    @checks_over_last_n_days = eligibility_checks_total.values.sum
 
     @live_service_data = [%w[Date Requests]]
     @live_service_data +=
-      last_7_days.map do |day|
+      last_n_days.map do |day|
         [day.strftime("%d %B"), eligibility_checks_total[day] || 0]
       end
 
@@ -23,7 +34,7 @@ class PagesController < ApplicationController
 
     @submission_data = [["Date", "Successful checks"]]
     @submission_data +=
-      last_7_days.map do |day|
+      last_n_days.map do |day|
         [day.strftime("%d %B"), eligibility_checks_eligible[day] || 0]
       end
   end
