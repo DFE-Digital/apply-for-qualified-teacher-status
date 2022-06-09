dcd4e7df40c7bb54c4454bf94cd2b011407b4b39
Thomas Leese <thomas@leese.io>
2022-06-09 09:44:17 +0100

    Refactor mutual recognition URL to a constant
    This allow us to use it in multiple places.

--- app/controllers/teacher_interface/pages_controller.rb
+++ app/controllers/teacher_interface/pages_controller.rb
@@ -3,7 +3,11 @@ module TeacherInterface
     before_action :load_eligibility_check
     before_action :complete_eligibility_check, only: %i[eligible ineligible]
 
+    MUTUAL_RECOGNITION_URL =
+      "https://teacherservices.education.gov.uk/MutualRecognition/"
+
     def eligible
+      @mutual_recognition_url = MUTUAL_RECOGNITION_URL
       session[:eligibility_check_complete] = true
     end
 
