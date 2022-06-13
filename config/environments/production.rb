11e95c6c641a08b07174438c62de58d88fee9b99
Thomas Leese <thomas@leese.io>
2022-06-13 09:08:01 +0100

    Configure GOV.UK Notify in production
    This relies on their existing a `GOVUK_NOTIFY_API_KEY` environment
    variable being set.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :notify
+  config.action_mailer.notify_settings = {
+    api_key: ENV.fetch("GOVUK_NOTIFY_API_KEY")
+  }
 end
