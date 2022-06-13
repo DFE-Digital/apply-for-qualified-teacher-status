8d71380ae0f89e6369cd27477ee6a8b561217d68
Thomas Leese <thomas@leese.io>
2022-06-13 12:40:48 +0100

    Configure Sidekiq
    This sets up three queues, `mailer`, `analytics` and `default` in that
    priority order. I've intentionally left `default` as lowest priority to
    encourage the use of named queues.
    
    I've also added a Sidekiq process to the Procfile for production and
    development.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bin/rails db:migrate && bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
+worker: bundle exec sidekiq -C ./config/sidekiq.yml
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "apply_for_qualified_teacher_status_production"
+  config.active_job.queue_adapter = :sidekiq
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
