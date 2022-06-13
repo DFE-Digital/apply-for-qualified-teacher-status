d418354c7c5be49af41acbd4ba2ddab32b0037c9
Thomas Leese <thomas@leese.io>
2022-06-13 09:04:39 +0100

    Make it easy to test ActionMailer
    This enabled the test delivery method and test queue adapter so we can
    test email sending via RSpec.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.action_mailer.delivery_method = :test
+
+  config.active_job.queue_adapter = :test
 end
