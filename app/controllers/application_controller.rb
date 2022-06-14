8c8aa3720cbaa9e44a84081ea78f1af359d8d11a
Thomas Leese <thomas@leese.io>
2022-06-14 09:31:55 +0100

    Record analytics for requests and models
    This uses the DfE Analytics library to record web requests and model
    changes to BigQuery.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  include DfE::Analytics::Requests
+
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
 
   http_basic_authenticate_with name: ENV.fetch("SUPPORT_USERNAME", "test"),
                                password: ENV.fetch("SUPPORT_PASSWORD", "test"),
                                unless: -> { FeatureFlag.active?(:service_open) }
+
+  def current_user
+    nil
+  end
 
   helper_method :current_namespace
 end
