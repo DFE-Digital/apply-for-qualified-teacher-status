3f61d64c45b2de92bd55ffe605283337c15cd830
Thomas Leese <thomas@leese.io>
2022-06-14 08:28:59 +0100

    Add healthcheck for Redis
    This ensures that Redis is healthy before the app takes requests.

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,6 +1,16 @@
 OkComputer.mount_at = "healthcheck"
 
 OkComputer::Registry.register "postgresql", OkComputer::ActiveRecordCheck.new
+
+OkComputer::Registry.register "redis",
+                              OkComputer::RedisCheck.new(
+                                url:
+                                  ENV.fetch(
+                                    "REDIS_URL",
+                                    "redis://localhost:6379/1"
+                                  )
+                              )
+
 OkComputer::Registry.register "version", OkComputer::AppVersionCheck.new
 
 OkComputer.make_optional %w[version]
