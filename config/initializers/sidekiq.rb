4098a91fd7d502a54c2761e9ef2c63e78f211f4a
Thomas Leese <thomas@leese.io>
2022-06-14 10:27:29 +0100

    Reduce duplication of REDIS_URL
    We now only fetch it one place and use the same value in multiple
    places.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,5 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
-end
+url = ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1") }
-end
+Sidekiq.configure_server { |config| config.redis = { url: } }
+
+Sidekiq.configure_client { |config| config.redis = { url: } }
