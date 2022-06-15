32d8b96af8760c9be0b248c68b83ec50d7b0fdec
Thomas Leese <thomas@leese.io>
2022-06-15 18:08:37 +0100

    Increase session cookie expiration to 20 hours
    This is to satisfy an accessibility assurance item 2.2.1:
    
    2.2.1 - Users are warned of time limits shorter than 20 hours and
    time limits can be turned off or extended

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store,
                                        key: "_session_id",
                                        secure: Rails.env.production?,
-                                       expire_after: 2.hours
+                                       expire_after: 20.hours
