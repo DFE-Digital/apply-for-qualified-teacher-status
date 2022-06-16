d0067ea6ea4a18e6921297546b0e6e5f8a37c5e3
Thomas Leese <thomas@leese.io>
2022-06-16 09:46:50 +0100

    Fix logic to check whether analytics should be disabled
    Previously we were saying that analytics should be enabled only if
    `BIGQUERY_DISABLE` was set to something other than `false`, whereas we
    want it to be the other way around.

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -1,6 +1,6 @@
 DfE::Analytics.configure do |config|
   config.enable_analytics =
-    proc { ENV.fetch("BIGQUERY_DISABLE", "false") != "false" }
+    proc { ENV.fetch("BIGQUERY_DISABLE", "false") != "true" }
   config.queue = :analytics
   config.environment = ENV.fetch("SENTRY_ENVIRONMENT", "development")
 end
