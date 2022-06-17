7627bba7ddbcb310cf8dc4ca9cec8227825423d1
Thomas Leese <thomas@leese.io>
2022-06-17 08:49:36 +0100

    Add environment phase banners
    This adds a tag with a distinctive colour for each phase banner. It
    allows us to differentiate between the different versions, and reduces
    the risk of doing the wrong thing on production.
    
    I can't use the `govuk_phase_banner` component as the classes get
    overridden.

--- app/lib/hosting_environment.rb
+++ app/lib/hosting_environment.rb
@@ -2,4 +2,20 @@ module HostingEnvironment
   def self.name
     ENV.fetch("HOSTING_ENVIRONMENT", "development")
   end
+
+  def self.phase
+    return nil if production?
+
+    name.capitalize
+  end
+
+  def self.phase_text
+    return nil if production?
+
+    "This is a #{name} version of the service."
+  end
+
+  def self.production?
+    name == "production"
+  end
 end
