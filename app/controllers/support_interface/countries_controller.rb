cb347986ecd07657578078ed78676486a9b84edb
Thomas Leese <thomas@leese.io>
2022-06-17 15:08:31 +0100

    Remove private beta countries support
    We don't need this section now that we'll support modifying the
    countries directly.

--- app/controllers/support_interface/countries_controller.rb
+++ app/controllers/support_interface/countries_controller.rb
@@ -2,12 +2,5 @@ module SupportInterface
   class CountriesController < BaseController
     def index
     end
-
-    def configure_private_beta
-      ConfigureCountries.private_beta!
-
-      flash[:success] = "Private beta countries configured"
-      redirect_to support_interface_countries_path
-    end
   end
 end
