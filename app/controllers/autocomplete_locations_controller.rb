DFE-Digital/apply-for-qualified-teacher-status

commit a4e5e92d801584d8f092c0cd1f767ade1c7eeb0f

Enforce eligibility question order
Currently, we don't enforce the order of the steps in the eligibility
check and if someone happens to know a valid URL, they can enter it
and effectively skip steps in the question flow.

This determines the furthest step that has been completed in the flow
based on the presence of the attributes.

The system needs to allow people to go to a previously completed step as
well.

This is based off
https://github.com/DFE-Digital/find-a-lost-trn/pull/119.

app/controllers/eligibility_interface/countries_controller.rb
@@ -1,5 +1,7 @@
 module EligibilityInterface
   class CountriesController < BaseController
+    include EnforceEligibilityQuestionOrder
+
     def index
       render json: LOCATION_AUTOCOMPLETE_GRAPH
     end

app/controllers/concerns/enforce_eligibility_question_order.rb
@@ -1,0 +1,31 @@
+module EnforceEligibilityQuestionOrder
+  extend ActiveSupport::Concern
+
+  included { before_action :redirect_by_status }
+
+  def redirect_by_status
+    return if current_page_is_allowed?
+
+    expected_path = paths[eligibility_check.status]
+    redirect_to(expected_path) if expected_path && expected_path != request.path
+  end
+
+  private
+
+  def current_page_is_allowed?
+    order = paths.keys.index(eligibility_check&.status)
+    current_position = paths.values.index(request.path)
+    current_position && order && current_position <= order
+  end
+
+  def paths
+    {
+      country: eligibility_interface_countries_path,
+      region: eligibility_interface_region_path,
+      degree: eligibility_interface_degree_path,
+      qualification: eligibility_interface_qualifications_path,
+      teach_children: eligibility_interface_teach_children_path,
+      misconduct: eligibility_interface_misconduct_path
+    }
+  end
+end

app/controllers/eligibility_interface/degrees_controller.rb
@@ -1,5 +1,7 @@
 module EligibilityInterface
   class DegreesController < BaseController
+    include EnforceEligibilityQuestionOrder
+
     def new
       @degree_form = DegreeForm.new
     end

app/controllers/eligibility_interface/misconduct_controller.rb
@@ -1,5 +1,7 @@
 module EligibilityInterface
   class MisconductController < BaseController
+    include EnforceEligibilityQuestionOrder
+
     def new
       @misconduct_form = MisconductForm.new
     end

app/controllers/eligibility_interface/pages_controller.rb
@@ -1,7 +1,9 @@
 module EligibilityInterface
   class PagesController < BaseController
-    before_action :load_eligibility_check, except: %i[root]
+    before_action :ensure_eligibility_check_status,
+                  only: %i[eligible ineligible]
     before_action :complete_eligibility_check, only: %i[eligible ineligible]
+    before_action :load_eligibility_check, only: %i[eligible ineligible]
 
     MUTUAL_RECOGNITION_URL =
       "https://teacherservices.education.gov.uk/MutualRecognition/".freeze
@@ -25,8 +27,14 @@ module EligibilityInterface
 
     private
 
+    def ensure_eligibility_check_status
+      if eligibility_check.status != :eligibility
+        redirect_to eligibility_interface_start_path
+      end
+    end
+
     def complete_eligibility_check
-      @eligibility_check.complete! if @eligibility_check.persisted?
+      eligibility_check.complete! if eligibility_check.persisted?
     end
 
     def load_eligibility_check

app/controllers/eligibility_interface/qualifications_controller.rb
@@ -1,5 +1,7 @@
 module EligibilityInterface
   class QualificationsController < BaseController
+    include EnforceEligibilityQuestionOrder
+
     def new
       @qualification_form = QualificationForm.new
     end

app/controllers/eligibility_interface/recognised_controller.rb
@@ -1,5 +1,7 @@
 module EligibilityInterface
   class RecognisedController < BaseController
+    include EnforceEligibilityQuestionOrder
+
     def new
       @recognised_form = RecognisedForm.new
     end

app/controllers/eligibility_interface/region_controller.rb
@@ -1,5 +1,7 @@
 module EligibilityInterface
   class RegionController < BaseController
+    include EnforceEligibilityQuestionOrder
+
     before_action :load_regions
 
     def new

commit 76fb6306e1eebe48872c5e3ddd1208e74fda54aa

Fix autocomplete locations
This was accidentally broken in a4e5e92d801584d8f092c0cd1f767ade1c7eeb0f
as the controller method for getting the locations ended up redirecting
the user rather than serving the results. I've pulled it out into a
separate controller since it might be useful in other parts of the app
anyway.

app/controllers/autocomplete_locations_controller.rb
@@ -1,0 +1,8 @@
+class AutocompleteLocationsController < ApplicationController
+  LOCATION_AUTOCOMPLETE_GRAPH =
+    JSON.parse(File.read("public/location-autocomplete-graph.json"))
+
+  def index
+    render json: LOCATION_AUTOCOMPLETE_GRAPH
+  end
+end

app/controllers/eligibility_interface/countries_controller.rb
@@ -2,10 +2,6 @@ module EligibilityInterface
   class CountriesController < BaseController
     include EnforceEligibilityQuestionOrder
 
-    def index
-      render json: LOCATION_AUTOCOMPLETE_GRAPH
-    end
-
     def new
       @country_form = CountryForm.new
     end
@@ -21,9 +17,6 @@ module EligibilityInterface
     end
 
     private
-
-    LOCATION_AUTOCOMPLETE_GRAPH =
-      JSON.parse(File.read("public/location-autocomplete-graph.json"))
 
     def location_form_params
       params.require(:eligibility_interface_country_form).permit(:location)

app/javascript/application.js
@@ -14,7 +14,7 @@ const loadCountryAutoComplete = () => {
   if (locationPicker) {
     openregisterLocationPicker({
       selectElement: locationPicker,
-      url: "/eligibility/locations.json",
+      url: "/autocomplete_locations.json",
     });
   }
 };

config/routes.rb
@@ -28,7 +28,6 @@ Rails.application.routes.draw do
     get "ineligible", to: "pages#ineligible"
     get "misconduct", to: "misconduct#new"
     post "misconduct", to: "misconduct#create"
-    get "locations", to: "countries#index"
   end
 
   namespace :support_interface, path: "/support" do
@@ -63,6 +62,8 @@ Rails.application.routes.draw do
 
     mount Sidekiq::Web, at: "sidekiq"
   end
+
+  resources :autocomplete_locations, only: %i[index]
 
   get "accessibility", to: "static#accessibility"
   get "cookies", to: "static#cookies"