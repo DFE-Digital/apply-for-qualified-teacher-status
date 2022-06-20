30ba38307ab87d554f744ceda775303cfa44eaba
Thomas Leese <thomas@leese.io>
2022-06-20 08:35:11 +0100

    Fix autocomplete locations
    This was accidentally broken in a4e5e92d801584d8f092c0cd1f767ade1c7eeb0f
    as the controller method for getting the locations ended up redirecting
    the user rather than serving the results. I've pulled it out into a
    separate controller since it might be useful in other parts of the app
    anyway.

---
+++ app/controllers/autocomplete_locations_controller.rb
@@ -0,0 +1,8 @@
+class AutocompleteLocationsController < ApplicationController
+  LOCATION_AUTOCOMPLETE_GRAPH =
+    JSON.parse(File.read("public/location-autocomplete-graph.json"))
+
+  def index
+    render json: LOCATION_AUTOCOMPLETE_GRAPH
+  end
+end
