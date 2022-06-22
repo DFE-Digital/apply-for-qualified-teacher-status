d12401691b19eb688b00b232b4247bf3cd8ce1cc
Thomas Leese <thomas@leese.io>
2022-06-22 17:42:30 +0100

    Add staff model
    This adds a model which represents our staff users, those who are
    assessors and have access to the support interface.
    
    I've enabled all the relevant sounding Devise modules, I figured it was
    better to start with them enabled, and we can always remove them if we
    find they're not useful in the future.
    
    I also had to add a `devise_for` in the routes to allow the model to be
    created without Devise complaining, even though these controllers don't
    work yet.

---
+++ spec/models/staff_spec.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: staff
+#
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_staff_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_staff_on_email                 (email) UNIQUE
+#  index_staff_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_staff_on_unlock_token          (unlock_token) UNIQUE
+#
+require "rails_helper"
+
+RSpec.describe Staff, type: :model do
+  subject(:staff) { build(:staff) }
+
+  describe "validations" do
+    it { is_expected.to be_valid }
+
+    it { is_expected.to validate_presence_of(:email) }
+    it do
+      is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity
+    end
+
+    it { is_expected.to validate_presence_of(:password).allow_nil }
+  end
+end
