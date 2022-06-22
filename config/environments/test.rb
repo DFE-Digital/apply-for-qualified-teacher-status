97305ad2d6e173f551739a053133f992f29a554e
Thomas Leese <thomas@leese.io>
2022-06-22 13:36:59 +0100

    Set default_url_options
    This is required by Devise when sending emails in development.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -53,5 +53,6 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :test
 
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :test
 end
