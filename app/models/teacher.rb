0f4e143b6132d5dd7f8100a3bcec0bf005f1ae11
Thomas Leese <thomas@leese.io>
2022-06-23 09:34:45 +0100

    Add teacher model
    This adds a model which represents our teacher users, those who will
    apply for QTS and log in using a magic link mechanism.
    
    Since we won't be using many of the Devise modules for this model, it's
    quite a basic version for now, and we can add more fields in the future.

---
+++ app/models/teacher.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: teachers
+#
+#  id         :bigint           not null, primary key
+#  email      :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_teachers_on_email  (email) UNIQUE
+#
+class Teacher < ApplicationRecord
+  devise :timeoutable
+
+  validates :email,
+            presence: true,
+            uniqueness: {
+              allow_blank: true,
+              case_sensitive: true,
+              if: :will_save_change_to_email?
+            },
+            format: {
+              with: Devise.email_regexp,
+              allow_blank: true,
+              if: :will_save_change_to_email?
+            }
+end
