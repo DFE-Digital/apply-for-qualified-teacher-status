8c6e9fcc3110aecd1a17ce09c0b3e9d292f6a040
Thomas Leese <thomas@leese.io>
2022-06-23 15:45:55 +0100

    Write emails to a file in development environment
    This makes local development easier as the contents of the email can
    then be read without the need to run a mail server locally.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,4 +66,5 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :file
 end
