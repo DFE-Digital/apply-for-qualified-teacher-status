786aca397d2e723e5ccc40d8833d4a400548e1b6
Thomas Leese <thomas@leese.io>
2022-06-24 14:23:51 +0100

    Add system tests for Sidekiq
    This ensures that you can only view the dashboard if you are signed in
    as a support user.

---
+++ spec/system/support_interface/sidekiq_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Sidekiq support", type: :system do
+  it "allows viewing Sidekiq dashboard" do
+    when_i_am_authorized_as_a_support_user
+    when_i_visit_the_sidekiq_page
+    then_i_see_the_sidekiq_dashboard
+  end
+
+  private
+
+  def when_i_am_authorized_as_a_support_user
+    page.driver.basic_authorize(
+      ENV.fetch("SUPPORT_USERNAME", "test"),
+      ENV.fetch("SUPPORT_PASSWORD", "test")
+    )
+  end
+
+  def when_i_visit_the_sidekiq_page
+    visit "/support/sidekiq"
+  end
+
+  def then_i_see_the_sidekiq_dashboard
+    expect(page).to have_current_path("/support/sidekiq")
+    expect(page).to have_content("Dashboard")
+    expect(page).to have_content("History")
+  end
+end
