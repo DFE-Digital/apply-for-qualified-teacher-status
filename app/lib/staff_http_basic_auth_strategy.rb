4cce32832d4fb6439f72de8b0bbea8041db3bd9f
Thomas Leese <thomas@leese.io>
2022-06-24 15:34:28 +0100

    Add StaffHttpBasicAuthStrategy
    This adds a Warden strategy which uses HTTP Basic authentication to
    check whether someone is authenticated as a staff user.

---
+++ app/lib/staff_http_basic_auth_strategy.rb
@@ -0,0 +1,48 @@
+class StaffHttpBasicAuthStrategy < Warden::Strategies::Base
+  def valid?
+    FeatureFlag.active?(:staff_http_basic_auth) || !Staff.exists?
+  end
+
+  def store?
+    false
+  end
+
+  def authenticate!
+    auth = Rack::Auth::Basic::Request.new(env)
+
+    return success!(true) if credentials_valid?(auth)
+
+    custom!(
+      [
+        401,
+        {
+          "Content-Type" => "text/plain",
+          "Content-Length" => "0",
+          "WWW-Authenticate" => "Basic realm=\"Application\""
+        },
+        []
+      ]
+    )
+  end
+
+  private
+
+  SUPPORT_USERNAME =
+    Digest::SHA256.hexdigest(ENV.fetch("SUPPORT_USERNAME", "test"))
+  SUPPORT_PASSWORD =
+    Digest::SHA256.hexdigest(ENV.fetch("SUPPORT_PASSWORD", "test"))
+
+  def credentials_valid?(auth)
+    return false unless auth.provided? && auth.basic? && auth.credentials
+
+    valid_comparison?(SUPPORT_USERNAME, auth.credentials.first) &&
+      valid_comparison?(SUPPORT_PASSWORD, auth.credentials.last)
+  end
+
+  def valid_comparison?(correct_value, given_value)
+    ActiveSupport::SecurityUtils.secure_compare(
+      Digest::SHA256.hexdigest(given_value),
+      correct_value
+    )
+  end
+end
