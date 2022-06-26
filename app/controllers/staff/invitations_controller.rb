8ccc9987098c25b448eba85f15db64dc0e8594ab
Thomas Leese <thomas@leese.io>
2022-06-26 20:29:14 +0200

    Generate Devise controllers for staff
    This generates the default controllers provided by Devise and places
    them in the app so we can begin to customise them to our needs.

---
+++ app/controllers/staff/invitations_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Staff::InvitationsController < Devise::InvitationsController
+  # GET /resource/invitation/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/invitation
+  # def create
+  #   super
+  # end
+
+  # GET /resource/invitation/accept?invitation_token=abcdef
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource/invitation
+  # def update
+  #   super
+  # end
+
+  # GET /resource/invitation/remove?invitation_token=abcdef
+  # def destroy
+  #   super
+  # end
+end
