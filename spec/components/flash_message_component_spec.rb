78688a6f012a8156fc14044e15abee4c956d7b07
Thomas Leese <thomas@leese.io>
2022-06-28 13:55:14 +0100

    Support Devise flash message keys
    Devise uses hard coded flash message keys `alert` and `notice` so we
    need to make sure they're supported by the flash message component.

--- spec/components/flash_message_component_spec.rb
+++ spec/components/flash_message_component_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe FlashMessageComponent, type: :component do
   end
 
   context "when an invalid flash key is provided" do
-    let(:flash) { { alert: "Message" } }
+    let(:flash) { { invalid: "Message" } }
 
     it "does not render any content" do
       expect(component.text).to be_empty
@@ -20,6 +20,16 @@ RSpec.describe FlashMessageComponent, type: :component do
 
   context "when a valid flash key is provided" do
     let(:flash) { { success: "Your application has been updated" } }
+
+    it "renders the correct content" do
+      expect(
+        component.css(".govuk-notification-banner__heading").text
+      ).to include("Your application has been updated")
+    end
+  end
+
+  context "when a Devise flash key is provided" do
+    let(:flash) { { alert: "Your application has been updated" } }
 
     it "renders the correct content" do
       expect(
