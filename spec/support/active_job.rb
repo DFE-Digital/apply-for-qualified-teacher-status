cd9f262b890f0bcd7b7f82f7e0b5f84814b366ef
Thomas Leese <thomas@leese.io>
2022-06-28 10:37:05 +0100

    Run jobs in system tests
    This ensures the system tests can run as realistically as possible as
    the jobs will always be run.

--- spec/support/active_job.rb
+++ spec/support/active_job.rb
@@ -1,1 +1,13 @@
-RSpec.configure { |config| config.include ActiveJob::TestHelper }
+RSpec.configure do |config|
+  config.include ActiveJob::TestHelper
+
+  config.around(:example, type: :system) do |example|
+    perform_enqueued_jobs { example.run }
+  end
+
+  config.after(:example, type: :system) { clear_enqueued_jobs }
+
+  # Necessary for `perform_enqueued_jobs` to work:
+  # https://github.com/rails/rails/issues/37270
+  (ActiveJob::Base.descendants << ActiveJob::Base).each(&:disable_test_adapter)
+end
