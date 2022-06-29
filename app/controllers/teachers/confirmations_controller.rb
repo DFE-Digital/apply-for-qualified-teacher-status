96e1ff85c29b1f6b017e3502cf17d47cfab5aea0
Thomas Leese <thomas@leese.io>
2022-06-29 10:43:30 +0100

    Automatically sign in after confirming as a teacher
    Since we sign in using magic links anyway, it makes sense that once
    confirmed, you shouldn't need to sign in with the same method.

--- app/controllers/teachers/confirmations_controller.rb
+++ app/controllers/teachers/confirmations_controller.rb
@@ -13,10 +13,23 @@ class Teachers::ConfirmationsController < Devise::ConfirmationsController
   #   super
   # end
 
-  # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      sign_in(resource)
+      respond_with_navigational(resource) do
+        redirect_to after_confirmation_path_for(resource_name, resource)
+      end
+    else
+      respond_with_navigational(
+        resource.errors,
+        status: :unprocessable_entity
+      ) { render :new }
+    end
+  end
 
   # protected
 
