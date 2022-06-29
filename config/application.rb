5786e5c260ba464e364cc52c682bffb2a6c9af3e
Thomas Leese <thomas@leese.io>
2022-06-29 11:26:30 +0100

    Specify ActionMailer Sidekiq queue
    We want emails to go out on the `mailer` queue so we have better
    visibility of those jobs. It applies to both the `production` and
    `development` environment so I've put it in the global configuration.

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,7 @@ module ApplyForQualifiedTeacherStatus
     config.assets.paths << Rails.root.join(
       "node_modules/govuk-frontend/govuk/assets"
     )
+
+    config.action_mailer.deliver_later_queue_name = "mailer"
   end
 end
