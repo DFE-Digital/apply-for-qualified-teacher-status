1ddf5f1c840f31ecadb7101dc43e74f10f335a77
Thomas Leese <thomas@leese.io>
2022-06-29 11:27:05 +0100

    Send emails in development
    This allows us to read the contents of emails which have been sent using
    the application, which helps with sign in, magic links and email
    confirmation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :file
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
