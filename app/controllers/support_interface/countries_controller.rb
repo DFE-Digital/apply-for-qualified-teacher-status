9a48998311f353e1fa5f233685c8b6a395d09f61
Thomas Leese <thomas@leese.io>
2022-07-04 10:19:23 +0100

    Add route for confirming region changes
    This loads the changes and performs them against the database, resulting
    in a success banner to let the user know the changes have been made.

--- app/controllers/support_interface/countries_controller.rb
+++ app/controllers/support_interface/countries_controller.rb
@@ -1,4 +1,6 @@
 class SupportInterface::CountriesController < SupportInterface::BaseController
+  before_action :load_country_and_edit_actions, only: %w[confirm_edit update]
+
   def index
     @countries = Country.includes(:regions)
   end
@@ -9,16 +11,32 @@ class SupportInterface::CountriesController < SupportInterface::BaseController
   end
 
   def confirm_edit
-    @country = Country.includes(:regions).find(params[:id])
-    @all_regions = (params.dig(:country, :all_regions) || "").chomp
-    @diff_actions = calculate_diff_actions
-
     if @diff_actions.empty?
       redirect_to edit_support_interface_country_path(@country)
     end
   end
 
+  def update
+    @diff_actions.each do |action|
+      case action[:action]
+      when :create
+        @country.regions.create!(name: action[:name])
+      when :delete
+        @country.regions.find_by!(name: action[:name]).destroy!
+      end
+    end
+
+    flash[:success] = "Successfully updated #{@country.name}"
+    redirect_to support_interface_countries_path
+  end
+
   private
+
+  def load_country_and_edit_actions
+    @country = Country.includes(:regions).find(params[:id])
+    @all_regions = (params.dig(:country, :all_regions) || "").chomp
+    @diff_actions = calculate_diff_actions
+  end
 
   def calculate_diff_actions
     current_region_names = @country.regions.map(&:name)
