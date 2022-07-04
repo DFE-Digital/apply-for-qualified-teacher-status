31f1936799b1c2afb2a364fbc4d15f7cdb9c9e24
Thomas Leese <thomas@leese.io>
2022-07-04 10:11:05 +0100

    Add a page for confirm region changes
    Before a user can actually make changes to the regions for a country, we
    should have a page that shows which changes are going to be made and
    allow a user to confirm.

--- app/controllers/support_interface/countries_controller.rb
+++ app/controllers/support_interface/countries_controller.rb
@@ -7,4 +7,34 @@ class SupportInterface::CountriesController < SupportInterface::BaseController
     @country = Country.includes(:regions).find(params[:id])
     @all_regions = @country.regions.map(&:name).join("\n")
   end
+
+  def confirm_edit
+    @country = Country.includes(:regions).find(params[:id])
+    @all_regions = (params.dig(:country, :all_regions) || "").chomp
+    @diff_actions = calculate_diff_actions
+
+    if @diff_actions.empty?
+      redirect_to edit_support_interface_country_path(@country)
+    end
+  end
+
+  private
+
+  def calculate_diff_actions
+    current_region_names = @country.regions.map(&:name)
+    new_region_names = @all_regions.split("\n").map(&:chomp)
+    new_region_names = [""] if new_region_names.empty?
+
+    regions_to_delete = current_region_names - new_region_names
+    regions_to_create = new_region_names - current_region_names
+
+    delete_actions = regions_to_delete.map { |name| { action: :delete, name: } }
+    create_actions = regions_to_create.map { |name| { action: :create, name: } }
+
+    (delete_actions + create_actions).sort_by { |action| action[:name] }
+  end
+
+  def country_params
+    params.require(:country).permit(:all_regions)
+  end
 end
