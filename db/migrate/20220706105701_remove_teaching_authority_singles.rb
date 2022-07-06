eb5b015c18038cc2e7c1382ce18833cd35243d44
Thomas Leese <thomas@leese.io>
2022-07-06 12:01:14 +0100

    Remove single teaching authority columns
    We're not longer using them since
    https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/207
    has been deployed.

---
+++ db/migrate/20220706105701_remove_teaching_authority_singles.rb
@@ -0,0 +1,17 @@
+class RemoveTeachingAuthoritySingles < ActiveRecord::Migration[7.0]
+  def up
+    change_table :regions, bulk: true do |t|
+      t.remove :teaching_authority_website, :teaching_authority_email_address
+    end
+  end
+
+  def down
+    change_table :regions, bulk: true do |t|
+      t.column :teaching_authority_website, :text, default: "", null: false
+      t.column :teaching_authority_email_address,
+               :text,
+               default: "",
+               null: false
+    end
+  end
+end
