d661fb4f6370bd0200c763bed6910c6b60950509
Thomas Leese <thomas@leese.io>
2022-07-11 13:37:40 +0100

    Add teaching authority contactable
    This represents something that holds a teaching authority which can be
    contacted. For now this is only regions, but we're going to be extending
    this to countries.

---
+++ app/models/concerns/teaching_authority_contactable.rb
@@ -0,0 +1,21 @@
+module TeachingAuthorityContactable
+  extend ActiveSupport::Concern
+
+  def teaching_authority_emails_string
+    teaching_authority_emails.join("\n")
+  end
+
+  def teaching_authority_emails_string=(string)
+    self.teaching_authority_emails =
+      string.split("\n").map(&:chomp).compact_blank
+  end
+
+  def teaching_authority_websites_string
+    teaching_authority_websites.join("\n")
+  end
+
+  def teaching_authority_websites_string=(string)
+    self.teaching_authority_websites =
+      string.split("\n").map(&:chomp).compact_blank
+  end
+end
