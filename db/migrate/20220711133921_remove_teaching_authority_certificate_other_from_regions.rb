2394f822b2484a6e191d7d0338d9f90f0c17fb57
Thomas Leese <thomas@leese.io>
2022-07-11 14:43:12 +0100

    Remove teaching authority fields from regions
    This removes the "other" and "certificate" teaching authority fields
    from regions as they're now fetched from the country.

---
+++ db/migrate/20220711133921_remove_teaching_authority_certificate_other_from_regions.rb
@@ -0,0 +1,16 @@
+class RemoveTeachingAuthorityCertificateOtherFromRegions < ActiveRecord::Migration[
+  7.0
+]
+  def up
+    change_table :regions, bulk: true do |t|
+      t.remove :teaching_authority_certificate, :teaching_authority_other
+    end
+  end
+
+  def down
+    change_table :regions, bulk: true do |t|
+      t.column :teaching_authority_certificate, :text, default: "", null: false
+      t.column :teaching_authority_other, :text, default: "", null: false
+    end
+  end
+end
