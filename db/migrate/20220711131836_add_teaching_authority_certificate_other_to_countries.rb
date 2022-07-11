1ad823c6a8623fd90d43b11bd4c7e733b48c1998
Thomas Leese <thomas@leese.io>
2022-07-11 14:23:39 +0100

    Add certificate and other field to countries
    This takes the existing fields from regions and adds them to the
    countries as we will be moving this value away from regions as they
    apply to the entire country.

---
+++ db/migrate/20220711131836_add_teaching_authority_certificate_other_to_countries.rb
@@ -0,0 +1,25 @@
+class AddTeachingAuthorityCertificateOtherToCountries < ActiveRecord::Migration[
+  7.0
+]
+  def change
+    change_table :countries, bulk: true do |t|
+      t.column :teaching_authority_certificate, :text, default: "", null: false
+      t.column :teaching_authority_other, :text, default: "", null: false
+    end
+
+    regions_to_update =
+      Region
+        .includes(:country)
+        .where.not(teaching_authority_certificate: "")
+        .or(Region.where.not(teaching_authority_other: ""))
+
+    regions_to_update.find_each do |region|
+      region.country.update!(
+        {
+          teaching_authority_certificate: region.teaching_authority_certificate,
+          teaching_authority_other: region.teaching_authority_other
+        }.compact_blank
+      )
+    end
+  end
+end
