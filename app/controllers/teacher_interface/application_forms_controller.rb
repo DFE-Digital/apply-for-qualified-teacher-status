fe9467b420c165555b7298614ecd5e1c26430374
Thomas Leese <thomas@leese.io>
2022-07-13 14:33:56 +0100

    Add basic application show and index pages
    This for now contains a simple table showing a users applications and a
    continue button that allows a user to continue an ongoing application.

--- app/controllers/teacher_interface/application_forms_controller.rb
+++ app/controllers/teacher_interface/application_forms_controller.rb
@@ -1,10 +1,30 @@
 class TeacherInterface::ApplicationFormsController < TeacherInterface::BaseController
   def index
-  end
-
-  def new
+    @application_forms =
+      ApplicationForms.where(teacher: current_teacher).order(created_at: :desc)
   end
 
   def create
+    @application_form = ApplicationForm.new
+    if @application_form.update(application_form_params)
+      redirect_to teacher_interface_application_form_path(@application_form)
+    else
+      flash[:warning] = "Could not start application."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @application_form =
+      ApplicationForm.where(teacher: current_teacher).find(params[:id])
+  end
+
+  private
+
+  def application_form_params
+    {
+      teacher: current_teacher,
+      eligibility_check_id: session[:eligibility_check_id]
+    }
   end
 end
