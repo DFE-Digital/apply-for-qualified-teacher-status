6e97cd2669d434e8a8e1324995c86d7f56ea3461
Thomas Leese <thomas@leese.io>
2022-07-15 13:19:17 +0100

    Add personal information fields
    This adds a `given_names`, `family_name` and `date_of_birth` field to
    the application form model so we can start to capture this information.

---
+++ db/migrate/20220715115640_add_personal_information_to_application_forms.rb
@@ -0,0 +1,9 @@
+class AddPersonalInformationToApplicationForms < ActiveRecord::Migration[7.0]
+  def change
+    change_table :application_forms, bulk: true do |t|
+      t.column :given_names, :text, default: "", null: false
+      t.column :family_name, :text, default: "", null: false
+      t.column :date_of_birth, :date
+    end
+  end
+end
