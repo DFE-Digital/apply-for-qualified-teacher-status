fbacfec5f4eba4d39327b7d5d7d8a0e84b4eaafd
Thomas Leese <thomas@leese.io>
2022-07-18 13:39:32 +0100

    Add ValidForNotifyValidator
    This validates that the field looks like an email addres which can be
    sent using GOV.UK Notify.
    
    Copied from: https://github.com/DFE-Digital/apply-for-teacher-training/blob/aa087f76aa32489b833e9d683fa696a45c0ea16e/app/validators/valid_for_notify_validator.rb

---
+++ app/validators/valid_for_notify_validator.rb
@@ -0,0 +1,22 @@
+class ValidForNotifyValidator < ActiveModel::EachValidator
+  NUMBERS_AND_LETTERS = "a-zA-Z0-9".freeze
+  CHINESE_JAPANESE_AND_KOREAN_CHARS =
+    '\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B'.freeze
+  ALPHANUMERIC = NUMBERS_AND_LETTERS
+  EMAIL_REGEX =
+    %r{\A[#{ALPHANUMERIC}.!\#$%&'*+/=?^_`{|}~-] # Local part
+                  +@[#{ALPHANUMERIC}](?:[#{ALPHANUMERIC}-] # Domain name
+                  {0,61}[#{ALPHANUMERIC}])?(?:\. # Allow periods in domain name
+                  [#{ALPHANUMERIC}](?:[#{ALPHANUMERIC}-]{0,61}[#{ALPHANUMERIC}])?)*\.
+                  [#{ALPHANUMERIC}](?:[#{ALPHANUMERIC}-]{0,61} # # End of domain
+                  [#{ALPHANUMERIC}])\z}x
+
+  def validate_each(record, attribute, value)
+    if value.blank? || !value.match?(EMAIL_REGEX)
+      record.errors.add(
+        attribute,
+        "Enter an email address in the correct format, like name@example.com"
+      )
+    end
+  end
+end
