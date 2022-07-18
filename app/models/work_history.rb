e5b14cbe8c1202fda204add80027681c95c4a686
Thomas Leese <thomas@leese.io>
2022-07-18 13:51:48 +0100

    Add WorkHistory model
    This will represent the work history of a teacher as they fill in the
    application form.

---
+++ app/models/work_history.rb
@@ -0,0 +1,63 @@
+# == Schema Information
+#
+# Table name: work_histories
+#
+#  id                  :bigint           not null, primary key
+#  city                :text             default(""), not null
+#  country             :text             default(""), not null
+#  email               :text             default(""), not null
+#  end_date            :date
+#  job                 :text             default(""), not null
+#  school_name         :text             default(""), not null
+#  start_date          :date
+#  still_employed      :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  application_form_id :bigint           not null
+#
+# Indexes
+#
+#  index_work_histories_on_application_form_id  (application_form_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (application_form_id => application_forms.id)
+#
+class WorkHistory < ApplicationRecord
+  include DfE::Analytics::Entities
+
+  belongs_to :application_form
+
+  validates :email, valid_for_notify: true, allow_blank: true
+  validates :end_date, presence: true, allow_nil: true, unless: :still_employed?
+
+  scope :completed,
+        -> {
+          where
+            .not(school_name: "", city: "", country: "", job: "", email: "")
+            .where.not(start_date: nil)
+            .where(still_employed: true)
+            .or(where(still_employed: false).where.not(end_date: nil))
+        }
+
+  def status
+    values = [
+      school_name,
+      city,
+      country,
+      job,
+      email,
+      start_date,
+      still_employed
+    ]
+
+    if still_employed == false
+      values.pop
+      values.append(end_date)
+    end
+
+    return :not_started if values.all?(&:blank?)
+    return :completed if values.all?(&:present?)
+    :in_progress
+  end
+end
---
+++ spec/factories/work_histories.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: work_histories
+#
+#  id                  :bigint           not null, primary key
+#  city                :text             default(""), not null
+#  country             :text             default(""), not null
+#  email               :text             default(""), not null
+#  end_date            :date
+#  job                 :text             default(""), not null
+#  school_name         :text             default(""), not null
+#  start_date          :date
+#  still_employed      :boolean
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  application_form_id :bigint           not null
+#
+# Indexes
+#
+#  index_work_histories_on_application_form_id  (application_form_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (application_form_id => application_forms.id)
+#
+FactoryBot.define do
+  factory :work_history do
+    association :application_form
+
+    trait :completed do
+      school_name { "School" }
+      city { "City" }
+      country { "Country" }
+      job { "Job" }
+      email { "school@example.com" }
+      start_date { Date.new(2020, 1, 1) }
+      still_employed { true }
+    end
+  end
+end
