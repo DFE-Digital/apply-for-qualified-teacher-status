ab67df6c0397cc7b895aa97c003728634fa487db
Thomas Leese <thomas@leese.io>
2022-07-20 13:56:52 +0100

    Add check your answers for personal information
    This moves the logic related to personal information controller in to
    its own controller so we can better display a "Check your answers" page
    rather than polluting the aplpication forms controller with lots of
    methods.

--- app/controllers/teacher_interface/base_controller.rb
+++ app/controllers/teacher_interface/base_controller.rb
@@ -4,4 +4,11 @@ class TeacherInterface::BaseController < ApplicationController
   layout "two_thirds"
 
   before_action :authenticate_teacher!
+
+  def load_application_form
+    @application_form =
+      ApplicationForm.where(teacher: current_teacher).find(
+        params[:application_form_id] || params[:id]
+      )
+  end
 end
