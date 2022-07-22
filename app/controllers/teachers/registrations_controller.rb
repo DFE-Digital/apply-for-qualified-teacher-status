c1d9df8ac29ae3300c6685cff082c9b1e7b5f056
Thomas Leese <thomas@leese.io>
2022-07-22 09:43:43 +0100

    Remove registration edit page
    This comes with Devise, but we don't need the functionality.

--- app/controllers/teachers/registrations_controller.rb
+++ app/controllers/teachers/registrations_controller.rb
@@ -5,14 +5,6 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
 
   layout "two_thirds"
 
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
   def create
     if (resource = Teacher.find_by(email: sign_up_params[:email]))
       if resource.active_for_authentication?
@@ -27,29 +19,9 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+  def update
+    redirect_to new_teacher_session_path
+  end
 
   protected
 
