bdabdd9cac02a818d09723268799701a82065c02
Thomas Leese <thomas@leese.io>
2022-07-25 13:59:42 +0100

    Create documents table
    This adds a migration which creates the table which we'll be using to
    store a reference to an uploaded document, potentially containing
    multiple files.

---
+++ db/migrate/20220725125324_create_documents.rb
@@ -0,0 +1,11 @@
+class CreateDocuments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :documents do |t|
+      t.string :type, null: false
+      t.references :documentable, polymorphic: true
+      t.timestamps
+    end
+
+    add_index :documents, :type
+  end
+end
