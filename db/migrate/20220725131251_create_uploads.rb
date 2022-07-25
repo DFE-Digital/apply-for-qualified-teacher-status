71be9455b2d5e2aea3effb69481866d8e666148e
Thomas Leese <thomas@leese.io>
2022-07-25 14:15:55 +0100

    Create uploads table
    This adds a migration which creates the table which we'll be using to
    a reference to an uploaded document, and whether it's an original or a
    translation.

---
+++ db/migrate/20220725131251_create_uploads.rb
@@ -0,0 +1,9 @@
+class CreateUploads < ActiveRecord::Migration[7.0]
+  def change
+    create_table :uploads do |t|
+      t.references :document, null: false, foreign_key: true
+      t.boolean :translation, null: false
+      t.timestamps
+    end
+  end
+end
