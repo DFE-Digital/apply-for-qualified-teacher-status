5e4ce3bc86a29d72cf300007515cf89691fc6fd5
Thomas Leese <thomas@leese.io>
2022-07-26 10:44:23 +0100

    Add CheckYourAnswersSummaryComponent
    This is a component we can use to render the summary list card on each
    "Check your answers" page, and on the overall "Check your answers" page.
    We can't use the govuk-components helper for this as it doesn't yet
    support the summary card styles.

---
+++ app/components/check_your_answers_summary_component.rb
@@ -0,0 +1,37 @@
+class CheckYourAnswersSummaryComponent < ViewComponent::Base
+  def initialize(model:, title:, fields:)
+    super
+    @model = model
+    @title = title
+    @fields = fields
+  end
+
+  attr_reader :title
+
+  def rows
+    fields.map { |key, opts| opts ? row_for_field(key, opts) : nil }.compact
+  end
+
+  private
+
+  def row_for_field(key, opts)
+    {
+      key: opts.fetch(:key, key.to_s.humanize),
+      value: format_value(model.send(key), opts),
+      href: opts.fetch(:href)
+    }
+  end
+
+  def format_value(value, opts)
+    return "" if value.nil?
+
+    if value.is_a?(Date)
+      format = opts[:format] == :without_day ? "%B %Y" : "%e %B %Y"
+      return value.strftime(format).strip
+    end
+
+    value.to_s
+  end
+
+  attr_reader :model, :fields
+end
