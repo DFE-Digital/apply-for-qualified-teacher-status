caad6a51b3949e32752e6796fc3163f7f4660215
Thomas Leese <thomas@leese.io>
2022-07-26 13:52:57 +0100

    Add NameAndDateOfBirthForm
    This is a form for the first section of the personal information
    question, and the refactor means we're using forms in all places.

--- app/controllers/teacher_interface/personal_information_controller.rb
+++ app/controllers/teacher_interface/personal_information_controller.rb
@@ -1,42 +1,55 @@
-class TeacherInterface::PersonalInformationController < TeacherInterface::BaseController
-  before_action :load_application_form
+module TeacherInterface
+  class PersonalInformationController < BaseController
+    before_action :load_application_form
 
-  def show
-    unless application_form.subsection_started?(
-             :about_you,
-             :personal_information
-           )
-      redirect_to [
-                    :edit,
-                    :teacher_interface,
-                    application_form,
-                    :personal_information
-                  ]
+    def show
+      unless application_form.subsection_started?(
+               :about_you,
+               :personal_information
+             )
+        redirect_to [
+                      :edit,
+                      :teacher_interface,
+                      application_form,
+                      :personal_information
+                    ]
+      end
     end
-  end
 
-  def edit
-  end
-
-  def update
-    if application_form.update(personal_information_params)
-      redirect_to_if_save_and_continue [
-                                         :teacher_interface,
-                                         application_form,
-                                         :personal_information
-                                       ]
-    else
-      render :edit, status: :unprocessable_entity
+    def edit
+      @name_and_date_of_birth_form =
+        NameAndDateOfBirthForm.new(
+          application_form:,
+          given_names: application_form.given_names,
+          family_name: application_form.family_name,
+          date_of_birth: application_form.date_of_birth
+        )
     end
-  end
 
-  private
+    def update
+      @name_and_date_of_birth_form =
+        NameAndDateOfBirthForm.new(
+          name_and_date_of_birth_params.merge(application_form:)
+        )
+      if @name_and_date_of_birth_form.save
+        redirect_to_if_save_and_continue [
+                                           :teacher_interface,
+                                           application_form,
+                                           :personal_information
+                                         ]
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
 
-  def personal_information_params
-    params.require(:application_form).permit(
-      :given_names,
-      :family_name,
-      :date_of_birth
-    )
+    private
+
+    def name_and_date_of_birth_params
+      params.require(:teacher_interface_name_and_date_of_birth_form).permit(
+        :given_names,
+        :family_name,
+        :date_of_birth
+      )
+    end
   end
 end
