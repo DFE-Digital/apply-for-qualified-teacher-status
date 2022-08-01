6752c1a519952875504f0a1b958e9538116e77ce
Graham Pengelly <graham@three-tier.com>
2022-08-01 11:59:51 +0100

    Add basic smoke test
    * Spec to run through the eligibility checker flow as a smoke test
    * Add bin/smoke
    * Modify bin/test to exclude the smoke test

---
+++ spec/system/smoke_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+require "spec_helper"
+require "capybara/rspec"
+require "capybara/cuprite"
+
+Capybara.javascript_driver = :cuprite
+
+describe "Smoke test", type: :system, js: true, smoke_test: true do
+  before do
+    page.driver.basic_authorize(
+      ENV["SUPPORT_USERNAME"],
+      ENV["SUPPORT_PASSWORD"]
+    )
+  end
+
+  it "runs" do
+    when_i_visit_the_service_domain
+    and_i_click_the_start_button
+    and_i_enter_a_country
+    and_i_select_a_state
+    and_i_have_a_teaching_qualification
+    and_i_have_a_university_degree
+    and_i_am_qualified_to_teach
+    and_i_dont_have_sanctions
+    then_i_should_be_eligible_to_apply
+  end
+
+  private
+
+  def when_i_visit_the_service_domain
+    page.visit(ENV["HOSTING_DOMAIN"])
+  end
+
+  def and_i_click_the_start_button
+    click_button("Start now")
+  end
+
+  def and_i_enter_a_country
+    fill_in "eligibility-interface-country-form-location-field", with: "Canada"
+    continue
+  end
+
+  def and_i_select_a_state
+    choose "Ontario", visible: false
+    continue
+  end
+
+  def and_i_have_a_teaching_qualification
+    choose "Yes", visible: false
+    continue
+  end
+
+  def and_i_have_a_university_degree
+    choose "Yes", visible: false
+    continue
+  end
+
+  def and_i_am_qualified_to_teach
+    choose "Yes", visible: false
+    continue
+  end
+
+  def and_i_dont_have_sanctions
+    choose "No", visible: false
+    continue
+  end
+
+  def then_i_should_be_eligible_to_apply
+    expect(page).to have_content("You’re eligible to apply")
+  end
+
+  private
+
+  def continue
+    click_button("Continue")
+  end
+end
