301eab4195b2e0cb3513bb5eea79541895393cf6
Thomas Leese <thomas@leese.io>
2022-08-02 10:45:59 +0100

    Fix use of association in migration
    We can't rely on the association here as it may have been removed from
    the models when the migration runs. Instead we need to be explicit about
    fetching the related model.
    
    We don't need to use a join here as in all environments except dev,
    there aren't any application forms.

--- db/migrate/20220801091806_add_region_to_application_form.rb
+++ db/migrate/20220801091806_add_region_to_application_form.rb
@@ -2,13 +2,11 @@ class AddRegionToApplicationForm < ActiveRecord::Migration[7.0]
   def up
     add_reference :application_forms, :region, foreign_key: true, null: true
 
-    ApplicationForm
-      .includes(:eligibility_check)
-      .find_each do |application_form|
-        application_form.update!(
-          region_id: application_form.eligibility_check.region.id
-        )
-      end
+    ApplicationForm.find_each do |application_form|
+      eligibility_check =
+        EligibilityCheck.find(application_form.eligibility_check_id)
+      application_form.update!(region_id: eligibility_check.region_id)
+    end
 
     change_column_null :application_forms, :region_id, false
   end
