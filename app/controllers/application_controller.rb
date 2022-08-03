234339d44a627af4fa29172de30f5dea20f1016c
Thomas Leese <thomas@leese.io>
2022-08-03 11:00:06 +0100

    Prepend protect from forgery
    This is necessary when we use Devise to authenticate as a staff user
    as `authenticate_staff!` is called before the forgery protection is
    called and this leads to CSRF errors.
    
    It's noted in the Devise documentation:
    https://github.com/heartcombo/devise#controller-filters-and-helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   include DfE::Analytics::Requests
 
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
+
   before_action :authenticate_staff!,
                 unless: -> { FeatureFlag.active?(:service_open) }
+  protect_from_forgery prepend: true,
+                       unless: -> { FeatureFlag.active?(:service_open) }
 
   def current_user
     nil
