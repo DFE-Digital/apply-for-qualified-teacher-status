2ec2482a41d5d6044a077417aeea74922cf09fe6
Thomas Leese <thomas@leese.io>
2022-08-03 13:46:36 +0100

    Refactor how we authenticate as a test user
    This partially reverts a14b7b587b47abf905f7fae6beaeb3609b3dd2f7 and
    28a9936571e1f406bdd6a3a84cbfc5557952eb74 to try and solve an issue where
    Devise seems to be affecting the session, which means that when the
    service is not open it's not possible to complete an eligibility check.
    
    This refactors how we authenticate as a test user to instead not use
    Devise in the base application controller and stick with simple Rails
    HTTP Basic authentication. Some benefits of this is that we don't need
    to get Devise involved for the eligibility check, and it simplifies the
    staff auth strategy. It also means that if someone were to authenticate
    using the test credentials, they wouldn't get access to the support
    interface (if we accidentally left the feature flag on for example).
    
    There is a downside which is that authentication as a support user is
    duplicated in two places, but I think that's an okay trade-off. However,
    I do like that it restricts the staff authentication strategy to just
    authenticating as a staff user, with only access to the support
    interface.

--- app/lib/staff_http_basic_auth_strategy.rb
+++ app/lib/staff_http_basic_auth_strategy.rb
@@ -32,27 +32,13 @@ class StaffHttpBasicAuthStrategy < Warden::Strategies::Base
   SUPPORT_PASSWORD =
     Digest::SHA256.hexdigest(ENV.fetch("SUPPORT_PASSWORD", "support"))
 
-  TEST_USERNAME = Digest::SHA256.hexdigest(ENV.fetch("TEST_USERNAME", "test"))
-  TEST_PASSWORD = Digest::SHA256.hexdigest(ENV.fetch("TEST_PASSWORD", "test"))
-
   ANONYMOUS_SUPPORT_USER = AnonymousSupportUser.new
-
-  def valid_users
-    users = [{ username: SUPPORT_USERNAME, password: SUPPORT_PASSWORD }]
-
-    if FeatureFlag.active?(:staff_test_user)
-      users.push({ username: TEST_USERNAME, password: TEST_PASSWORD })
-    end
-    users
-  end
 
   def credentials_valid?(auth)
     return false unless auth.provided? && auth.basic? && auth.credentials
 
-    valid_users.any? do |user|
-      valid_comparison?(user[:username], auth.credentials.first) &&
-        valid_comparison?(user[:password], auth.credentials.last)
-    end
+    valid_comparison?(SUPPORT_USERNAME, auth.credentials.first) &&
+      valid_comparison?(SUPPORT_PASSWORD, auth.credentials.last)
   end
 
   def valid_comparison?(correct_value, given_value)
