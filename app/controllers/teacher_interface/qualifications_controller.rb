cef872698d99e18e45655629f3c2405f09bb9e8b
Thomas Leese <thomas@leese.io>
2022-08-03 10:37:40 +0100

    Add QualificationsController
    Since there can be more than one qualification we need a dedicated
    controller for this model.

---
+++ app/controllers/teacher_interface/qualifications_controller.rb
@@ -0,0 +1,21 @@
+module TeacherInterface
+  class QualificationsController < BaseController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
