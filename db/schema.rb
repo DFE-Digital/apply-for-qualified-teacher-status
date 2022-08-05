ba9e0a4d9614564ef472fd3e028a2e65228d664b
Thomas Leese <thomas@leese.io>
2022-08-05 07:20:08 +0100

    Add part_of_university_degree column
    This will be on qualifications recording the answer to the question
    about whether they need to add a degree.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_085227) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_05_061904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,6 +119,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_085227) do
     t.date "certificate_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "part_of_university_degree"
     t.index ["application_form_id"], name: "index_qualifications_on_application_form_id"
   end
 
