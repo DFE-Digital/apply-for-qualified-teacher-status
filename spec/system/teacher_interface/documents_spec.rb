f8a4b7dc0f1ae08411383e6426a40a284384909c
Thomas Leese <thomas@leese.io>
2022-08-08 13:18:15 +0100

    Fix content for second page upload form
    This makes it clearer to users what they're doing at this point and
    doesn't make it seem like they're stuck in a loop.

--- spec/system/teacher_interface/documents_spec.rb
+++ spec/system/teacher_interface/documents_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Teacher documents", type: :system do
 
     when_i_choose_yes
     and_i_click_continue
-    then_i_see_document_form
+    then_i_see_the_next_page_document_form
 
     when_i_upload_a_document
     and_i_click_continue
@@ -49,7 +49,7 @@ RSpec.describe "Teacher documents", type: :system do
   def when_i_upload_a_document
     attach_file "teacher-interface-upload-form-original-attachment-field",
                 Rails.root.join(file_fixture("upload.pdf"))
-    choose "No, I'll upload a translation as well", visible: false
+    choose "No, I’ll upload a translation as well", visible: false
     attach_file "teacher-interface-upload-form-translated-attachment-field",
                 Rails.root.join(file_fixture("upload.pdf"))
   end
@@ -61,6 +61,13 @@ RSpec.describe "Teacher documents", type: :system do
   def then_i_see_document_form
     expect(page).to have_title("Upload a document")
     expect(page).to have_content("Upload your written statement")
+  end
+
+  def then_i_see_the_next_page_document_form
+    expect(page).to have_title("Upload a document")
+    expect(page).to have_content(
+      "Upload the next page of your written statement document"
+    )
   end
 
   def then_i_see_the_check_your_uploaded_files_page
