aef06a968fd01c260c737b8fdc8f3cdeb362f6d7
Graham Pengelly <graham@three-tier.com>
2022-08-10 13:02:31 +0100

    Add ServicePattern
    Some common service boilerplate to standardise services to a static
    `.call` method with keyword args.

---
+++ app/lib/service_pattern.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module ServicePattern
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.class_eval { private_class_method(:new) }
+  end
+
+  def call
+    raise(NotImplementedError("#call must be implemented"))
+  end
+
+  module ClassMethods
+    def call(**args)
+      return new.call if args.empty?
+
+      new(**args).call
+    end
+  end
+end
