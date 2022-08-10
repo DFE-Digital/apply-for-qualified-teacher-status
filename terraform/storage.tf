b8fec960e3e8c6024be6f74567fd18f84b7037c6
Venkata Dorisala <venkata.dorisala@doris-tech.com>
2022-08-10 11:59:12 +0100

    Destroying review apps should destory storage as well.

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -15,10 +15,6 @@ resource "azurerm_storage_account" "forms" {
       days = var.forms_container_delete_retention_days
     }
   }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 
@@ -33,8 +29,4 @@ resource "azurerm_storage_container" "uploads" {
   name                  = "uploads"
   storage_account_name  = azurerm_storage_account.forms.name
   container_access_type = "private"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
