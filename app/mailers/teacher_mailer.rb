d2c20bd2fd2c75e53fb8bd2ad5dd5e34b9c39726
Thomas Leese <thomas@leese.io>
2022-08-15 13:45:31 +0100

    Add application received email
    This adds a new type of email to the teacher mailer which will be sent
    out when the application is submitted, sending a confirmation to the
    teacher.

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -1,2 +1,21 @@
 class TeacherMailer < ApplicationMailer
+  GOVUK_NOTIFY_TEMPLATE_ID =
+    ENV.fetch(
+      "GOVUK_NOTIFY_TEMPLATE_ID_TEACHER",
+      "95adafaf-0920-4623-bddc-340853c047af"
+    )
+
+  def application_received
+    teacher = params[:teacher]
+    application_form = teacher.application_form
+
+    @name = "#{application_form.given_names} #{application_form.family_name}"
+    @reference = application_form.reference
+
+    view_mail(
+      GOVUK_NOTIFY_TEMPLATE_ID,
+      to: teacher.email,
+      subject: I18n.t("mailer.teacher.application_received.subject")
+    )
+  end
 end
---
+++ spec/mailers/teacher_mailer_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe TeacherMailer, type: :mailer do
+  let(:teacher) { create(:teacher, email: "teacher@example.com") }
+  before do
+    create(
+      :application_form,
+      teacher:,
+      reference: "abc",
+      given_names: "First",
+      family_name: "Last"
+    )
+  end
+
+  describe "application_received" do
+    subject(:mail) { described_class.with(teacher:).application_received }
+
+    describe "#subject" do
+      subject(:subject) { mail.subject }
+
+      it do
+        is_expected.to eq(
+          "We’ve received your application for qualified teacher status (QTS)."
+        )
+      end
+    end
+
+    describe "#to" do
+      subject(:to) { mail.to }
+
+      it { is_expected.to eq(["teacher@example.com"]) }
+    end
+
+    describe "#body" do
+      subject(:body) { mail.body.encoded }
+
+      it { is_expected.to include("Dear First Last") }
+      it { is_expected.to include("abc") }
+      it { is_expected.to include("http://localhost:3000/teacher/sign_in") }
+    end
+  end
+end
