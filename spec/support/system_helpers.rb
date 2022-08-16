38401c9d39f6f13d22f204decfecd5991ea74251
Thomas Leese <thomas@leese.io>
2022-08-16 08:23:29 +0100

    Only take users to application form if it's enabled
    We want only certain regions/countries to be allowed to go through to
    the application form when we launch, so we need to take this value in to
    account when rendering the button.

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -14,6 +14,7 @@ module SystemHelpers
   def given_an_eligible_eligibility_check(country_check:)
     country = create(:country, :with_national_region, code: "GB-SCT")
     country.regions.first.update!(
+      application_form_enabled: true,
       status_check: country_check,
       sanction_check: country_check
     )
