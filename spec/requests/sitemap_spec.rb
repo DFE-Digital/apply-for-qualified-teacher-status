ad29e07d7cd64ac44c62db8835a4f664225db8df
Thomas Leese <thomas@leese.io>
2022-08-17 08:51:51 +0100

    Add a simple test for sitemap
    This only checks that the request succeeds, but it's better than no test
    at all.

---
+++ spec/requests/sitemap_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "Sitemap", type: :request do
+  it "responds successfully" do
+    get "/sitemap.xml"
+
+    expect(response).to have_http_status(:ok)
+  end
+end
