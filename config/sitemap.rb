4abcd7d11feb65de0ad139a9edb30a1fb847c43e
Thomas Leese <thomas@leese.io>
2022-08-17 08:40:55 +0100

    Add sitemap configuration
    This adds the main entry points and static pages from the service in to
    the sitemap configuration. There isn't much in this file as there aren't
    many routes we'd like to expose to search engines as users should always
    go through the start page.

---
+++ config/sitemap.rb
@@ -0,0 +1,12 @@
+# Run `bundle exec rails sitemap:refresh` after making changes to this file.
+
+SitemapGenerator::Sitemap.default_host =
+  "https://apply-for-qts-in-england.education.gov.uk"
+SitemapGenerator::Sitemap.compress = false
+
+SitemapGenerator::Sitemap.create do # rubocop:disable Rails/SaveBang
+  add new_teacher_session_path, changefreq: :monthly
+  add accessibility_path, changefreq: :monthly
+  add cookies_path, changefreq: :monthly
+  add privacy_path, changefreq: :monthly
+end
