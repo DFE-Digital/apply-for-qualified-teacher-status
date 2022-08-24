dd7eedb7e4dbc2f38404e7182f40bab105875003
Thomas Leese <thomas@leese.io>
2022-08-24 09:18:46 +0100

    Add DescriptionListComponent
    This renders a defintion list using GOV.UK styles, based on the
    component in "find-teacher-training":
    https://github.com/DFE-Digital/find-teacher-training/blob/main/app/webpacker/styles/components/_description-list.scss
    
    It accepts the same data structure as the summary card component so the
    same data can be used in both components. I've decided _not_ to use
    slots as it feels unnecessary but we should refactor this to use slots
    in the future.

---
+++ spec/components/description_list_component_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe DescriptionListComponent, type: :component do
+  subject(:component) { render_inline(described_class.new(rows:)) }
+
+  let(:rows) do
+    [
+      { key: { text: "Label 1" }, value: { text: "Value 1" } },
+      { key: { text: "Label 2" }, value: { text: "Value 2" } }
+    ]
+  end
+
+  subject(:dl) { component.at_css("dl") }
+
+  it "has the class" do
+    expect(dl["class"]).to eq("app-description-list")
+  end
+
+  describe "first item" do
+    let(:dt) { component.css("dt")[0] }
+    let(:dd) { component.css("dd")[0] }
+
+    it "has the class" do
+      expect(dt["class"]).to eq("app-description-list__label")
+    end
+
+    it "has the label text" do
+      expect(dt.text.strip).to eq("Label 1")
+    end
+
+    it "has the value text" do
+      expect(dd.text.strip).to eq("Value 1")
+    end
+  end
+
+  describe "second item" do
+    let(:dt) { component.css("dt")[1] }
+    let(:dd) { component.css("dd")[1] }
+
+    it "has the class" do
+      expect(dt["class"]).to eq("app-description-list__label")
+    end
+
+    it "has the label text" do
+      expect(dt.text.strip).to eq("Label 2")
+    end
+
+    it "has the value text" do
+      expect(dd.text.strip).to eq("Value 2")
+    end
+  end
+end
