6ae601c27fec84562319fe5473f3cb88a6e272aa
Thomas Leese <thomas@leese.io>
2022-08-26 11:51:56 +0100

    Remove module from filter specs
    I don't think there's a big benefit to structuring the code like this,
    and it means we can reduce the indentation.

--- spec/lib/filters/assessor_spec.rb
+++ spec/lib/filters/assessor_spec.rb
@@ -2,78 +2,76 @@
 
 require "rails_helper"
 
-module Filters
-  RSpec.describe Assessor do
-    let(:assessor_one) { create(:staff) }
-    let(:assessor_two) { create(:staff) }
+RSpec.describe Filters::Assessor do
+  let(:assessor_one) { create(:staff) }
+  let(:assessor_two) { create(:staff) }
 
-    subject { described_class.apply(scope:, params:) }
+  subject { described_class.apply(scope:, params:) }
 
-    context "the params include assessor_id" do
-      describe "filtering 'assigned to'" do
-        let(:params) { { assessor_ids: assessor_one.id } }
-        let(:scope) { ApplicationForm.all }
-
-        let!(:included) { create(:application_form, assessor: assessor_one) }
-
-        let!(:filtered) { create(:application_form, assessor: assessor_two) }
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
-      end
-
-      describe "filtering 'reviewer'" do
-        let(:params) { { assessor_ids: assessor_one.id } }
-        let(:scope) { ApplicationForm.all }
-
-        let!(:included) { create(:application_form, reviewer: assessor_one) }
-
-        let!(:filtered) { create(:application_form, reviewer: assessor_two) }
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
-      end
-    end
-
-    context "the params include multiple assessor_ids" do
-      let(:params) { { assessor_ids: [assessor_one.id, assessor_two.id] } }
+  context "the params include assessor_id" do
+    describe "filtering 'assigned to'" do
+      let(:params) { { assessor_ids: assessor_one.id } }
       let(:scope) { ApplicationForm.all }
 
-      let!(:included) do
-        [
-          create(:application_form, assessor: assessor_one),
-          create(:application_form, assessor: assessor_two)
-        ]
-      end
+      let!(:included) { create(:application_form, assessor: assessor_one) }
 
-      let!(:filterd) do
-        create(:application_form)
-        create(:application_form, assessor: create(:staff))
-      end
+      let!(:filtered) { create(:application_form, assessor: assessor_two) }
 
       it "returns a filtered scope" do
-        expect(subject).to eq(included)
+        expect(subject).to eq([included])
       end
     end
 
-    context "the params include a blank string" do
-      let(:params) { { assessor_ids: [""] } }
-      let(:scope) { double }
+    describe "filtering 'reviewer'" do
+      let(:params) { { assessor_ids: assessor_one.id } }
+      let(:scope) { ApplicationForm.all }
 
-      it "returns the original scope" do
-        expect(subject).to eq(scope)
+      let!(:included) { create(:application_form, reviewer: assessor_one) }
+
+      let!(:filtered) { create(:application_form, reviewer: assessor_two) }
+
+      it "returns a filtered scope" do
+        expect(subject).to eq([included])
       end
     end
+  end
 
-    context "the params don't include :assessor_ids" do
-      let(:params) { {} }
-      let(:scope) { double }
+  context "the params include multiple assessor_ids" do
+    let(:params) { { assessor_ids: [assessor_one.id, assessor_two.id] } }
+    let(:scope) { ApplicationForm.all }
 
-      it "returns the original scope" do
-        expect(subject).to eq(scope)
-      end
+    let!(:included) do
+      [
+        create(:application_form, assessor: assessor_one),
+        create(:application_form, assessor: assessor_two)
+      ]
+    end
+
+    let!(:filtered) do
+      create(:application_form)
+      create(:application_form, assessor: create(:staff))
+    end
+
+    it "returns a filtered scope" do
+      expect(subject).to eq(included)
+    end
+  end
+
+  context "the params include a blank string" do
+    let(:params) { { assessor_ids: [""] } }
+    let(:scope) { double }
+
+    it "returns the original scope" do
+      expect(subject).to eq(scope)
+    end
+  end
+
+  context "the params don't include :assessor_ids" do
+    let(:params) { {} }
+    let(:scope) { double }
+
+    it "returns the original scope" do
+      expect(subject).to eq(scope)
     end
   end
 end
--- spec/lib/filters/name_spec.rb
+++ spec/lib/filters/name_spec.rb
@@ -2,126 +2,124 @@
 
 require "rails_helper"
 
-module Filters
-  RSpec.describe Name do
-    subject { described_class.apply(scope:, params:) }
+RSpec.describe Filters::Name do
+  subject { described_class.apply(scope:, params:) }
 
-    context "the params include :name" do
-      let(:params) { { name: "Dave" } }
-      let(:scope) { ApplicationForm.all }
+  context "the params include :name" do
+    let(:params) { { name: "Dave" } }
+    let(:scope) { ApplicationForm.all }
 
-      context "first name match" do
-        let!(:included) do
-          create(
-            :application_form,
-            :with_personal_information,
-            given_names: "Dave"
-          )
-        end
-        let!(:filtered) do
-          create(
-            :application_form,
-            :with_personal_information,
-            given_names: "Maude",
-            family_name: "Ling"
-          )
-        end
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
+    context "first name match" do
+      let!(:included) do
+        create(
+          :application_form,
+          :with_personal_information,
+          given_names: "Dave"
+        )
+      end
+      let!(:filtered) do
+        create(
+          :application_form,
+          :with_personal_information,
+          given_names: "Maude",
+          family_name: "Ling"
+        )
       end
 
-      context "first name partial match" do
-        let!(:included) do
-          create(
-            :application_form,
-            :with_personal_information,
-            given_names: "Cornishpasty Dave"
-          )
-        end
-        let!(:filtered) do
-          create(
-            :application_form,
-            :with_personal_information,
-            given_names: "Maude",
-            family_name: "Ling"
-          )
-        end
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
-      end
-
-      context "family name match" do
-        let!(:included) do
-          create(
-            :application_form,
-            :with_personal_information,
-            family_name: "Dave"
-          )
-        end
-        let!(:filtered) do
-          create(
-            :application_form,
-            :with_personal_information,
-            given_names: "Maude",
-            family_name: "Ling"
-          )
-        end
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
-      end
-
-      context "family name partial match" do
-        let!(:included) do
-          create(
-            :application_form,
-            :with_personal_information,
-            family_name: "Davethegangster"
-          )
-        end
-        let!(:filtered) do
-          create(
-            :application_form,
-            :with_personal_information,
-            given_names: "Maude",
-            family_name: "Ling"
-          )
-        end
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
-      end
-
-      context "match with different case" do
-        let(:params) { { name: "daVe" } }
-
-        let!(:included) do
-          create(
-            :application_form,
-            :with_personal_information,
-            family_name: "Dave"
-          )
-        end
-
-        it "returns a filtered scope" do
-          expect(subject).to eq([included])
-        end
+      it "returns a filtered scope" do
+        expect(subject).to eq([included])
       end
     end
 
-    context "the params don't include :name" do
-      let(:params) { {} }
-      let(:scope) { double }
-
-      it "returns the original scope" do
-        expect(subject).to eq(scope)
+    context "first name partial match" do
+      let!(:included) do
+        create(
+          :application_form,
+          :with_personal_information,
+          given_names: "Cornishpasty Dave"
+        )
       end
+      let!(:filtered) do
+        create(
+          :application_form,
+          :with_personal_information,
+          given_names: "Maude",
+          family_name: "Ling"
+        )
+      end
+
+      it "returns a filtered scope" do
+        expect(subject).to eq([included])
+      end
+    end
+
+    context "family name match" do
+      let!(:included) do
+        create(
+          :application_form,
+          :with_personal_information,
+          family_name: "Dave"
+        )
+      end
+      let!(:filtered) do
+        create(
+          :application_form,
+          :with_personal_information,
+          given_names: "Maude",
+          family_name: "Ling"
+        )
+      end
+
+      it "returns a filtered scope" do
+        expect(subject).to eq([included])
+      end
+    end
+
+    context "family name partial match" do
+      let!(:included) do
+        create(
+          :application_form,
+          :with_personal_information,
+          family_name: "Davethegangster"
+        )
+      end
+      let!(:filtered) do
+        create(
+          :application_form,
+          :with_personal_information,
+          given_names: "Maude",
+          family_name: "Ling"
+        )
+      end
+
+      it "returns a filtered scope" do
+        expect(subject).to eq([included])
+      end
+    end
+
+    context "match with different case" do
+      let(:params) { { name: "daVe" } }
+
+      let!(:included) do
+        create(
+          :application_form,
+          :with_personal_information,
+          family_name: "Dave"
+        )
+      end
+
+      it "returns a filtered scope" do
+        expect(subject).to eq([included])
+      end
+    end
+  end
+
+  context "the params don't include :name" do
+    let(:params) { {} }
+    let(:scope) { double }
+
+    it "returns the original scope" do
+      expect(subject).to eq(scope)
     end
   end
 end
