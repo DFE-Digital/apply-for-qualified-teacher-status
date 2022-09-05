73c06cd3402f6eb22d68608129bf3c28e3977e44
Thomas Leese <thomas@leese.io>
2022-09-05 19:29:16 +0100

    Add SubmittedAt filter
    This filters the application forms based on the submitted at value, so
    the user can enter a range of dates and the application forms will be
    filters to show those only within that range.

---
+++ app/lib/filters/submitted_at.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Filters
+  class SubmittedAt < Base
+    def apply
+      new_scope = scope
+
+      if submitted_at_after.present?
+        new_scope =
+          new_scope.where("DATE(submitted_at) >= ?", submitted_at_after)
+      end
+
+      if submitted_at_before.present?
+        new_scope =
+          new_scope.where("DATE(submitted_at) <= ?", submitted_at_before)
+      end
+
+      new_scope
+    end
+
+    private
+
+    def submitted_at_after
+      Date.new(
+        params["submitted_at_after(1i)"].to_i,
+        params["submitted_at_after(2i)"].to_i,
+        params["submitted_at_after(3i)"].to_i
+      )
+    rescue Date::Error
+      nil
+    end
+
+    def submitted_at_before
+      Date.new(
+        params["submitted_at_before(1i)"].to_i,
+        params["submitted_at_before(2i)"].to_i,
+        params["submitted_at_before(3i)"].to_i
+      )
+    rescue Date::Error
+      nil
+    end
+  end
+end
---
+++ spec/lib/filters/submitted_at_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Filters::SubmittedAt do
+  subject { described_class.apply(scope:, params:) }
+
+  context "with both submitted_at params " do
+    let(:params) do
+      {
+        "submitted_at_after(1i)" => "2020",
+        "submitted_at_after(2i)" => "01",
+        "submitted_at_after(3i)" => "01",
+        "submitted_at_before(1i)" => "2020",
+        "submitted_at_before(2i)" => "01",
+        "submitted_at_before(3i)" => "31"
+      }
+    end
+    let(:scope) { ApplicationForm.all }
+
+    let!(:included) do
+      create(:application_form, submitted_at: Date.new(2020, 1, 10))
+    end
+
+    let!(:filtered) do
+      create(:application_form, :submitted, submitted_at: Date.new(2019, 1, 1))
+      create(:application_form, :submitted, submitted_at: Date.new(2021, 1, 1))
+    end
+
+    it { is_expected.to eq([included]) }
+  end
+
+  context "with submitted_at before param" do
+    let(:params) do
+      {
+        "submitted_at_before(1i)" => "2020",
+        "submitted_at_before(2i)" => "01",
+        "submitted_at_before(3i)" => "01"
+      }
+    end
+    let(:scope) { ApplicationForm.all }
+
+    let!(:included) do
+      create(:application_form, submitted_at: Date.new(2020, 1, 1))
+    end
+
+    let!(:filtered) do
+      create(:application_form, :submitted, submitted_at: Date.new(2020, 1, 2))
+    end
+
+    it { is_expected.to eq([included]) }
+  end
+
+  context "with submitted_at after param" do
+    let(:params) do
+      {
+        "submitted_at_after(1i)" => "2020",
+        "submitted_at_after(2i)" => "01",
+        "submitted_at_after(3i)" => "02"
+      }
+    end
+    let(:scope) { ApplicationForm.all }
+
+    let!(:included) do
+      create(:application_form, submitted_at: Date.new(2020, 1, 2))
+    end
+
+    let!(:filtered) do
+      create(:application_form, :submitted, submitted_at: Date.new(2020, 1, 1))
+    end
+
+    it { is_expected.to eq([included]) }
+  end
+
+  context "without submitted_at params" do
+    let(:params) { {} }
+    let(:scope) { double }
+
+    it { is_expected.to eq(scope) }
+  end
+end
