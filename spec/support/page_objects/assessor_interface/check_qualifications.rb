c079dfa14ab66851259d3be41f72b78e8e0e8638
Graham Pengelly <graham@three-tier.com>
2022-09-08 12:04:10 +0100

    [843] Update qualifications page object
    Extend and refactor for new tests.

--- spec/support/page_objects/assessor_interface/check_qualifications.rb
+++ spec/support/page_objects/assessor_interface/check_qualifications.rb
@@ -2,6 +2,8 @@ module PageObjects
   module AssessorInterface
     class QualificationCard < SitePrism::Section
       element :heading, "h2"
+      element :title,
+              "dl.govuk-summary-list > div:nth-of-type(1) > dd:nth-of-type(1)"
     end
 
     class CheckQualifications < SitePrism::Page
@@ -12,6 +14,10 @@ module PageObjects
       sections :qualification_cards,
                QualificationCard,
                ".govuk-summary-list__card"
+
+      def teaching_qualification
+        qualification_cards&.first
+      end
     end
   end
 end
