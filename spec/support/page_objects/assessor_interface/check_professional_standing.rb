9e4610f3e1c8417721c40cf18a2849db2daf3879
Graham Pengelly <graham@three-tier.com>
2022-09-08 12:03:35 +0100

    [843] Update prof standing page object
    Extend and refactor for new tests.

--- spec/support/page_objects/assessor_interface/check_professional_standing.rb
+++ spec/support/page_objects/assessor_interface/check_professional_standing.rb
@@ -2,6 +2,8 @@ module PageObjects
   module AssessorInterface
     class ProfessionalStandingCard < SitePrism::Section
       element :heading, "h2"
+      element :reference_number,
+              "dl.govuk-summary-list > div:nth-of-type(1) > dd:nth-of-type(1)"
     end
 
     class CheckProfessionalStanding < SitePrism::Page
@@ -12,6 +14,10 @@ module PageObjects
       sections :professional_standing_cards,
                ProfessionalStandingCard,
                ".govuk-summary-list__card"
+
+      def proof_of_recognition
+        professional_standing_cards&.first
+      end
     end
   end
 end
