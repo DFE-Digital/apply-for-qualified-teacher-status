2c6b8433c9dfd3db4cbe04cfe2dce4ff94d6d6b7
Graham Pengelly <graham@three-tier.com>
2022-09-08 11:58:01 +0100

    [843] Add PageHeader section
    SitePrism::Section for the standard page header

---
+++ spec/support/page_objects/page_header.rb
@@ -0,0 +1,9 @@
+module PageObjects
+  class PageHeader < SitePrism::Section
+    element :search_link, "a.govuk-header__link", text: "Search"
+    element :sign_out_link, "a.govuk-header__link", text: "Sign out"
+    element :support_console_link,
+            "a.govuk-header__link",
+            text: "Support console"
+  end
+end
