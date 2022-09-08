4c99ec2044e791918fc40d7b6e4c5c8d43b86248
Graham Pengelly <graham@three-tier.com>
2022-09-08 13:57:27 +0100

    [843] Refactor authentication spec
    Update to use the PageObject pattern.

--- spec/system/assessor_interface/authentication_spec.rb
+++ spec/system/assessor_interface/authentication_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe "Assessor authentication", type: :system do
     given_the_service_is_open
     given_staff_exist
 
-    when_i_visit_the_assessor_page
-    then_i_see_the_sign_in_form
+    when_i_visit_the(:applications_page)
+    then_i_see_the(:login_page)
 
-    when_i_fill_in_the_sign_in_form
-    and_i_click_continue
+    when_i_login
+    then_i_see_the(:applications_page)
 
     when_i_click_sign_out
-    then_i_see_the_sign_in_form
+    then_i_see_the(:login_page)
     and_i_see_the_signed_out_message
   end
 
@@ -24,25 +24,15 @@ RSpec.describe "Assessor authentication", type: :system do
     create(:staff, :confirmed, email: "staff@example.com", password: "password")
   end
 
-  def when_i_visit_the_assessor_page
-    visit assessor_interface_root_path
-  end
-
-  def when_i_fill_in_the_sign_in_form
-    fill_in "staff-email-field", with: "staff@example.com"
-    fill_in "staff-password-field", with: "password"
+  def when_i_login
+    login_page.submit(email: "staff@example.com", password: "password")
   end
 
   def when_i_click_sign_out
-    click_link "Sign out"
-  end
-
-  def then_i_see_the_sign_in_form
-    expect(page).to have_content("Email")
-    expect(page).to have_content("Password")
+    applications_page.header.sign_out_link.click
   end
 
   def and_i_see_the_signed_out_message
-    expect(page).to have_content("Signed out successfully.")
+    expect(login_page.signed_out_message).to be_visible
   end
 end
