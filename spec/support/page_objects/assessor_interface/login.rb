27015ee40b56cae347c81368dde5912ba26398e1
Graham Pengelly <graham@three-tier.com>
2022-09-08 11:59:15 +0100

    [843] Add Login page object

---
+++ spec/support/page_objects/assessor_interface/login.rb
@@ -0,0 +1,23 @@
+module PageObjects
+  module AssessorInterface
+    class Login < SitePrism::Page
+      set_url "/staff/sign_in"
+
+      element :signed_out_message,
+              ".govuk-notification-banner__heading",
+              text: "Signed out successfully."
+
+      section :form, "form" do
+        element :email_field, "#staff-email-field"
+        element :password_field, "#staff-password-field"
+        element :continue_button, "button"
+      end
+
+      def submit(email:, password:)
+        form.email_field.fill_in with: email
+        form.password_field.fill_in with: password
+        form.continue_button.click
+      end
+    end
+  end
+end
