59228f844c3603d7a0956d9db274b609332da2a8
Graham Pengelly <graham@three-tier.com>
2022-09-08 12:01:19 +0100

    [843] Add assign assessor page object

---
+++ spec/support/page_objects/assessor_interface/assign_assessor.rb
@@ -0,0 +1,11 @@
+module PageObjects
+  module AssessorInterface
+    class AssignAssessor < SitePrism::Page
+      set_url "/assessor/applications/{application_id}/assign-assessor"
+
+      element :heading, "h1"
+      sections :assessors, PageObjects::GovukRadioItem, ".govuk-radios__item"
+      element :continue_button, "button"
+    end
+  end
+end
