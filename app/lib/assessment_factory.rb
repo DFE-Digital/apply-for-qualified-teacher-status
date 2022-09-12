33e483f922e12f2cb00508ddbfcd9916d262e95f
Graham Pengelly <graham@three-tier.com>
2022-09-12 16:54:45 +0100

    [853] Add qualifications checks & failures
    * Update `AssessmentFactory` to create the qualifications section with the
    checks and failure reasons
    * Add the translations for these to assessor_interface.en.yml

--- app/lib/assessment_factory.rb
+++ app/lib/assessment_factory.rb
@@ -9,7 +9,7 @@ class AssessmentFactory
 
   def call
     sections =
-      [personal_information_section] +
+      [personal_information_section, qualifications_section] +
         section_keys.map { |key| AssessmentSection.new(key:) }
     Assessment.create!(application_form:, sections:)
   end
@@ -19,7 +19,7 @@ class AssessmentFactory
   attr_reader :application_form
 
   def section_keys
-    %i[qualifications].tap do |keys|
+    [].tap do |keys|
       keys << :work_history if application_form.needs_work_history
       if application_form.needs_written_statement ||
            application_form.needs_registration_number
@@ -52,5 +52,35 @@ class AssessmentFactory
       checks:,
       failure_reasons:
     )
+  end
+
+  def qualifications_section
+    checks = %i[
+      qualifications_meet_level_6_or_equivalent
+      teaching_qualifcations_completed_in_eligible_country
+      qualified_in_mainstream_education
+      has_teacher_qualification_certificate
+      has_teacher_qualification_transcript
+      has_university_degree_certificate
+      has_university_degree_transcript
+      has_additional_qualification_certificate
+      has_additional_degree_transcript
+    ].compact
+
+    failure_reasons = %i[
+      teaching_qualifications_from_ineligible_country
+      teaching_qualifications_not_at_required_level
+      not_qualified_to_teach_mainstream
+      teaching_certificate_illegible
+      teaching_qualification_illegible
+      degree_certificate_illegible
+      degree_transcript_illegible
+      application_and_qualification_names_do_not_match
+      teaching_hours_not_fulfilled
+      qualifications_dont_support_subjects
+      qualifications_dont_match_those_entered
+    ].compact
+
+    AssessmentSection.new(key: "qualifications", checks:, failure_reasons:)
   end
 end
