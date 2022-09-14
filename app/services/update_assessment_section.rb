7d5ba97798e05642049432a201ef049f01cc5664
Graham Pengelly <graham@three-tier.com>
2022-09-14 16:06:12 +0100

    [890] Create Timeline event on section complete
    * When an assessment section is successfully completed create a timeline
      event that is associated with the section

--- app/services/update_assessment_section.rb
+++ app/services/update_assessment_section.rb
@@ -3,16 +3,31 @@
 class UpdateAssessmentSection
   include ServicePattern
 
-  def initialize(assessment_section:, params:)
+  def initialize(assessment_section:, user:, params:)
     @assessment_section = assessment_section
     @params = params
+    @user = user
   end
 
   def call
-    assessment_section.update(params)
+    create_timeline_event if (result = assessment_section.update(params))
+    result
   end
 
   private
 
-  attr_reader :assessment_section, :params
+  attr_reader :assessment_section, :user, :params
+
+  def create_timeline_event
+    TimelineEvent.create!(
+      creator: user,
+      event_type: :assessment_section_completed,
+      eventable: assessment_section,
+      application_form:
+    )
+  end
+
+  def application_form
+    assessment_section.assessment.application_form
+  end
 end
