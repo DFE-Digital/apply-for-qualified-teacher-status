41ff11df7b1f9ed405d1fd77184efd25b9709054
Thomas Leese <thomas@leese.io>
2022-09-27 07:22:51 +0100

    Add FurtherInformationRequestItemTextForm
    This will be used in the further information request item controller for
    items which are expecting a text response.

---
+++ spec/forms/teacher_interface/further_information_request_item_text_form.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe TeacherInterface::FurtherInformationRequestItemTextForm,
+               type: :model do
+  subject(:form) do
+    described_class.new(further_information_request_item:, response:)
+  end
+
+  let(:further_information_request_item) do
+    build(:further_information_request_item)
+  end
+  let(:response) { nil }
+
+  it { is_expected.to validate_presence_of(:further_information_request_item) }
+  it { is_expected.to validate_presence_of(:response) }
+
+  describe "#save" do
+    subject(:save) { form.save }
+
+    context "with a blank value" do
+      let(:response) { "" }
+
+      it { is_expected.to be false }
+    end
+
+    context "with a present value" do
+      let(:response) { "response" }
+
+      it { is_expected.to be true }
+    end
+  end
+
+  describe "#update_model" do
+    subject(:response) { further_information_request_item.response }
+
+    before { form.update_model }
+
+    context "with a blank value" do
+      let(:response) { "" }
+
+      it { is_expected.to eq("") }
+    end
+
+    context "with a present value" do
+      let(:response) { "response" }
+
+      it { is_expected.to eq("response") }
+    end
+  end
+end
