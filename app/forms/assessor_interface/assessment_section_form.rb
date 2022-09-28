e76382083a7db0cf1eac34bacd8b53e36ae0029d
Thomas Leese <thomas@leese.io>
2022-09-28 12:17:44 +0100

    Add AssessmentSectionForm
    This is a form which is generated dynamically to help render and
    validate the assessment section view where we expect users to start
    provided notes with their assessment.

---
+++ app/forms/assessor_interface/assessment_section_form.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+class AssessorInterface::AssessmentSectionForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attr_accessor :assessment_section, :user
+  validates :assessment_section, :user, presence: true
+
+  attribute :passed, :boolean
+  validates :passed, inclusion: [true, false]
+
+  validates :selected_failure_reasons,
+            presence: true,
+            if: -> { passed == false }
+
+  def selected_failure_reasons
+    return {} if passed
+
+    assessment_section
+      .failure_reasons
+      .each_with_object({}) do |failure_reason, memo|
+        if send("#{failure_reason}_checked")
+          memo[failure_reason] = send("#{failure_reason}_notes")
+        end
+      end
+  end
+
+  def selected_failure_reasons=(value)
+    value.each do |failure_reason, notes|
+      send("#{failure_reason}_checked=", true)
+      send("#{failure_reason}_notes=", notes)
+    end
+  end
+
+  def save
+    return false unless valid?
+
+    UpdateAssessmentSection.call(
+      assessment_section:,
+      user:,
+      params: {
+        passed:,
+        selected_failure_reasons:,
+      },
+    )
+
+    true
+  end
+
+  class << self
+    def for_assessment_section(assessment_section)
+      klass =
+        Class.new(self) do
+          def self.name
+            "AssessorInterface::AssessmentSectionForm"
+          end
+        end
+
+      assessment_section.failure_reasons.each do |failure_reason|
+        klass.attribute "#{failure_reason}_checked", :boolean
+        klass.attribute "#{failure_reason}_notes", :string
+        klass.validates "#{failure_reason}_notes",
+                        presence: true,
+                        if: :"#{failure_reason}_checked"
+      end
+
+      klass
+    end
+
+    def permit_parameters(params)
+      args =
+        attribute_names.filter { |attr_name| attr_name.ends_with?("_notes") }
+      kwargs =
+        attribute_names
+          .filter { |attr_name| attr_name.ends_with?("_checked") }
+          .index_with { |_key| [] }
+
+      params.permit(:passed, *args, **kwargs)
+    end
+  end
+end
---
+++ spec/forms/assessor_interface/assessment_section_form_spec.rb
@@ -0,0 +1,152 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AssessorInterface::AssessmentSectionForm, type: :model do
+  let(:assessment_section) do
+    create(
+      :assessment_section,
+      key: "personal_information",
+      failure_reasons: %i[reason_a reason_b reason_c],
+    )
+  end
+  let(:user) { create(:staff, :confirmed) }
+  let(:attributes) { {} }
+
+  subject(:form) do
+    described_class.for_assessment_section(assessment_section).new(
+      assessment_section:,
+      user:,
+      **attributes,
+    )
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:assessment_section) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to allow_values(true, false).for(:passed) }
+
+    context "when passed" do
+      let(:attributes) { { passed: true } }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when reasons are checked" do
+      let(:attributes) do
+        {
+          passed: false,
+          reason_a_checked: true,
+          reason_b_checked: true,
+          reason_c_checked: true,
+        }
+      end
+
+      it { is_expected.to validate_presence_of(:reason_a_notes) }
+      it { is_expected.to validate_presence_of(:reason_b_notes) }
+      it { is_expected.to validate_presence_of(:reason_c_notes) }
+    end
+
+    context "when reasons are checked and notes are provided" do
+      let(:attributes) do
+        {
+          passed: false,
+          reason_a_checked: true,
+          reason_a_notes: "Notes.",
+          reason_b_checked: true,
+          reason_b_notes: "Notes.",
+          reason_c_checked: true,
+          reason_c_notes: "Notes.",
+        }
+      end
+
+      it { is_expected.to be_valid }
+    end
+  end
+
+  describe "#selected_failure_reasons" do
+    subject(:selected_failure_reasons) { form.selected_failure_reasons }
+
+    context "when passed" do
+      let(:attributes) { { passed: true } }
+
+      it { is_expected.to eq({}) }
+    end
+
+    context "when failed" do
+      let(:attributes) do
+        { passed: false, reason_a_checked: true, reason_a_notes: "Notes." }
+      end
+
+      it { is_expected.to eq({ "reason_a" => "Notes." }) }
+    end
+  end
+
+  describe "#selected_failure_reasons=" do
+    before { form.selected_failure_reasons = { reason_a: "Notes." } }
+
+    it "sets the attributes" do
+      expect(form.reason_a_checked).to be true
+      expect(form.reason_a_notes).to eq("Notes.")
+    end
+  end
+
+  describe "#save" do
+    subject(:save) { form.save }
+
+    describe "when invalid attributes" do
+      it { is_expected.to be false }
+    end
+
+    describe "with valid attributes and passed" do
+      let(:attributes) do
+        { passed: true, reason_a_checked: true, reason_a_notes: "Notes." }
+      end
+
+      it { is_expected.to be true }
+
+      it "calls the update assessment service" do
+        expect(UpdateAssessmentSection).to receive(:call).with(
+          assessment_section:,
+          user:,
+          params: {
+            passed: true,
+            selected_failure_reasons: {
+            },
+          },
+        )
+
+        save # rubocop:disable Rails/SaveBang
+      end
+    end
+
+    describe "with valid attributes and failed" do
+      let(:attributes) do
+        {
+          passed: false,
+          reason_a_checked: true,
+          reason_a_notes: "Notes.",
+          reason_b_checked: false,
+          reason_c_checked: false,
+        }
+      end
+
+      it { is_expected.to be true }
+
+      it "calls the update assessment service" do
+        expect(UpdateAssessmentSection).to receive(:call).with(
+          assessment_section:,
+          user:,
+          params: {
+            passed: false,
+            selected_failure_reasons: {
+              "reason_a" => "Notes.",
+            },
+          },
+        )
+
+        save # rubocop:disable Rails/SaveBang
+      end
+    end
+  end
+end
