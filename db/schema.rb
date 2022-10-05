445977f96513aed61cb2b869215ae5a4d5a6f882
Thomas Leese <thomas@leese.io>
2022-10-05 09:10:39 +0100

    Remove email_content column
    This removes the column from the further information requests table as
    we're no longer using this for anything.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_132452) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_080511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -159,7 +159,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_132452) do
     t.datetime "received_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "email_content"
     t.index ["assessment_id"], name: "index_further_information_requests_on_assessment_id"
   end
 
