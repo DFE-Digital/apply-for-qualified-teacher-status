42d005ca0467dc9caa78582f25815bc64cbe9a10
Thomas Leese <thomas@leese.io>
2022-10-09 16:32:07 +0100

    Allow value in check your answers to be specified
    In case it needs to be overridden.

--- app/components/check_your_answers_summary/component.rb
+++ app/components/check_your_answers_summary/component.rb
@@ -32,8 +32,7 @@ module CheckYourAnswersSummary
     end
 
     def has_translation?(field)
-      object = model.send(field[:key])
-
+      object = value_for(field)
       return false unless object.is_a?(Document)
       object.translated_uploads.any?
     end
@@ -53,11 +52,15 @@ module CheckYourAnswersSummary
       fields.filter_map { |key, options| options&.merge(key:) }
     end
 
+    def value_for(field)
+      field[:value].presence || model.send(field[:key])
+    end
+
     def row_for_field(field)
       {
         key: field[:key],
         title: row_title_for(field),
-        value: format_value(model.send(field[:key]), field),
+        value: format_value(value_for(field), field),
         href: changeable ? field.fetch(:href) : nil,
       }
     end
