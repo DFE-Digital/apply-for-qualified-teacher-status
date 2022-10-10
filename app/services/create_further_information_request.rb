5d0434b6bd9112dacc485663bd8d6b9f9e899b7d
Thomas Leese <thomas@leese.io>
2022-10-10 12:48:46 +0100

    Add CreateFurtherInformationRequest
    This is a service which creates the further information request and
    associated actions, sending an email, updating the application form
    state, and creating the record.

---
+++ app/services/create_further_information_request.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class CreateFurtherInformationRequest
+  include ServicePattern
+
+  def initialize(assessment:, user:)
+    @assessment = assessment
+    @user = user
+  end
+
+  def call
+    further_information_request =
+      ActiveRecord::Base.transaction do
+        ChangeApplicationFormState.call(
+          application_form:,
+          user:,
+          new_state: :further_information_requested,
+        )
+
+        assessment.further_information_requests.create!(
+          items:
+            FurtherInformationRequestItemsFactory.call(
+              assessment_sections: assessment.sections,
+            ),
+          state: :requested,
+        )
+      end
+
+    TeacherMailer
+      .with(teacher:, further_information_request:)
+      .further_information_requested
+      .deliver_later
+  end
+
+  private
+
+  attr_reader :assessment, :user
+
+  def application_form
+    @application_form ||= assessment.application_form
+  end
+
+  def teacher
+    @teacher ||= application_form.teacher
+  end
+end
---
+++ spec/services/create_further_information_request_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe CreateFurtherInformationRequest do
+  let(:application_form) { create(:application_form, :submitted) }
+  let(:assessment) { create(:assessment, application_form:) }
+  let(:user) { create(:staff, :confirmed) }
+
+  subject(:call) { described_class.call(assessment:, user:) }
+
+  describe "creating further information request" do
+    subject(:further_information_request) do
+      FurtherInformationRequest.find_by(assessment:)
+    end
+
+    it { is_expected.to be_nil }
+
+    context "after calling the service" do
+      before { call }
+
+      it { is_expected.to_not be_nil }
+
+      it "sets the attributes correctly" do
+        expect(further_information_request.requested?).to be true
+      end
+    end
+  end
+
+  describe "updating application form state" do
+    subject(:state) { application_form.state }
+
+    it { is_expected.to eq("submitted") }
+
+    context "after calling the service" do
+      before { call }
+
+      it { is_expected.to eq("further_information_requested") }
+    end
+  end
+
+  describe "sending application received email" do
+    it "queues an email job" do
+      expect { call }.to have_enqueued_mail(
+        TeacherMailer,
+        :further_information_requested,
+      )
+    end
+  end
+end
