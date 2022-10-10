f06c267f0224760f39a9f8ebab1b2ce9be509aa6
Graham Pengelly <graham@three-tier.com>
2022-10-10 18:04:24 +0100

    [920] Require no sanctions confirmation
    * Add no sanctions confirmation check box to the application submit
      page.

--- app/controllers/teacher_interface/application_forms_controller.rb
+++ app/controllers/teacher_interface/application_forms_controller.rb
@@ -41,14 +41,27 @@ module TeacherInterface
     end
 
     def edit
+      @sanction_confirmation_form =
+        SanctionConfirmationForm.new(
+          application_form:,
+          confirmed_no_sanctions: application_form.confirmed_no_sanctions,
+        )
     end
 
     def update
-      if application_form.can_submit?
+      if (
+           @sanction_confirmation_form =
+             SanctionConfirmationForm.new(
+               application_form:,
+               confirmed_no_sanctions:
+                 application_form_params[:confirmed_no_sanctions],
+             )
+         ).save(validate: true)
         SubmitApplicationForm.call(application_form:, user: current_teacher)
+        redirect_to %i[teacher_interface application_form]
+      else
+        render :edit, status: :unprocessable_entity
       end
-
-      redirect_to %i[teacher_interface application_form]
     end
 
     private
@@ -57,6 +70,12 @@ module TeacherInterface
       params.require(:teacher_interface_country_region_form).permit(
         :location,
         :region_id,
+      )
+    end
+
+    def application_form_params
+      params.fetch(:teacher_interface_sanction_confirmation_form, {}).permit(
+        :confirmed_no_sanctions,
       )
     end
   end
