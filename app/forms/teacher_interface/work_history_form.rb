0b64cac1c14164f1bd2446ae68d0e9627006b6f5
Graham Pengelly <graham@three-tier.com>
2022-10-11 18:15:21 +0100

    Validate work history dates
    * Start date required
    * Start date must be valid
    * Start date must be in the past
    * End date required if not still employed
    * End date must be valid
    * End date must be after start date
    
    Entering an invalid date e.g. 13 2022 into either date was raising an
    error. It doesn't seem possible to validate these as strongly typed
    dates as it throws a ActiveRecord::MultiparameterAssignmentErrors error
    because the month is out of range. This happens before validation so it
    doesn't look like we can check first.
    
    Remove the `date` typing on the attribute and handle as a hash of
    values.

--- app/forms/teacher_interface/work_history_form.rb
+++ app/forms/teacher_interface/work_history_form.rb
@@ -11,19 +11,20 @@ module TeacherInterface
     attribute :country_code, :string
     attribute :contact_name, :string
     attribute :contact_email, :string
-    attribute :start_date, :date
+    attribute :start_date
+    attribute :end_date
     attribute :still_employed, :boolean
-    attribute :end_date, :date
 
+    validate :start_date_valid
+    validate :end_date_valid
+    validate :end_date_is_after_start_date
     validates :job, presence: true
     validates :school_name, presence: true
     validates :city, presence: true
     validates :country_code, presence: true
     validates :contact_name, presence: true
     validates :contact_email, presence: true, valid_for_notify: true
-    validates :start_date, presence: true
     validates :still_employed, inclusion: [true, false]
-    validates :end_date, presence: true, unless: :still_employed
 
     def country_code=(value)
       super(CountryCode.from_location(value))
@@ -41,6 +42,57 @@ module TeacherInterface
         still_employed:,
         end_date:,
       )
+    end
+
+    private
+
+    def start_date_valid
+      unless date_params_present?(start_date)
+        errors.add(:start_date, :blank) && return
+      end
+      unless date_params_are_valid?(start_date)
+        errors.add(:start_date, :invalid)
+      end
+      if date_params_are_valid?(start_date) &&
+           as_date(start_date) >= Time.zone.now
+        errors.add(:start_date, :future)
+      end
+    end
+
+    def end_date_valid
+      unless still_employed
+        unless date_params_present?(end_date)
+          errors.add(:end_date, :blank) && return
+        end
+        errors.add(:end_date, :invalid) unless date_params_are_valid?(end_date)
+      end
+    end
+
+    def end_date_is_after_start_date
+      if !still_employed && date_params_present?(start_date) &&
+           date_params_present?(end_date) &&
+           date_params_are_valid?(start_date) &&
+           date_params_are_valid?(end_date) &&
+           (as_date(end_date) <= as_date(start_date))
+        errors.add(:end_date, :before_start_date)
+      end
+    end
+
+    def date_params_are_valid?(date_hash)
+      as_date(date_hash)
+      true
+    rescue Date::Error
+      false
+    end
+
+    def as_date(date_hash)
+      Date.new(date_hash[1], date_hash[2], date_hash[3])
+    end
+
+    def date_params_present?(date_hash)
+      return false if date_hash.blank?
+
+      date_hash.compact.length == 3
     end
   end
 end
