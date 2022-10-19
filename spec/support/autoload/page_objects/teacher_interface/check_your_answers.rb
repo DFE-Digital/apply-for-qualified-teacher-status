68798c5b46b1d01263f094b80302cbe1003e7607
Thomas Leese <thomas@leese.io>
2022-10-19 10:48:17 +0100

    Refactor check your answers test
    We want to start testing the change links so we should refactor the page
    objects to use the GovukSummaryList page object to make this easier.

--- spec/support/autoload/page_objects/teacher_interface/check_your_answers.rb
+++ spec/support/autoload/page_objects/teacher_interface/check_your_answers.rb
@@ -1,18 +1,62 @@
 module PageObjects
   module TeacherInterface
     class CheckYourAnswers < SitePrism::Page
+      set_url "/teacher/application/edit"
+
       element :heading, "h1"
       element :content_title, "app-task-list_section"
-      element :about_you_section, "section#app-application-form-about-you"
-      element :who_you_can_teach_section,
-              "section#app-application-form-who-you-can-teach"
-      element :work_history_section, "section#app-application-form-work-history"
-      element :proof_of_recognition_section,
-              "section#app-application-form-proof-of-recognition"
-      element :submission_declaration_section,
-              "section#app-application-form-submission-declaration"
 
-      element :confirm_no_sanctions, ".govuk-checkboxes__input", visible: false
+      section :about_you, "#app-application-form-about-you" do
+        section :personal_information_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-personal-information"
+        section :identity_document_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-identity-document"
+      end
+
+      section :who_you_can_teach, "#app-application-form-who-you-can-teach" do
+        section :qualification_summary_list,
+                GovukSummaryList,
+                ".govuk-summary-list__card" \
+                  ":not(#app-check-your-answers-summary-age-range)" \
+                  ":not(#app-check-your-answers-summary-subjects)"
+        section :age_range_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-age-range"
+        section :subjects_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-subjects"
+      end
+
+      section :work_history, "section#app-application-form-work-history" do
+        section :add_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-add-work-history"
+        section :work_history_summary_list,
+                GovukSummaryList,
+                ".govuk-summary-list__card:not(#app-check-your-answers-summary-add-work-history)"
+      end
+
+      section :proof_of_recognition,
+              "section#app-application-form-proof-of-recognition" do
+        section :registration_number_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-registration-number"
+        section :written_statement_summary_list,
+                GovukSummaryList,
+                "#app-check-your-answers-summary-written-statement"
+      end
+
+      section :submission_declaration,
+              "#app-application-form-submission-declaration" do
+        section :form, "form" do
+          element :confirm_no_sanctions,
+                  ".govuk-checkboxes__input",
+                  visible: false
+          element :submit_button, ".govuk-button"
+        end
+      end
     end
   end
 end
