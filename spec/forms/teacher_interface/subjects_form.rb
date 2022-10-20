ea00ce3f7bf280fff4e19e0342c122353d042bd8
Thomas Leese <thomas@leese.io>
2022-10-20 11:08:37 +0100

    Add TeacherInterface::SubjectsForm
    This is a form which handles updating the subjects of an application,
    taking three individual values where one of them is a required field.

---
+++ spec/forms/teacher_interface/subjects_form.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe TeacherInterface::SubjectsForm, type: :model do
+  let(:application_form) { build(:application_form) }
+
+  subject(:form) do
+    described_class.new(application_form:, subject_1:, subject_2:, subject_3:)
+  end
+
+  describe "validations" do
+    let(:subject_1) { "" }
+    let(:subject_2) { "" }
+    let(:subject_3) { "" }
+
+    it { is_expected.to validate_presence_of(:application_form) }
+    it { is_expected.to validate_presence_of(:subject_1) }
+    it { is_expected.to_not validate_presence_of(:subject_2) }
+    it { is_expected.to_not validate_presence_of(:subject_3) }
+  end
+
+  describe "#save" do
+    let(:subject_1) { "Subject 1" }
+    let(:subject_2) { "Subject 2" }
+    let(:subject_3) { "" }
+
+    before { form.save(validate: true) }
+
+    it "saves the application form" do
+      expect(application_form.subjects).to eq(["Subject 1", "Subject 2"])
+    end
+  end
+end
