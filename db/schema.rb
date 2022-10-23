6e1917d608a2d52a07a5c9df75b7502d7ca78843
Thomas Leese <thomas@leese.io>
2022-10-23 13:42:09 +0100

    Add TRN field to teachers
    This will be used to capture the TRN number of teachers after making a
    request to the DQT API.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_181512) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_123629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -274,6 +274,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_181512) do
     t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "trn"
     t.index ["email"], name: "index_teachers_on_email", unique: true
   end
 
