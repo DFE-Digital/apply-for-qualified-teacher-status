ea866814e42c5b0f30d3a538268f1064bec98b35
Thomas Leese <thomas@leese.io>
2022-10-24 11:07:01 +0100

    Use ConfirmRecommendationForm
    This changes the assessment controller to use the new confirm
    recommendation form.

--- app/controllers/assessor_interface/assessments_controller.rb
+++ app/controllers/assessor_interface/assessments_controller.rb
@@ -3,14 +3,24 @@ module AssessorInterface
     before_action :load_assessment_and_application_form
 
     def edit
+      @confirm_recommendation_form =
+        ConfirmRecommendationForm.new(
+          assessment:,
+          user: current_staff,
+          recommendation: assessment.recommendation,
+        )
     end
 
     def update
-      if UpdateAssessmentRecommendation.call(
-           assessment: @assessment,
-           user: current_staff,
-           new_recommendation: assessment_params[:recommendation],
-         )
+      @confirm_recommendation_form =
+        ConfirmRecommendationForm.new(
+          confirm_recommendation_form_params.merge(
+            assessment:,
+            user: current_staff,
+          ),
+        )
+
+      if @confirm_recommendation_form.save
         redirect_to post_update_redirect_path
       else
         render :edit, status: :unprocessable_entity
@@ -20,17 +30,23 @@ module AssessorInterface
     private
 
     def load_assessment_and_application_form
-      @assessment =
+      @assessment = assessment
+      @application_form = assessment.application_form
+    end
+
+    def assessment
+      @assessment ||=
         Assessment
           .includes(:application_form)
           .where(application_form_id: params[:application_form_id])
           .find(params[:id])
-
-      @application_form = @assessment.application_form
     end
 
-    def assessment_params
-      params.require(:assessment).permit(:recommendation)
+    def confirm_recommendation_form_params
+      params.require(:assessor_interface_confirm_recommendation_form).permit(
+        :recommendation,
+        :confirm,
+      )
     end
 
     def post_update_redirect_path
