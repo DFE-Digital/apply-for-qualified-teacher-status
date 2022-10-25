DFE-Digital/apply-for-qualified-teacher-status

commit 89a21ecaa05023754f1666fd9bbabe7e685ac302

Add empty review further information request view
This is where the check your answers style component will be shown to
the assessors so they can review the further information.

app/views/assessor_interface/application_forms/show.html.erb
@@ -1,11 +1,6 @@
 <% content_for :page_title, "Application" %>
 <% content_for :back_link_url, @view_object.back_link_path %>
 
-<div class="app-inline-action">
-  <h1 class="govuk-heading-xl">Application</h1>
-  <%= govuk_button_link_to "Add note", new_assessor_interface_application_form_note_path(@view_object.application_form), secondary: true %>
-</div>
-
 <%= render(ApplicationFormOverview::Component.new(@view_object.application_form)) %>
 
 <h2 class="govuk-heading-l govuk-!-margin-top-9">Assessment</h2>

spec/support/autoload/page_objects/assessor_interface/application.rb
@@ -12,42 +12,30 @@ module PageObjects
         element :status, "div:nth-of-type(9) > dd:nth-of-type(1)"
       end
 
-      section :task_list, "ol.app-task-list" do
-        sections :tasks, "ol.app-task-list > li" do
-          sections :items, "li.app-task-list__item" do
-            section :name, ".app-task-list__task-name" do
-              element :link, "a"
-            end
-
-            element :status, "strong"
-          end
-        end
-      end
+      section :task_list, TaskList, ".app-task-list"
 
       def personal_information_task
-        task_item_for("Check personal information")
+        task_list.find_item("Check personal information")
       end
 
       def qualifications_task
-        task_item_for("Check qualifications")
+        task_list.find_item("Check qualifications")
       end
 
       def age_range_subjects_task
-        task_item_for("Verify age range and subjects")
+        task_list.find_item("Verify age range and subjects")
       end
 
       def work_history_task
-        task_item_for("Check work history")
+        task_list.find_item("Check work history")
       end
 
       def professional_standing_task
-        task_item_for("Check professional standing")
+        task_list.find_item("Check professional standing")
       end
 
-      private
-
-      def task_item_for(text)
-        task_list.tasks.first.items.find { |item| item.name.text == text }
+      def review_requested_information_task
+        task_list.find_item("Review requested information from applicant")
       end
     end
   end

app/controllers/assessor_interface/further_information_requests_controller.rb
@@ -1,8 +1,10 @@
 module AssessorInterface
   class FurtherInformationRequestsController < BaseController
     before_action :load_application_form_and_assessment,
-                  only: %i[preview new show]
+                  only: %i[preview new show edit]
     before_action :load_new_further_information_request, only: %i[preview new]
+    before_action :load_existing_further_information_request,
+                  only: %i[show edit]
 
     def preview
     end
@@ -28,7 +30,9 @@ module AssessorInterface
     end
 
     def show
-      @further_information_request = further_information_request
+    end
+
+    def edit
     end
 
     private
@@ -46,6 +50,10 @@ module AssessorInterface
               assessment_sections: assessment.sections,
             ),
         )
+    end
+
+    def load_existing_further_information_request
+      @further_information_request = further_information_request
     end
 
     def further_information_request

app/views/assessor_interface/further_information_requests/edit.html.erb
@@ -1,0 +1,4 @@
+<% content_for :title, t(".title") %>
+<% content_for :back_link_url, assessor_interface_application_form_path(@application_form) %>
+
+<h1 class="govuk-heading-xl"><%= t(".title") %></h1>

config/locales/assessor_interface.en.yml
@@ -77,6 +77,10 @@ en:
           qualified_to_teach: We were not provided with sufficient evidence to confirm qualification to teach at state or government schools.
           full_professional_status: Recognition level as a teacher does not match the required level, or has outstanding additional conditions.
 
+    further_information_requests:
+      edit:
+        title: Review further information from applicant
+
   components:
     timeline_entry:
       title:

spec/support/autoload/page_objects/assessor_interface/review_further_information_request.rb
@@ -1,0 +1,10 @@
+module PageObjects
+  module AssessorInterface
+    class ReviewFurtherInformationRequest < SitePrism::Page
+      set_url "/assessor/applications/{application_id}/assessments/{assessment_id}" \
+                "/further-information-requests/{further_information_request_id}/edit"
+
+      element :heading, ".govuk-heading-xl"
+    end
+  end
+end

spec/support/autoload/page_objects/task_list.rb
@@ -3,7 +3,7 @@ module PageObjects
     sections :sections, TaskListSection, ".app-task-list > li"
 
     def find_item(text)
-      sections.flat_map(&:items).find { |item| item.link.text == text }
+      sections.flat_map(&:items).find { |item| item.name.text == text }
     end
 
     def click_item(link_text)

spec/support/autoload/page_objects/task_list_item.rb
@@ -1,5 +1,6 @@
 module PageObjects
   class TaskListItem < SitePrism::Section
+    element :name, "span"
     element :link, "a"
     element :status_tag, ".govuk-tag"
   end

commit 15004bde1fcd371cdbafbeed9a2c51ec52e0d6d3

Add "Add note" button to multiple pages
This partially reverts
https://github.com/DFE-Digital/apply-for-qualified-teacher-status/commit/89a21ecaa05023754f1666fd9bbabe7e685ac302
which accidentally removed the button, and then adds the missing button
to various pages.

app/views/assessor_interface/application_forms/show.html.erb
@@ -1,6 +1,11 @@
 <% content_for :page_title, "Application" %>
 <% content_for :back_link_url, @view_object.back_link_path %>
 
+<div class="app-inline-action">
+  <h1 class="govuk-heading-xl">Application</h1>
+  <%= govuk_button_link_to "Add&nbsp;note".html_safe, new_assessor_interface_application_form_note_path(@view_object.application_form), secondary: true %>
+</div>
+
 <%= render(ApplicationFormOverview::Component.new(@view_object.application_form)) %>
 
 <h2 class="govuk-heading-l govuk-!-margin-top-9">Assessment</h2>

app/views/assessor_interface/assessment_sections/show.html.erb
@@ -1,7 +1,10 @@
 <% content_for :page_title, t(".title.#{@assessment_section_view_object.assessment_section.key}") %>
 <% content_for :back_link_url, assessor_interface_application_form_path(@assessment_section_view_object.application_form) %>
 
-<h1 class="govuk-heading-xl"><%= t(".title.#{@assessment_section_view_object.assessment_section.key}") %></h1>
+<div class="app-inline-action">
+  <h1 class="govuk-heading-xl"><%= t(".title.#{@assessment_section_view_object.assessment_section.key}") %></h1>
+  <%= govuk_button_link_to "Add&nbsp;note".html_safe, new_assessor_interface_application_form_note_path(@assessment_section_view_object.application_form), secondary: true %>
+</div>
 
 <% case @assessment_section_view_object.assessment_section.key %>
 <% when "personal_information" %>

app/views/assessor_interface/assessor_assignments/new.html.erb
@@ -1,18 +1,17 @@
-<h1 class="govuk-heading-l">
-  Select an assessor
-</h1>
+<% content_for :page_title, "Select an assessor" %>
+<% content_for :back_link_url, assessor_interface_application_form_path(@application_form) %>
 
-<%= form_for @assessor_assignment_form, url: assessor_interface_application_form_assign_assessor_path(@application_form) do |form| %>
-    <%= form.govuk_error_summary %>
+<div class="app-inline-action">
+  <h1 class="govuk-heading-xl">Select an assessor</h1>
+  <%= govuk_button_link_to "Add&nbsp;note".html_safe, new_assessor_interface_application_form_note_path(@application_form), secondary: true %>
+</div>
 
-    <%= form.govuk_collection_radio_buttons :assessor_id,
-      Staff.all,
-      :id,
-      :name,
-      legend: nil
-    %>
-    <%= form.govuk_submit "Continue" do %>
-      <%= govuk_link_to "Cancel", assessor_interface_application_form_path(@application_form) %>
-    <%- end -%>
-<%- end -%>
+<%= form_with model: @assessor_assignment_form, url: assessor_interface_application_form_assign_assessor_path(@application_form) do |f| %>
+  <%= f.govuk_error_summary %>
 
+  <%= f.govuk_collection_radio_buttons :assessor_id, Staff.all, :id, :name, legend: nil %>
+
+  <%= f.govuk_submit do %>
+    <%= govuk_link_to "Cancel", assessor_interface_application_form_path(@application_form) %>
+  <% end %>
+<% end %>

spec/support/autoload/page_objects/assessor_interface/application.rb
@@ -5,6 +5,8 @@ module PageObjects
 
       element :back_link, "a", text: "Back"
 
+      element :add_note_button, ".app-inline-action .govuk-button"
+
       section :overview, "#app-application-overview" do
         element :name, "div:nth-of-type(1) > dd:nth-of-type(1)"
         element :assessor_name, "div:nth-of-type(7) > dd:nth-of-type(1)"

spec/system/assessor_interface/creating_note_spec.rb
@@ -9,8 +9,14 @@ RSpec.describe "Creating a note", type: :system do
     given_there_is_an_application_form
     given_an_assessor_exists
 
-    when_i_visit_the(:create_note_page, application_id: application_form.id)
-    and_i_create_a_note
+    when_i_visit_the(
+      :assessor_application_page,
+      application_id: application_form.id,
+    )
+    and_i_click_add_note
+    then_i_see_the(:create_note_page, application_id: application_form.id)
+
+    when_i_create_a_note
     then_i_see_the(
       :assessor_application_page,
       application_id: application_form.id,
@@ -31,7 +37,11 @@ RSpec.describe "Creating a note", type: :system do
     assessor
   end
 
-  def and_i_create_a_note
+  def and_i_click_add_note
+    assessor_application_page.add_note_button.click
+  end
+
+  def when_i_create_a_note
     create_note_page.form.text_textarea.fill_in with: "A note."
     create_note_page.form.submit_button.click
   end