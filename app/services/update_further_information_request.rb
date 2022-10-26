4e07a9d03a897f079355d88e42b0f3e5196abeff
Thomas Leese <thomas@leese.io>
2022-10-26 15:50:18 +0100

    Add UpdateFurtherInformationRequest
    This is a service which updates further information requests, and if
    the request has been assessed it creates a timeline event.

---
+++ app/services/update_further_information_request.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class UpdateFurtherInformationRequest
+  include ServicePattern
+
+  def initialize(further_information_request:, user:, params:)
+    @further_information_request = further_information_request
+    @user = user
+    @params = params
+  end
+
+  def call
+    ActiveRecord::Base.transaction do
+      further_information_request.update!(params)
+      create_timeline_event
+    end
+  end
+
+  private
+
+  attr_reader :further_information_request, :user, :params
+
+  def create_timeline_event
+    unless further_information_request.passed.nil?
+      TimelineEvent.create!(
+        creator: user,
+        event_type: :further_information_request_assessed,
+        further_information_request:,
+        application_form:,
+      )
+    end
+  end
+
+  def application_form
+    further_information_request.assessment.application_form
+  end
+end
