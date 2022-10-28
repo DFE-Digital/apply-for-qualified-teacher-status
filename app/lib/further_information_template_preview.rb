11a77437aed346f0f428684fa2d6a5054161e194
Thomas Leese <thomas@leese.io>
2022-10-28 10:09:45 +0100

    Use GOVUK_NOTIFY_TEMPLATE_ID from TeacherMailer
    We want to make sure we're using the same template ID as we're rendering
    the same emails.

--- app/lib/further_information_template_preview.rb
+++ app/lib/further_information_template_preview.rb
@@ -1,10 +1,4 @@
 class FurtherInformationTemplatePreview
-  GOVUK_NOTIFY_TEMPLATE_ID =
-    ENV.fetch(
-      "GOVUK_NOTIFY_TEMPLATE_ID_MAILER",
-      "95adafaf-0920-4623-bddc-340853c047af",
-    )
-
   class << self
     def with(teacher:, further_information_request:)
       new(teacher:, further_information_request:)
@@ -18,7 +12,7 @@ class FurtherInformationTemplatePreview
 
   def render
     client.generate_template_preview(
-      GOVUK_NOTIFY_TEMPLATE_ID,
+      TeacherMailer::GOVUK_NOTIFY_TEMPLATE_ID,
       personalisation: {
         to: mail.to.first,
         subject: mail.subject,
