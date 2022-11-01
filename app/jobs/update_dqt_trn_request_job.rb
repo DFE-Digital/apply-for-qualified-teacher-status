ab5002a6b93b4b835eb22053d0e4df356b7eec74
Thomas Leese <thomas@leese.io>
2022-11-01 13:25:59 +0000

    Refactor UpdateDQTTRNRequestJob
    This improves the readability of the job by splitting up the various
    actions in to individual methods.

--- app/jobs/update_dqt_trn_request_job.rb
+++ app/jobs/update_dqt_trn_request_job.rb
@@ -10,30 +10,40 @@ class UpdateDQTTRNRequestJob < ApplicationJob
   def perform(dqt_trn_request)
     return if dqt_trn_request.complete?
 
-    response =
-      if dqt_trn_request.initial?
-        DQT::Client::CreateTRNRequest.call(
-          request_id: dqt_trn_request.request_id,
-          application_form: dqt_trn_request.application_form,
-        )
-      else
-        DQT::Client::ReadTRNRequest.call(request_id: dqt_trn_request.request_id)
-      end
+    response = fetch_response(dqt_trn_request)
 
     dqt_trn_request.pending! if dqt_trn_request.initial?
 
     if (trn = response[:trn]).present?
-      ActiveRecord::Base.transaction do
-        dqt_trn_request.application_form.teacher.update!(trn:)
-        dqt_trn_request.complete!
-      end
-
-      TeacherMailer
-        .with(teacher: dqt_trn_request.application_form.teacher)
-        .application_awarded
-        .deliver_later
+      update_teacher_trn(dqt_trn_request, trn)
+      send_award_email(dqt_trn_request)
+      dqt_trn_request.complete!
     end
 
     raise StillPending if dqt_trn_request.pending?
+  end
+
+  private
+
+  def fetch_response(dqt_trn_request)
+    if dqt_trn_request.initial?
+      DQT::Client::CreateTRNRequest.call(
+        request_id: dqt_trn_request.request_id,
+        application_form: dqt_trn_request.application_form,
+      )
+    else
+      DQT::Client::ReadTRNRequest.call(request_id: dqt_trn_request.request_id)
+    end
+  end
+
+  def update_teacher_trn(dqt_trn_request, trn)
+    dqt_trn_request.application_form.teacher.update!(trn:)
+  end
+
+  def send_award_email(dqt_trn_request)
+    TeacherMailer
+      .with(teacher: dqt_trn_request.application_form.teacher)
+      .application_awarded
+      .deliver_later
   end
 end
