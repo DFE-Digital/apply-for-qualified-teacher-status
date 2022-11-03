e36770f8e0b7a2f7d5bcc4d1153d2d82030702e5
Graham Pengelly <graham@three-tier.com>
2022-11-03 10:48:04 +0000

    [1076] Make decline failure reason notes optional
    Remove the presence validation on the notes on decline failure reasons as they aren't required.
    
    Non-decline failure reasons still require a note as they feed into FI.

--- app/forms/assessor_interface/assessment_section_form.rb
+++ app/forms/assessor_interface/assessment_section_form.rb
@@ -60,6 +60,8 @@ class AssessorInterface::AssessmentSectionForm
       assessment_section.failure_reasons.each do |failure_reason|
         klass.attribute "#{failure_reason}_checked", :boolean
         klass.attribute "#{failure_reason}_notes", :string
+        next if decline_failure_reason?(failure_reason)
+
         klass.validates "#{failure_reason}_notes",
                         presence: true,
                         if: :"#{failure_reason}_checked"
@@ -79,6 +81,10 @@ class AssessorInterface::AssessmentSectionForm
     def permit_parameters(params)
       args, kwargs = permittable_parameters
       params.permit(:passed, *args, **kwargs)
+    end
+
+    def decline_failure_reason?(failure_reason)
+      AssessmentSection::DECLINE_FAILURE_REASONS.include?(failure_reason)
     end
 
     protected
