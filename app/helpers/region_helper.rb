50b9996a8276a79915e7b56b1fd88b5d2da2e118
Thomas Leese <thomas@leese.io>
2022-11-15 19:06:15 +0000

    Show letter of professional standing
    Instead of certificate, which isn't meaningful to our users.

--- app/helpers/region_helper.rb
+++ app/helpers/region_helper.rb
@@ -4,7 +4,8 @@ module RegionHelper
   def region_certificate_phrase(region)
     certificate =
       region.teaching_authority_certificate.presence ||
-        region.country.teaching_authority_certificate.presence || "certificate"
+        region.country.teaching_authority_certificate.presence ||
+        "letter that proves you’re recognised as a teacher"
     "#{certificate.indefinite_article} #{tag.span(certificate, lang: region.country.code)}".html_safe
   end
 end
