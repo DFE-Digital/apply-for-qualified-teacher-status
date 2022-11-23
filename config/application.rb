baa1a9d079dcd2ab6350cf07f30aa35c972987ea
Thomas Leese <thomas@leese.io>
2022-11-23 13:09:16 +0000

    Add policies to eager load path
    We're going to put the Pundit policies in there.

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,8 @@ module ApplyForQualifiedTeacherStatus
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.eager_load_paths << "#{root}/app/policies"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
