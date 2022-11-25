ba9e8381e7653157ccede67baf9902c025274e2c
Thomas Leese <thomas@leese.io>
2022-11-25 11:11:31 +0000

    Ignore Pundit::NotAuthorizedError
    We don't need to send these to Sentry since there's no action to be
    taken from them.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -14,13 +14,15 @@ Sentry.init do |config|
   config.before_send = lambda { |event, _hint| filter.filter(event.to_hash) }
 
   config.inspect_exception_causes_for_exclusion = true
-  config.excluded_exceptions += [
-    # The following exceptions are user-errors that aren't actionable, and can
-    # be safely ignored.
-    "ActionController::BadRequest",
-    "ActionController::UnknownFormat",
-    "ActionController::UnknownHttpMethod",
-    "ActionDispatch::Http::Parameters::ParseError",
-    "Mime::Type::InvalidMimeType",
+
+  # The following exceptions are user-errors that aren't actionable, and can
+  # be safely ignored.
+  config.excluded_exceptions += %w[
+    ActionController::BadRequest
+    ActionController::UnknownFormat
+    ActionController::UnknownHttpMethod
+    ActionDispatch::Http::Parameters::ParseError
+    Mime::Type::InvalidMimeType
+    Pundit::NotAuthorizedError
   ]
 end
