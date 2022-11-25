7bc310511a958a01c4d186edc70d7695f6131ebb
Thomas Leese <thomas@leese.io>
2022-11-25 08:23:53 +0000

    Store filter parameters in the session
    When applying or clearing filters, we want the parameters to be stored
    in the session so the view object can then fetch them from the session.
    This means the parameters are kept as the user is navigating around the
    site.

--- app/controllers/assessor_interface/application_forms_controller.rb
+++ app/controllers/assessor_interface/application_forms_controller.rb
@@ -5,12 +5,39 @@ module AssessorInterface
       render layout: "full_from_desktop"
     end
 
+    def apply_filters
+      session[:filter_params] = extract_filter_params(
+        remove_cleared_autocomplete_values(params),
+      )
+      redirect_to assessor_interface_application_forms_path(page: params[:page])
+    end
+
+    def clear_filters
+      session[:filter_params] = {}
+      redirect_to assessor_interface_application_forms_path(page: params[:page])
+    end
+
     def show
       @view_object = ApplicationFormsShowViewObject.new(params:)
     end
 
     def status
       @view_object = ApplicationFormsShowViewObject.new(params:)
+    end
+
+    private
+
+    def extract_filter_params(params)
+      params[:assessor_interface_filter_form].permit!
+    end
+
+    def remove_cleared_autocomplete_values(params)
+      if params.include?(:location_autocomplete) &&
+           params[:location_autocomplete].blank?
+        params[:assessor_interface_filter_form]&.delete(:location)
+      end
+
+      params
     end
   end
 end
