39f0b713b75058ad7dfe9549c26f3c2ec2d13cb4
Thomas Leese <thomas@leese.io>
2022-11-28 11:27:28 +0000

    Add HistoryStack
    This stores a history of where the user has been and allows us to
    implement back links correctly where users are taken to the right place.

---
+++ app/lib/history_stack.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class HistoryStack
+  def initialize(session:)
+    @session = session
+  end
+
+  def push(path:, origin:, check:, reset:)
+    apply_to_stack do |stack|
+      stack.clear if reset
+      current_entry = stack.last
+      new_entry = { path:, origin:, check: }
+      stack.push(new_entry) if current_entry != new_entry
+    end
+  end
+
+  def push_self(request, origin:, check:, reset: false)
+    push(path: request.fullpath, origin:, check:, reset:)
+  end
+
+  def pop
+    apply_to_stack(&:pop)
+  end
+
+  def pop_back
+    pop # self
+
+    entry = pop
+    entry[:path] if entry
+  end
+
+  def pop_to_origin
+    pop # self
+
+    loop do
+      entry = pop
+      return nil if entry.nil?
+      return entry[:path] if entry[:origin]
+    end
+  end
+
+  def replace_self(path:, origin:, check:)
+    pop
+    push(path:, origin:, check:, reset: false)
+  end
+
+  def last_entry
+    apply_to_stack(&:second_to_last)
+  end
+
+  private
+
+  def apply_to_stack
+    stack = session[:history_stack] || []
+    return_value = yield stack
+    session[:history_stack] = stack
+    return_value
+  end
+
+  attr_reader :session
+end
