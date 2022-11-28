b03a4ac0be622ffb5876b8b8899caf0d427de778
Thomas Leese <thomas@leese.io>
2022-11-28 12:28:17 +0000

    Add sessions table
    This adds a migration which creates the table we'll be using to store
    sessions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_093227) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_122306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -232,6 +232,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_093227) do
     t.string "teaching_authority_online_checker_url", default: "", null: false
     t.index ["country_id", "name"], name: "index_regions_on_country_id_and_name", unique: true
     t.index ["country_id"], name: "index_regions_on_country_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "staff", force: :cascade do |t|
