128a7b295e29a4e70022ab0e883183e95de55798
Thomas Leese <thomas@leese.io>
2022-11-28 10:47:07 +0000

    Switch to Redis Session Store
    This changes the session store to use our Redis instance rather than
    relying on storing the data in a cookie. It allows us to store more data
    in the session than we can in the session store and we anticipate we'll
    need this to support our back links stack and filters.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,11 @@
-Rails.application.config.session_store :cookie_store,
+url = ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
+
+Rails.application.config.session_store :redis_session_store,
                                        key: "_session_id",
                                        secure: Rails.env.production?,
-                                       expire_after: 20.hours
+                                       serializer: :json,
+                                       redis: {
+                                         expire_after: 20.hours,
+                                         key_prefix: "session:",
+                                         url:,
+                                       }
