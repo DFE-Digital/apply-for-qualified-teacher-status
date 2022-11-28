c96c9de1021e397b15312365ba38940db4e6fd23
Thomas Leese <thomas@leese.io>
2022-11-28 12:11:01 +0000

    Add HistoryController
    This is a controller for managing the history stack, specifically
    there's an endpoint which we'll use for back links that pops values from
    the stack depending on certain parameters.

---
+++ app/controllers/history_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class HistoryController < ApplicationController
+  def back
+    to_origin = back_params[:origin] == "true"
+    method = to_origin ? :pop_to_origin : :pop_back
+
+    path = history_stack.send(method) || default_path
+    redirect_to path, status: :see_other
+  end
+
+  private
+
+  def default_path
+    URI.parse(back_params[:default] || "/teacher/application").path
+  end
+
+  def back_params
+    params.permit(:origin, :default)
+  end
+
+  def history_stack
+    @history_stack ||= HistoryStack.new(session:)
+  end
+end
