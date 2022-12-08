8af1a3a23191d5e52f62ff0a52f17391622c663f
Thomas Leese <thomas@leese.io>
2022-12-08 16:02:09 +0000

    Remove confirmation fields from teachers
    We no longer need to use these fields as users will be authenticated
    using a OTP.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,24 +2,20 @@
 #
 # Table name: teachers
 #
-#  id                   :bigint           not null, primary key
-#  confirmation_sent_at :datetime
-#  confirmation_token   :string
-#  confirmed_at         :datetime
-#  current_sign_in_at   :datetime
-#  current_sign_in_ip   :string
-#  email                :string           default(""), not null
-#  last_sign_in_at      :datetime
-#  last_sign_in_ip      :string
-#  otp_created_at       :datetime
-#  otp_guesses          :integer          default(0), not null
-#  secret_key           :string
-#  sign_in_count        :integer          default(0), not null
-#  trn                  :string
-#  unconfirmed_email    :string
-#  uuid                 :uuid             not null
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
+#  id                 :bigint           not null, primary key
+#  current_sign_in_at :datetime
+#  current_sign_in_ip :string
+#  email              :string           default(""), not null
+#  last_sign_in_at    :datetime
+#  last_sign_in_ip    :string
+#  otp_created_at     :datetime
+#  otp_guesses        :integer          default(0), not null
+#  secret_key         :string
+#  sign_in_count      :integer          default(0), not null
+#  trn                :string
+#  uuid               :uuid             not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
 #
 # Indexes
 #
