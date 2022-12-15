fbc5a8091173fe714c95aeeb06df6f21fe39ecfb
Thomas Leese <thomas@leese.io>
2022-12-15 10:52:41 +0000

    Drop features table
    We won't be using these feature flags anymore as they're covered by the
    GOV.UK Feature Flags library.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_102100) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_105051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -173,14 +173,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_102100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_feature_flags_features_on_name", unique: true
-  end
-
-  create_table "features", force: :cascade do |t|
-    t.string "name", null: false
-    t.boolean "active", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_features_on_name", unique: true
   end
 
   create_table "further_information_request_items", force: :cascade do |t|
