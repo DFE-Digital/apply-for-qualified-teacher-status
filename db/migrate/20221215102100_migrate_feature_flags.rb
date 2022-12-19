b79b276ad30ce1b67c145da9272c482736ed14c6
Thomas Leese <thomas@leese.io>
2022-12-19 09:57:16 +0000

    Remove migration for feature flags
    It's not possible to run this migration anymore as the model it
    references no longer exists. It's already run in all the environments so
    we can safely delete it.

--- db/migrate/20221215102100_migrate_feature_flags.rb
+++
@@ -1,7 +0,0 @@
-class MigrateFeatureFlags < ActiveRecord::Migration[7.0]
-  def change
-    Feature.all.each do |feature|
-      FeatureFlags::Feature.create!(feature.attributes)
-    end
-  end
-end
