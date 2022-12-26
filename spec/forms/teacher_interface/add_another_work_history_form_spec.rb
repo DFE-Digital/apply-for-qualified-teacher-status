f9bfa1b449c15d9b01db761a22b98b5d0fa9372c
Thomas Leese <thomas@leese.io>
2022-12-26 11:35:12 +0000

    Add AddAnotherWorkHistoryForm
    This is a form which will be used to render the page asking a user
    whether they would like to add another item of work history.

--- spec/forms/teacher_interface/add_another_work_history_form_spec.rb
+++ spec/forms/teacher_interface/add_another_work_history_form_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe TeacherInterface::AddAnotherUploadForm, type: :model do
+RSpec.describe TeacherInterface::AddAnotherWorkHistoryForm, type: :model do
   subject(:form) { described_class.new }
 
   it { is_expected.to allow_values(true, false).for(:add_another) }
