16e421adc56fd57dc24df9a5f41078e5f1b1de1b
Thomas Leese <thomas@leese.io>
2023-01-05 09:39:24 +0000

    Add helper methods to work experience page object
    We'll use these methods in the system tests.

--- spec/support/autoload/page_objects/eligibility_interface/work_experience.rb
+++ spec/support/autoload/page_objects/eligibility_interface/work_experience.rb
@@ -2,6 +2,21 @@ module PageObjects
   module EligibilityInterface
     class WorkExperience < Question
       set_url "/eligibility/work-experience"
+
+      def submit_under_9_months
+        form.radio_items[0].choose
+        form.continue_button.click
+      end
+
+      def submit_between_9_and_20_months
+        form.radio_items[1].choose
+        form.continue_button.click
+      end
+
+      def submit_over_20_months
+        form.radio_items[2].choose
+        form.continue_button.click
+      end
     end
   end
 end
