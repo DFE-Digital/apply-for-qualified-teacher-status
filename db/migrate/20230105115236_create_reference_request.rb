93a5cbb669818c763288c74d1f6182adb5bf83b6
Thomas Leese <thomas@leese.io>
2023-01-05 13:41:57 +0000

    Add ReferenceRequest model
    This is a model which will store a request to a referee for information
    about a work history item. It's similar to the FurtherInformationRequest
    model and uses the same Requestable concern.

---
+++ db/migrate/20230105115236_create_reference_request.rb
@@ -0,0 +1,20 @@
+class CreateReferenceRequest < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reference_requests do |t|
+      t.string :slug, null: false
+      t.references :assessment, null: false, foreign_key: true
+      t.references :work_history, null: false, foreign_key: true
+      t.string :state, null: false
+      t.datetime :received_at
+      t.boolean :dates_response
+      t.boolean :hours_response
+      t.boolean :children_response
+      t.boolean :lessons_response
+      t.boolean :reports_response
+      t.text :additional_information_response, null: false, default: ""
+      t.timestamps
+    end
+
+    add_index :reference_requests, :slug, unique: true
+  end
+end
---
+++ spec/factories/reference_requests.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: reference_requests
+#
+#  id                              :bigint           not null, primary key
+#  additional_information_response :text             default(""), not null
+#  children_response               :boolean
+#  dates_response                  :boolean
+#  hours_response                  :boolean
+#  lessons_response                :boolean
+#  received_at                     :datetime
+#  reports_response                :boolean
+#  slug                            :string           not null
+#  state                           :string           not null
+#  created_at                      :datetime         not null
+#  updated_at                      :datetime         not null
+#  assessment_id                   :bigint           not null
+#  work_history_id                 :bigint           not null
+#
+# Indexes
+#
+#  index_reference_requests_on_assessment_id    (assessment_id)
+#  index_reference_requests_on_slug             (slug) UNIQUE
+#  index_reference_requests_on_work_history_id  (work_history_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (assessment_id => assessments.id)
+#  fk_rails_...  (work_history_id => work_histories.id)
+#
+FactoryBot.define do
+  factory :reference_request do
+    slug { Faker::Internet.unique.slug }
+
+    association :assessment
+    association :work_history, :completed
+
+    trait :requested do
+      state { "requested" }
+    end
+
+    trait :received do
+      state { "received" }
+      received_at { Faker::Time.between(from: 1.month.ago, to: Time.zone.now) }
+      receivable
+    end
+
+    trait :expired do
+      state { "expired" }
+    end
+
+    trait :receivable do
+      dates_response { Faker::Boolean.boolean }
+      hours_response { Faker::Boolean.boolean }
+      children_response { Faker::Boolean.boolean }
+      lessons_response { Faker::Boolean.boolean }
+      reports_response { Faker::Boolean.boolean }
+      additional_information_response { Faker::Lorem.sentence }
+    end
+
+    trait :responses_invalid do
+      dates_response { false }
+      hours_response { false }
+      children_response { false }
+      lessons_response { false }
+      reports_response { false }
+    end
+
+    trait :responses_valid do
+      dates_response { true }
+      hours_response { true }
+      children_response { true }
+      lessons_response { true }
+      reports_response { true }
+    end
+  end
+end
