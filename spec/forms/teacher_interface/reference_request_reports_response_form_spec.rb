a013aef81bc424b0e5ec10616c025b018b86d178
Thomas Leese <thomas@leese.io>
2023-01-06 15:20:50 +0000

    Add ReferenceRequestReportsResponseForm
    This is a form which will be used to render the page where we ask the
    referee about whether the applicant reported on the students.

---
+++ spec/forms/teacher_interface/reference_request_reports_response_form_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TeacherInterface::ReferenceRequestReportsResponseForm,
+               type: :model do
+  let(:reference_request) { create(:reference_request) }
+
+  subject(:form) { described_class.new(reference_request:, reports_response:) }
+
+  describe "validations" do
+    let(:reports_response) { "" }
+
+    it { is_expected.to validate_presence_of(:reference_request) }
+    it { is_expected.to allow_values(true, false).for(:reports_response) }
+  end
+
+  describe "#save" do
+    subject(:save) { form.save(validate: true) }
+
+    let(:reports_response) { "true" }
+
+    it "saves the application form" do
+      expect { save }.to change(reference_request, :reports_response).to(true)
+    end
+  end
+end
