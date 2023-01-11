19954d5d9f8d2c0c0f1f8f5e0f7fa85c9e5c382b
Thomas Leese <thomas@leese.io>
2023-01-11 10:20:23 +0000

    Add RefereeMailer
    This adds a new mailer class which we can use to send emails to
    referees.

---
+++ app/mailers/referee_mailer.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+class RefereeMailer < ApplicationMailer
+  include ApplicationFormHelper
+
+  before_action :set_reference_request, :set_work_history, :set_application_form
+
+  helper :application_form
+
+  def reference_reminder
+    view_mail(
+      GOVUK_NOTIFY_TEMPLATE_ID,
+      to: work_history.contact_email,
+      subject:
+        I18n.t(
+          "mailer.referee.reference_reminder.subject",
+          name: application_form_full_name(application_form),
+        ),
+    )
+  end
+
+  def reference_requested
+    view_mail(
+      GOVUK_NOTIFY_TEMPLATE_ID,
+      to: work_history.contact_email,
+      subject:
+        I18n.t(
+          "mailer.referee.reference_requested.subject",
+          name: application_form_full_name(application_form),
+        ),
+    )
+  end
+
+  private
+
+  def reference_request
+    params[:reference_request]
+  end
+
+  delegate :application_form, :work_history, to: :reference_request
+
+  def set_reference_request
+    @reference_request = reference_request
+  end
+
+  def set_work_history
+    @work_history = work_history
+  end
+
+  def set_application_form
+    @application_form = application_form
+  end
+end
---
+++ spec/mailers/referee_mailer_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe RefereeMailer, type: :mailer do
+  let(:application_form) do
+    create(:application_form, given_names: "First", family_name: "Last")
+  end
+
+  let(:assessment) { create(:assessment, application_form:) }
+  let(:work_history) do
+    create(
+      :work_history,
+      application_form:,
+      contact_name: "Contact Name",
+      contact_email: "referee@school.com",
+    )
+  end
+
+  let(:reference_request) do
+    create(:reference_request, assessment:, work_history:)
+  end
+
+  describe "#reference_reminder" do
+    subject(:mail) do
+      described_class.with(reference_request:).reference_reminder
+    end
+
+    describe "#subject" do
+      subject(:subject) { mail.subject }
+
+      it do
+        is_expected.to eq(
+          "We still need you to verify First Last’s application for qualified teacher status (QTS)",
+        )
+      end
+    end
+
+    describe "#to" do
+      subject(:to) { mail.to }
+
+      it { is_expected.to eq(["referee@school.com"]) }
+    end
+
+    describe "#body" do
+      subject(:body) { mail.body.encoded }
+
+      it { is_expected.to include("Dear Contact Name") }
+      it do
+        is_expected.to include(
+          "We still need you to confirm some information about First Last",
+        )
+      end
+      it do
+        is_expected.to include(
+          "http://localhost:3000/teacher/references/#{reference_request.id}",
+        )
+      end
+    end
+
+    it_behaves_like "an observable mailer", "reference_reminder"
+  end
+
+  describe "#reference_requested" do
+    subject(:mail) do
+      described_class.with(reference_request:).reference_requested
+    end
+
+    describe "#subject" do
+      subject(:subject) { mail.subject }
+
+      it do
+        is_expected.to eq(
+          "Please help us to verify First Last’s application for qualified teacher status (QTS)",
+        )
+      end
+    end
+
+    describe "#to" do
+      subject(:to) { mail.to }
+
+      it { is_expected.to eq(["referee@school.com"]) }
+    end
+
+    describe "#body" do
+      subject(:body) { mail.body.encoded }
+
+      it { is_expected.to include("Dear Contact Name") }
+      it do
+        is_expected.to include(
+          "We need you to confirm some information about First Last",
+        )
+      end
+      it do
+        is_expected.to include(
+          "http://localhost:3000/teacher/references/#{reference_request.id}",
+        )
+      end
+    end
+
+    it_behaves_like "an observable mailer", "reference_requested"
+  end
+end
