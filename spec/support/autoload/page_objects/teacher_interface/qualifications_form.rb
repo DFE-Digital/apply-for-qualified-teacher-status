ee74b6a5f1a99a813992f0288f6432e6e1a7eafe
Steve Laing <steve.laing@gmail.com>
2023-01-12 14:02:38 +0000

    Add qualifications information via Support UI
    Adds the support UI fields to capture qualifications information.
    Also adds the presentation of the qualifications information for
    previewing in support and for the eligible screener step.

--- spec/support/autoload/page_objects/teacher_interface/qualifications_form.rb
+++ spec/support/autoload/page_objects/teacher_interface/qualifications_form.rb
@@ -2,7 +2,7 @@ module PageObjects
   module TeacherInterface
     class QualificationsForm < SitePrism::Page
       element :heading, "h1"
-      element :body, ".govuk-body-l"
+      elements :body, ".govuk-body-l"
 
       section :form, "form" do
         element :title, "#teacher-interface-qualification-form-title-field"
