4e6f2dda4c5a918a21738e4aa9ff6e98b8331c1e
Thomas Leese <thomas@leese.io>
2023-01-16 14:55:53 +0000

    Add other to EnglishLanguageProviderForm
    This makes it possible to select "other" as one of the provider IDs and
    is stored on the application form correctly.

--- spec/forms/teacher_interface/english_language_provider_form_spec.rb
+++ spec/forms/teacher_interface/english_language_provider_form_spec.rb
@@ -18,18 +18,43 @@ RSpec.describe TeacherInterface::EnglishLanguageProviderForm, type: :model do
         providers.map(&:id).map(&:to_s),
       )
     end
+
+    context "when reduced evidence is accepted" do
+      before { application_form.update!(reduced_evidence_accepted: true) }
+
+      it do
+        is_expected.to validate_inclusion_of(:provider_id).in_array(
+          providers.map(&:id).map(&:to_s) + ["other"],
+        )
+      end
+    end
   end
 
   describe "#save" do
-    let(:provider_id) { providers.first.id.to_s }
-
     subject(:save) { form.save(validate: true) }
 
-    it "saves the application form" do
-      expect { save }.to change(
-        application_form,
-        :english_language_provider,
-      ).to(providers.first)
+    context "when a provider is selected" do
+      let(:provider_id) { providers.first.id.to_s }
+
+      it "saves the application form" do
+        expect { save }.to change(
+          application_form,
+          :english_language_provider,
+        ).to(providers.first)
+      end
+    end
+
+    context "when other is selected" do
+      before { application_form.update!(reduced_evidence_accepted: true) }
+
+      let(:provider_id) { "other" }
+
+      it "saves the application form" do
+        expect { save }.to change(
+          application_form,
+          :english_language_provider_other,
+        ).to(true)
+      end
     end
   end
 end
