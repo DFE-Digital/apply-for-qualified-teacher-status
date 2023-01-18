37c0ddcdc53ce34516ce6a1656b259c9849dff9d
Steve Laing <steve.laing@gmail.com>
2023-01-18 18:32:13 +0000

    Add forms for PI and Qualifications sections
    These need to share some common logic to update the English language proficiency section state if a checkbox is checked.

---
+++ app/forms/assessor_interface/updates_english_language_status.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+module AssessorInterface::UpdatesEnglishLanguageStatus
+  extend ActiveSupport::Concern
+
+  included do
+    attribute :english_language_section_passed, :boolean
+
+    def save
+      super && save_english_language_status
+    end
+
+    class << self
+      def initial_attributes(assessment_section)
+        assessment = assessment_section.assessment
+        application_form = assessment.application_form
+
+        super.merge(
+          english_language_section_passed:
+            (
+              assessment_section.passed &&
+                application_form.send(self::EXEMPTION_ATTR)
+            ),
+        )
+      end
+
+      def permittable_parameters
+        args, kwargs = super
+        [args, kwargs.merge(english_language_section_passed: [])]
+      end
+    end
+
+    private
+
+    def save_english_language_status
+      return true unless update_english_language_status?
+
+      UpdateAssessmentSection.call(
+        assessment_section: english_language_proficiency_section,
+        user:,
+        params: {
+          passed: english_language_section_passed,
+          selected_failure_reasons: {
+          },
+        },
+      )
+    end
+
+    def update_english_language_status?
+      english_language_section_passed &&
+        application_form.send(self.class::EXEMPTION_ATTR)
+    end
+
+    def english_language_proficiency_section
+      @english_language_proficiency_section ||=
+        AssessmentSection.find_by(
+          assessment: assessment_section.assessment,
+          key: "english_language_proficiency",
+        )
+    end
+
+    def application_form
+      assessment_section.assessment.application_form
+    end
+  end
+end
---
+++ spec/forms/assessor_interface/personal_information_form_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AssessorInterface::PersonalInformationForm, type: :model do
+  let(:assessment_section) do
+    create(:assessment_section, :personal_information)
+  end
+  let(:user) { create(:staff, :confirmed) }
+  let(:attributes) { {} }
+
+  subject(:form) do
+    described_class.for_assessment_section(assessment_section).new(
+      assessment_section:,
+      user:,
+      **attributes,
+    )
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:user) }
+    it do
+      is_expected.to allow_values(true, false).for(
+        :english_language_section_passed,
+      )
+    end
+  end
+
+  describe "#save" do
+    subject(:save) { form.save }
+
+    let(:assessment) { assessment_section.assessment }
+    let(:application_form) { assessment.application_form }
+
+    let!(:english_language_section) do
+      create(
+        :assessment_section,
+        :english_language_proficiency,
+        assessment: assessment_section.assessment,
+      )
+    end
+
+    describe "when invalid attributes" do
+      it { is_expected.to be false }
+    end
+
+    describe "with valid attributes" do
+      let(:attributes) do
+        { passed: true, english_language_section_passed: true }
+      end
+
+      it { is_expected.to be true }
+
+      it "sets the attributes" do
+        save # rubocop:disable Rails/SaveBang
+
+        expect(assessment_section.reload.passed).to be true
+      end
+
+      it "sets the passed attribute on the english language proficiency section" do
+        application_form.update!(english_language_citizenship_exempt: true)
+
+        save # rubocop:disable Rails/SaveBang
+
+        expect(english_language_section.reload.passed).to be true
+      end
+
+      it "ignores the passed attribute for the ELP section if exemption is by qualifications" do
+        application_form.update!(english_language_qualification_exempt: true)
+
+        save # rubocop:disable Rails/SaveBang
+
+        expect(english_language_section.reload.passed).to be_nil
+      end
+    end
+  end
+end
