ec2ac81057a50a32cc06b026055bba17094464a7
Thomas Leese <thomas@leese.io>
2023-01-23 11:30:34 +0000

    Remove data migration from structure migration
    This migration has run in all environments now so we don't need to
    change the data as the database will be empty, and it's causing problems
    when the state field is renamed.

--- db/migrate/20230119115001_add_potential_duplicate_to_dqt_trn_request.rb
+++ db/migrate/20230119115001_add_potential_duplicate_to_dqt_trn_request.rb
@@ -1,15 +1,5 @@
 class AddPotentialDuplicateToDQTTRNRequest < ActiveRecord::Migration[7.0]
-  def up
+  def change
     add_column :dqt_trn_requests, :potential_duplicate, :boolean
-
-    DQTTRNRequest.complete.update_all(potential_duplicate: false)
-
-    ApplicationForm.potential_duplicate_in_dqt.each do |application_form|
-      application_form.dqt_trn_request.update!(potential_duplicate: true)
-    end
-  end
-
-  def down
-    remove_column :dqt_trn_requests, :potential_duplicate
   end
 end
