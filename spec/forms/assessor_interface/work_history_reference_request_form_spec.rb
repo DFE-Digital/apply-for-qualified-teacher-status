2c9b68978cc75c938ff30f07df1e30cbec734f97
Thomas Leese <thomas@leese.io>
2023-01-30 19:16:00 +0000

    Add WorkHistoryReferenceRequestForm
    This adds a form which handles the choosing of work history reference
    requests after recommending that the candidate is awarded QTS.

---
+++ spec/forms/assessor_interface/work_history_reference_request_form_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AssessorInterface::WorkHistoryReferenceRequestForm,
+               type: :model do
+  let(:application_form) { create(:application_form, :submitted) }
+  let(:work_history_1) { create(:work_history, :completed, application_form:) }
+  let(:work_history_2) { create(:work_history, :completed, application_form:) }
+  let(:session) { {} }
+  let(:work_history_ids) { "" }
+
+  subject(:form) do
+    described_class.new(application_form:, session:, work_history_ids:)
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:application_form) }
+    it { is_expected.to_not allow_values(nil).for(:session) }
+    it do
+      is_expected.to validate_inclusion_of(:work_history_ids).in_array(
+        [work_history_1.id.to_s, work_history_2.id.to_s],
+      )
+    end
+
+    context "with not enough hours" do
+      it { is_expected.to_not be_valid }
+    end
+
+    context "with enough hours" do
+      let(:work_history_ids) { [work_history_1.id.to_s] }
+      it { is_expected.to be_valid }
+    end
+  end
+
+  describe "#save" do
+    subject(:save) { form.save }
+
+    let(:work_history_ids) { [work_history_1.id.to_s, work_history_2.id.to_s] }
+
+    it { is_expected.to be true }
+
+    it "changes the session" do
+      expect { save }.to change { session[:work_history_ids] }.to(
+        [work_history_1.id.to_s, work_history_2.id.to_s],
+      )
+    end
+  end
+end
