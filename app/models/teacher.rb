7848d968cbaafd9fe96d462317ae06d94bc224cd
Thomas Leese <thomas@leese.io>
2023-01-30 08:36:30 +0000

    Remove application not submitted emails
    We've sent these emails to all the teachers with draft applications
    now so we don't need to keep the email and we can remove all the
    related code.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -32,15 +32,6 @@ class Teacher < ApplicationRecord
 
   has_many :application_forms
 
-  scope :with_draft_application_forms,
-        -> {
-          joins(:application_forms).where(
-            application_forms: {
-              status: "draft",
-            },
-          )
-        }
-
   def application_form
     @application_form ||= application_forms.order(created_at: :desc).first
   end
