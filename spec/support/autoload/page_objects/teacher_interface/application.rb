da0becee6fab49705b4759a52278ee1eebd67780
Thomas Leese <thomas@leese.io>
2023-02-08 11:35:59 +0000

    Add submitting system test
    This adds a system test for the submitting section of the
    teacher interface to reduce the size of the application test
    which covers everything.

--- spec/support/autoload/page_objects/teacher_interface/application.rb
+++ spec/support/autoload/page_objects/teacher_interface/application.rb
@@ -7,8 +7,8 @@ module PageObjects
       element :content_title, "app-task-list_section"
       element :app_task_list, ".app-task-list"
 
-      element :check_answers, ".govuk-button:nth-of-type(1)"
-      element :save_and_sign_out, ".govuk-button:nth-of-type(2)"
+      element :check_answers, ".govuk-button:not(.govuk-button--secondary)"
+      element :save_and_sign_out, ".govuk-button.govuk-button--secondary"
 
       section :task_list, TaskList, ".app-task-list"
 
