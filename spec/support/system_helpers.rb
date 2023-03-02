db0eef49bd10fce6cf89ba31a0a47f49ddc598d5
Steve Laing <steve.laing@gmail.com>
2023-03-02 13:36:59 +0000

    Allow support users to enable preliminary checks
    by country or region.

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -18,6 +18,7 @@ module SystemHelpers
     country = create(:country, :with_national_region, code: "GB-SCT")
     country.regions.first.update!(
       qualifications_information: "Qualifications information",
+      requires_preliminary_check: true,
       status_check: country_check,
       sanction_check: country_check,
       teaching_authority_status_information: "Status information",
