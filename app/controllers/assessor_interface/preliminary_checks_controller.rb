5ca06b77f6ddc98fb58be9d34e5be61d69456238
Steve Laing <steve.laing@gmail.com>
2023-03-06 17:00:39 +0000

    Add preliminary check actions

---
+++ app/controllers/assessor_interface/preliminary_checks_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module AssessorInterface
+  class PreliminaryChecksController < BaseController
+    before_action :authorize_assessor
+
+    def edit
+      @form = PreliminaryCheckForm.new(assessment: application_form.assessment)
+    end
+
+    def update
+      @form =
+        PreliminaryCheckForm.new(
+          form_params.merge(assessment: application_form.assessment),
+        )
+
+      if @form.save
+        redirect_to assessor_interface_application_form_path(application_form)
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def form_params
+      params.require(:assessor_interface_preliminary_check_form).permit(
+        :preliminary_check_complete,
+      )
+    end
+
+    def application_form
+      @application_form ||=
+        ApplicationForm.includes(assessment: :sections).find(
+          params[:application_form_id],
+        )
+    end
+  end
+end
