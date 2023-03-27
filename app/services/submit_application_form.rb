df191002a69b140bb9889843194e69b7ad05dc03
Thomas Leese <thomas@leese.io>
2023-03-27 11:16:41 +0100

    Update requires preliminary check when submitting
    We want this value to be updated when submitting to ensure that it's
    correct if the option is changed before the application is submitted.

--- app/services/submit_application_form.rb
+++ app/services/submit_application_form.rb
@@ -13,8 +13,10 @@ class SubmitApplicationForm
 
     ActiveRecord::Base.transaction do
       application_form.subjects.compact_blank!
-      application_form.submitted_at = Time.zone.now
       application_form.working_days_since_submission = 0
+      application_form.requires_preliminary_check =
+        region.requires_preliminary_check
+      application_form.submitted_at = Time.zone.now
 
       assessment = AssessmentFactory.call(application_form:)
 
