e8d4c1e04ef7bd7c3ada914083c19ff886c901f4
Thomas Leese <thomas@leese.io>
2023-04-19 15:47:12 +0200

    Add AKS Postgres module
    This deploys a Postgres instance to Azure or AKS that we can use in the app.

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -1,8 +1,12 @@
+locals {
+  environment = "${var.app_environment}${var.app_suffix}"
+}
+
 module "redis" {
   source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/redis?ref=testing"
 
   namespace             = var.namespace
-  environment           = "${var.app_environment}${var.app_suffix}"
+  environment           = local.environment
   azure_resource_prefix = var.azure_resource_prefix
   service_short         = var.service_short
   config_short          = var.config_short
@@ -11,4 +15,20 @@ module "redis" {
 
   use_azure               = var.deploy_azure_backing_services
   azure_enable_monitoring = var.enable_monitoring
+}
+
+module "postgres" {
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/postgres?ref=testing"
+
+  namespace             = var.namespace
+  environment           = local.environment
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short
+  config_short          = var.config_short
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = var.deploy_azure_backing_services
+  azure_enable_monitoring = var.enable_monitoring
+  azure_extensions        = ["pg_stat_statements"]
 }
