37ee6b63ba6520abc9e2b71b47947d57458db4a6
Thomas Leese <thomas@leese.io>
2023-04-19 15:58:58 +0200

    Add AKS Application Configuration module
    This adds the application configuration necessary to deploy
    applications.

---
+++ terraform/aks/application.tf
@@ -0,0 +1,17 @@
+locals {
+  environment = "${var.app_environment}${var.app_suffix}"
+}
+
+module "application_configuration" {
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/application_configuration?ref=aks-application-configuration"
+
+  namespace             = var.namespace
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short
+  config_short          = var.config_short
+
+  secret_variables = {
+    DATABASE_URL = module.postgres.url
+    REDIS_URL    = module.redis.url
+  }
+}
