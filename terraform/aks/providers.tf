43099d91f42a8b77d7fc4a2345c2e741785310d6
Thomas Leese <thomas@leese.io>
2023-04-21 17:39:10 +0200

    Add Kubernetes provider
    This adds the provider we need to use Kubernetes.

--- terraform/aks/providers.tf
+++ terraform/aks/providers.tf
@@ -11,3 +11,10 @@ provider "azurerm" {
 
   features {}
 }
+
+provider "kubernetes" {
+  host                   = module.cluster_data.kubernetes_host
+  client_certificate     = module.cluster_data.kubernetes_client_certificate
+  client_key             = module.cluster_data.kubernetes_client_key
+  cluster_ca_certificate = module.cluster_data.kubernetes_cluster_ca_certificate
+}
