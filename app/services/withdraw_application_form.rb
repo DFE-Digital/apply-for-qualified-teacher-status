c7006959f75e502e3dfa8a1d2c8ffd60cd10749e
Thomas Leese <thomas@leese.io>
2023-06-07 13:23:45 +0100

    Add WithdrawApplicationForm
    This is a service which performs the necessary steps required to
    withdrawn an application.

---
+++ app/services/withdraw_application_form.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class WithdrawApplicationForm
+  include ServicePattern
+
+  def initialize(application_form:, user:)
+    @application_form = application_form
+    @user = user
+  end
+
+  def call
+    return if application_form.withdrawn?
+
+    ActiveRecord::Base.transaction do
+      application_form.update!(withdrawn_at: Time.zone.now)
+      ApplicationFormStatusUpdater.call(application_form:, user:)
+    end
+  end
+
+  private
+
+  attr_reader :application_form, :user
+end
