b0c52e1e9f17ae94bbba99d3e5db9b4dcb48629e
Thomas Leese <thomas@leese.io>
2023-06-18 20:14:56 +0100

    Refactor variable names
    This removes the unnecessary `assessment_section` prefix from the
    variables in the AssessmentSectionsController as it can only be related
    to assessment sections.

--- app/controllers/assessor_interface/assessment_sections_controller.rb
+++ app/controllers/assessor_interface/assessment_sections_controller.rb
@@ -5,17 +5,15 @@ module AssessorInterface
     before_action :authorize_assessor
 
     def show
-      @assessment_section_form =
-        assessment_section_form.new(
+      @form =
+        form.new(
           user: current_staff,
-          **assessment_section_form_class.initial_attributes(
-            assessment_section,
-          ),
+          **form_class.initial_attributes(assessment_section),
         )
     end
 
     def update
-      unless assessment_section_view_object.render_form?
+      unless view_object.render_form?
         redirect_to [
                       :assessor_interface,
                       application_form,
@@ -25,15 +23,10 @@ module AssessorInterface
         return
       end
 
-      @assessment_section_form =
-        assessment_section_form.new(
-          assessment_section_form_params.merge(
-            assessment_section:,
-            user: current_staff,
-          ),
-        )
+      @form =
+        form.new(form_params.merge(assessment_section:, user: current_staff))
 
-      if @assessment_section_form.save
+      if @form.save
         redirect_to [:assessor_interface, application_form]
       else
         render :show, status: :unprocessable_entity
@@ -42,16 +35,15 @@ module AssessorInterface
 
     private
 
-    def assessment_section_view_object
-      @assessment_section_view_object ||=
-        AssessmentSectionViewObject.new(params:)
+    def view_object
+      @view_object ||= AssessmentSectionViewObject.new(params:)
     end
 
-    def assessment_section_form
-      assessment_section_form_class.for_assessment_section(assessment_section)
+    def form
+      form_class.for_assessment_section(assessment_section)
     end
 
-    def assessment_section_form_class
+    def form_class
       if assessment_section.age_range_subjects?
         CheckAgeRangeSubjectsForm
       elsif assessment_section.professional_standing? &&
@@ -69,8 +61,8 @@ module AssessorInterface
       end
     end
 
-    def assessment_section_form_params
-      assessment_section_form.permit_parameters(
+    def form_params
+      form.permit_parameters(
         params.require(:assessor_interface_assessment_section_form),
       )
     end
@@ -78,6 +70,6 @@ module AssessorInterface
     delegate :assessment_section,
              :assessment,
              :application_form,
-             to: :assessment_section_view_object
+             to: :view_object
   end
 end
