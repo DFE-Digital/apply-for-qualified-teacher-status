77e06d43f2028f3559f9f32bf769de6dd5828f40
Thomas Leese <thomas@leese.io>
2023-06-19 13:44:50 +0100

    Remove unnecessary required_providers
    The version constraints are set by the modules themselves, we should
    leave it up to those modules to decide on which version is best unless
    we're using them directly in this project.

--- terraform/aks/terraform.tf
+++ terraform/aks/terraform.tf
@@ -4,19 +4,8 @@ terraform {
   backend "azurerm" {}
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.53.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.20.0"
-    }
-
     statuscake = {
-      source  = "StatusCakeDev/statuscake"
-      version = "2.1.0"
+      source = "StatusCakeDev/statuscake"
     }
   }
 }
