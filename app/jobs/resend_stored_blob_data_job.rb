fcd98cbcd3557012b858390113a8de2d19db354a
Thomas Leese <thomas@leese.io>
2023-06-21 14:23:32 +0100

    Revert "Remove tasks jobs and services for resending blob data"
    This reverts commit 02148817c58b5fb7dd4aeaeb78ae966da41aa532.
    
    Sometimes it seems like the virus scanner gets stuck and we end up with
    a number of pending uploads, these tasks will make it possible to resend
    them so they get scanned again.

---
+++ app/jobs/resend_stored_blob_data_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ResendStoredBlobDataJob < ApplicationJob
+  # Resending the blob data will trigger a malware scan.
+  def perform(batch_size: 1000)
+    return unless FeatureFlags::FeatureFlag.active?(:fetch_malware_scan_result)
+
+    Upload
+      .where(malware_scan_result: "pending")
+      .limit(batch_size)
+      .find_each do |upload|
+        sleep(2) # Avoid rate limiting
+        ResendStoredBlobData.call(upload:)
+      end
+  end
+end
