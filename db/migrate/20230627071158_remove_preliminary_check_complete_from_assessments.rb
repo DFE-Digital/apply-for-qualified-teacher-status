d5c0f13fdfdda7918404e281f6f1853347f2b3b6
Thomas Leese <thomas@leese.io>
2023-06-27 08:27:07 +0100

    Remove preliminary_check_complete column
    We've replaced this column with dedicated preliminary assessment
    sections and we're no longer using it for anything.

---
+++ db/migrate/20230627071158_remove_preliminary_check_complete_from_assessments.rb
@@ -0,0 +1,7 @@
+class RemovePreliminaryCheckCompleteFromAssessments < ActiveRecord::Migration[
+  7.0
+]
+  def change
+    remove_column :assessments, :preliminary_check_complete, :boolean
+  end
+end
