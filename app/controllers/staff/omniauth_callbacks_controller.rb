a7b81bf29010dc09f973711a32ce94cdec6c53ac
Richard Pattinson <richie.pattinson@googlemail.com>
2023-06-29 12:32:41 +0100

    Change controller to automatically confirm staff via azure logins. Lookup staff by find_by_email method to handle capitalization

--- app/controllers/staff/omniauth_callbacks_controller.rb
+++ app/controllers/staff/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Staff::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     email = auth["info"]["email"]
     azure_ad_uid = auth["uid"]
 
-    @staff = Staff.find_by(email:)
+    @staff = Staff.find_by_email(email)
 
     if @staff&.update(azure_ad_uid:)
       @staff.confirm unless @staff.confirmed?
@@ -16,5 +16,11 @@ class Staff::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def failure
     redirect_to new_staff_session_url, alert: t(".failure")
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || assessor_interface_root_path
   end
 end
