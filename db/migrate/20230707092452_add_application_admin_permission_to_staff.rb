048256f683f2fbe63df08a69f9ec6227bc9c7530
Thomas Leese <thomas@leese.io>
2023-07-07 10:28:57 +0100

    Add manage_applications_permission column
    This adds a new column to the staff table which records whether the
    staff user has permission to perform administrative tasks on
    applications.

---
+++ db/migrate/20230707092452_add_application_admin_permission_to_staff.rb
@@ -0,0 +1,9 @@
+class AddApplicationAdminPermissionToStaff < ActiveRecord::Migration[7.0]
+  def change
+    add_column :staff,
+               :manage_applications_permission,
+               :boolean,
+               default: false,
+               null: false
+  end
+end
--- spec/factories/staff.rb
+++ spec/factories/staff.rb
@@ -2,38 +2,39 @@
 #
 # Table name: staff
 #
-#  id                         :bigint           not null, primary key
-#  award_decline_permission   :boolean          default(FALSE)
-#  azure_ad_uid               :string
-#  confirmation_sent_at       :datetime
-#  confirmation_token         :string
-#  confirmed_at               :datetime
-#  current_sign_in_at         :datetime
-#  current_sign_in_ip         :string
-#  email                      :string           default(""), not null
-#  encrypted_password         :string           default(""), not null
-#  failed_attempts            :integer          default(0), not null
-#  invitation_accepted_at     :datetime
-#  invitation_created_at      :datetime
-#  invitation_limit           :integer
-#  invitation_sent_at         :datetime
-#  invitation_token           :string
-#  invitations_count          :integer          default(0)
-#  invited_by_type            :string
-#  last_sign_in_at            :datetime
-#  last_sign_in_ip            :string
-#  locked_at                  :datetime
-#  name                       :text             default(""), not null
-#  remember_created_at        :datetime
-#  reset_password_sent_at     :datetime
-#  reset_password_token       :string
-#  sign_in_count              :integer          default(0), not null
-#  support_console_permission :boolean          default(FALSE), not null
-#  unconfirmed_email          :string
-#  unlock_token               :string
-#  created_at                 :datetime         not null
-#  updated_at                 :datetime         not null
-#  invited_by_id              :bigint
+#  id                             :bigint           not null, primary key
+#  award_decline_permission       :boolean          default(FALSE)
+#  azure_ad_uid                   :string
+#  confirmation_sent_at           :datetime
+#  confirmation_token             :string
+#  confirmed_at                   :datetime
+#  current_sign_in_at             :datetime
+#  current_sign_in_ip             :string
+#  email                          :string           default(""), not null
+#  encrypted_password             :string           default(""), not null
+#  failed_attempts                :integer          default(0), not null
+#  invitation_accepted_at         :datetime
+#  invitation_created_at          :datetime
+#  invitation_limit               :integer
+#  invitation_sent_at             :datetime
+#  invitation_token               :string
+#  invitations_count              :integer          default(0)
+#  invited_by_type                :string
+#  last_sign_in_at                :datetime
+#  last_sign_in_ip                :string
+#  locked_at                      :datetime
+#  manage_applications_permission :boolean          default(FALSE), not null
+#  name                           :text             default(""), not null
+#  remember_created_at            :datetime
+#  reset_password_sent_at         :datetime
+#  reset_password_token           :string
+#  sign_in_count                  :integer          default(0), not null
+#  support_console_permission     :boolean          default(FALSE), not null
+#  unconfirmed_email              :string
+#  unlock_token                   :string
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#  invited_by_id                  :bigint
 #
 # Indexes
 #
@@ -53,6 +54,10 @@ FactoryBot.define do
 
     trait :confirmed do
       confirmed_at { Time.zone.now }
+    end
+
+    trait :with_manage_applications_permission do
+      manage_applications_permission { true }
     end
 
     trait :with_award_decline_permission do
