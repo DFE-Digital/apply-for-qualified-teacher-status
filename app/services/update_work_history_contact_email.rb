d807a2c7f28b640e733fadc8bd9d9954e2df0ff8
Thomas Leese <thomas@leese.io>
2023-07-12 11:48:51 +0100

    Add UpdateWorkHistoryContact service
    This adds a service allowing assessors to change the contact details of
    an item of work history. It also sends an email to referees if
    necessary, and records timeline events about the changed information.

--- app/services/update_work_history_contact_email.rb
+++ app/services/update_work_history_contact_email.rb
@@ -17,16 +17,11 @@ class UpdateWorkHistoryContactEmail
 
   def call
     work_histories.each do |work_history|
-      work_history.update!(
-        contact_email: new_email_address,
-        canonical_contact_email: EmailAddress.canonical(new_email_address),
+      UpdateWorkHistoryContact.call(
+        work_history:,
+        user:,
+        email: new_email_address,
       )
-
-      if (reference_request = work_history.reference_request)
-        RefereeMailer.with(reference_request:).reference_requested.deliver_later
-      end
-
-      create_timeline_event(work_history)
     end
   end
 
@@ -38,18 +33,6 @@ class UpdateWorkHistoryContactEmail
     application_form.work_histories.where(
       "LOWER(contact_email) = ?",
       old_email_address.downcase,
-    )
-  end
-
-  def create_timeline_event(work_history)
-    TimelineEvent.create!(
-      event_type: "information_changed",
-      application_form:,
-      creator: user,
-      work_history:,
-      column_name: "contact_email",
-      old_value: old_email_address,
-      new_value: new_email_address,
     )
   end
 end
---
+++ spec/services/update_work_history_contact_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UpdateWorkHistoryContact do
+  let(:work_history) { create(:work_history, :completed) }
+  let(:user) { create(:staff, :confirmed) }
+  let(:new_name) { "New name" }
+  let(:new_job) { "New job" }
+  let(:new_email) { "new@example.com" }
+
+  subject(:call) do
+    described_class.call(
+      work_history:,
+      user:,
+      name: new_name,
+      job: new_job,
+      email: new_email,
+    )
+  end
+
+  it "changes the contact name" do
+    expect { call }.to change(work_history, :contact_name).to(new_name)
+  end
+
+  it "changes the contact job" do
+    expect { call }.to change(work_history, :contact_job).to(new_job)
+  end
+
+  it "changes the contact email" do
+    expect { call }.to change(work_history, :contact_email).to(new_email)
+  end
+
+  it "changes the canonical contact email" do
+    expect { call }.to change(work_history, :canonical_contact_email).to(
+      new_email,
+    )
+  end
+
+  it "doesn't send any emails" do
+    expect { call }.to_not have_enqueued_mail(
+      RefereeMailer,
+      :reference_requested,
+    )
+  end
+
+  it "records timeline events" do
+    expect { call }.to have_recorded_timeline_event(
+      :information_changed,
+      creator: user,
+      work_history:,
+    )
+  end
+
+  describe "when references already sent out" do
+    before { create(:reference_request, work_history:) }
+
+    it "sends an email" do
+      expect { call }.to have_enqueued_mail(RefereeMailer, :reference_requested)
+    end
+  end
+end
