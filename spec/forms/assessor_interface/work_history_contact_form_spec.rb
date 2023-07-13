6b16846f4e57fb0770bbe386b238d71570a924a3
Thomas Leese <thomas@leese.io>
2023-07-13 11:08:08 +0100

    Add AssessorInterface::WorkHistoryContactForm
    This adds a new form which assessors will use to change the contact
    information of a referee.

---
+++ spec/forms/assessor_interface/work_history_contact_form_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AssessorInterface::WorkHistoryContactForm, type: :model do
+  let(:work_history) { create(:work_history) }
+  let(:user) { create(:staff) }
+
+  let(:name) { "A new name" }
+  let(:job) { "A new job" }
+  let(:email) { "new@example.com" }
+
+  subject(:form) do
+    described_class.new(work_history:, user:, name:, job:, email:)
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:work_history) }
+    it { is_expected.to validate_presence_of(:user) }
+  end
+
+  describe "#save" do
+    subject(:save) { form.save }
+
+    it "calls the update work history contact service" do
+      expect(UpdateWorkHistoryContact).to receive(:call).with(
+        work_history:,
+        user:,
+        name:,
+        job:,
+        email:,
+      )
+      expect(save).to be true
+    end
+  end
+end
