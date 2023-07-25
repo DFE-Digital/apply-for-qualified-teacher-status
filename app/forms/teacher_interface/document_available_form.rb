039b30ebbb657bff49d51078bc8c8092d759c80d
Thomas Leese <thomas@leese.io>
2023-07-25 12:23:16 +0100

    Add DocumentAvailableForm
    This adds a form which teachers will use to tell us if they've got a
    document available or not. If they do they'll be asked to upload it, and
    if not then they don't have to. This only applies to optional documents.

---
+++ app/forms/teacher_interface/document_available_form.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class TeacherInterface::DocumentAvailableForm < TeacherInterface::BaseForm
+  attr_accessor :document
+  attribute :available, :boolean
+
+  validates :document, presence: true
+  validates :available, inclusion: { in: [true, false] }
+
+  def update_model
+    document.update!(
+      available:,
+      completed: available ? !document.uploads.empty? : true,
+    )
+  end
+
+  delegate :application_form, to: :document
+end
