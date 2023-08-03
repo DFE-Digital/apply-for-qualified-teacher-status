2f44cff06ce5d3750b52062a17422cb492784454
Thomas Leese <thomas@leese.io>
2023-08-03 11:41:51 +0200

    Skip sign in page
    When authenticating via Active Directory we can skip the sign in page
    step and instead redirect users directly to the Azure sign in page,
    saving an unnecessary step.

--- app/controllers/assessor_interface/base_controller.rb
+++ app/controllers/assessor_interface/base_controller.rb
@@ -2,10 +2,10 @@
 
 class AssessorInterface::BaseController < ApplicationController
   include AssessorCurrentNamespace
+  include StaffAuthenticatable
 
   layout "two_thirds"
 
-  before_action :authenticate_staff!
   after_action :verify_authorized
 
   def authorize_assessor
@@ -15,6 +15,4 @@ class AssessorInterface::BaseController < ApplicationController
   def authorize_note
     authorize :note
   end
-
-  alias_method :pundit_user, :current_staff
 end
