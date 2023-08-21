05d077d24c15b9b576717de8da959376fd8babe8
Thomas Leese <thomas@leese.io>
2023-08-21 14:50:32 +0200

    Rename manage_applications_permission
    Initially we thought this permission would get used across the board,
    but we now discovering that it's only used for one feature as different
    sets of users will need to use each feature.

--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -5,6 +5,7 @@
 #  id                             :bigint           not null, primary key
 #  award_decline_permission       :boolean          default(FALSE)
 #  azure_ad_uid                   :string
+#  change_work_history_permission :boolean          default(FALSE), not null
 #  confirmation_sent_at           :datetime
 #  confirmation_token             :string
 #  confirmed_at                   :datetime
@@ -23,7 +24,6 @@
 #  last_sign_in_at                :datetime
 #  last_sign_in_ip                :string
 #  locked_at                      :datetime
-#  manage_applications_permission :boolean          default(FALSE), not null
 #  name                           :text             default(""), not null
 #  remember_created_at            :datetime
 #  reset_password_sent_at         :datetime
