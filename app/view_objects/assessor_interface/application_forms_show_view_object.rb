64a50a1b91eec1a9303ddc49f7aaf6c1508f2747
Shujat Khalid <khalidshujat@gmail.com>
2023-10-02 09:52:33 +0100

    Moved further information into assessment and updated section names

--- app/view_objects/assessor_interface/application_forms_show_view_object.rb
+++ app/view_objects/assessor_interface/application_forms_show_view_object.rb
@@ -22,7 +22,6 @@ class AssessorInterface::ApplicationFormsShowViewObject
     [
       pre_assessment_task_list_section,
       initial_assessment_task_list_section,
-      further_information_task_list_section,
       verification_task_list_section,
     ].compact
   end
@@ -122,18 +121,21 @@ class AssessorInterface::ApplicationFormsShowViewObject
 
   def initial_assessment_task_list_section
     return unless pre_assessment_complete?
-
+  
     {
       title:
         I18n.t(
           "assessor_interface.application_forms.show.assessment_tasks.sections.initial_assessment",
         ),
-      items: [
-        *assessment_section_task_list_items(preliminary: false),
-        initial_assessment_recommendation_task_list_item,
-      ],
+      items: (
+        assessment_section_task_list_items(preliminary: false) +
+        [initial_assessment_recommendation_task_list_item] +
+        further_information_task_list_section[:items]
+      ),
     }
   end
+  
+  
 
   ASSESSMENT_SECTION_KEYS = %i[
     personal_information
