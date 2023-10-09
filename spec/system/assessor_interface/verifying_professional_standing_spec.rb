ae94bffdf0584592441a676ad44b07cce697d5ce
Thomas Leese <thomas@leese.io>
2023-10-09 12:26:34 +0100

    Update system tests for verifying LoPS
    This updates the system tests for verifying LoPS to use the newer verify
    page and updates to use the right permissions.

--- spec/system/assessor_interface/verifying_professional_standing_spec.rb
+++ spec/system/assessor_interface/verifying_professional_standing_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Assessor verifying professional standing", type: :system do
   before do
     given_the_service_is_open
-    given_i_am_authorized_as_an_assessor_user
+    given_i_am_authorized_as_an_admin_user
     given_there_is_an_application_form_with_professional_standing_request
   end
 
@@ -22,13 +22,13 @@ RSpec.describe "Assessor verifying professional standing", type: :system do
     then_i_see_the(:assessor_application_page, application_id:)
     and_i_see_a_received_status
 
-    when_i_click_review_professional_standing_task
+    when_i_click_verify_professional_standing_task
     then_i_see_the(
       :assessor_verify_professional_standing_request_page,
       application_form_id:,
     )
 
-    when_i_fill_in_the_review_form
+    when_i_fill_in_the_verify_form
     then_i_see_the(:assessor_application_page, application_id:)
   end
 
@@ -51,7 +51,7 @@ RSpec.describe "Assessor verifying professional standing", type: :system do
       .click
   end
 
-  def when_i_click_review_professional_standing_task
+  def when_i_click_verify_professional_standing_task
     assessor_application_page
       .review_professional_standing_request_task
       .link
@@ -66,7 +66,7 @@ RSpec.describe "Assessor verifying professional standing", type: :system do
     form.submit_button.click
   end
 
-  def when_i_fill_in_the_review_form
+  def when_i_fill_in_the_verify_form
     form = assessor_verify_professional_standing_request_page.form
 
     form.yes_radio_item.choose
