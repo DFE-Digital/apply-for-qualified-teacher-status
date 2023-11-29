822ffcadf636bc3199efc6948ab5c028e274bab3
Thomas Leese <thomas@leese.io>
2023-11-29 14:01:41 +0000

    Add ReferenceRequestPolicy
    This adds a new policy for verifying reference requests.

---
+++ spec/policies/assessor_interface/reference_request_policy_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AssessorInterface::ReferenceRequestPolicy do
+  it_behaves_like "a policy"
+
+  let(:user) { nil }
+  let(:record) { nil }
+
+  subject(:policy) { described_class.new(user, record) }
+
+  describe "#index?" do
+    subject(:index?) { policy.index? }
+    it_behaves_like "a policy method requiring the assess permission"
+  end
+
+  describe "#update_verify_references?" do
+    subject(:index?) { policy.update_verify_references? }
+    it_behaves_like "a policy method requiring the assess permission"
+  end
+
+  describe "#show?" do
+    subject(:show?) { policy.show? }
+    it_behaves_like "a policy method without permission"
+  end
+
+  describe "#create?" do
+    subject(:create?) { policy.create? }
+    it_behaves_like "a policy method without permission"
+  end
+
+  describe "#new?" do
+    subject(:new?) { policy.new? }
+    it_behaves_like "a policy method without permission"
+  end
+
+  describe "#update?" do
+    subject(:update?) { policy.update? }
+    it_behaves_like "a policy method requiring the assess permission"
+  end
+
+  describe "#edit?" do
+    subject(:edit?) { policy.edit? }
+    it_behaves_like "a policy method requiring the assess permission"
+    it_behaves_like "a policy method requiring the change work history permission"
+  end
+
+  describe "#destroy?" do
+    subject(:destroy?) { policy.destroy? }
+    it_behaves_like "a policy method without permission"
+  end
+end
