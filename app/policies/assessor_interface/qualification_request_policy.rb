63ee09b82b88239ada6bfceda61d51093cec4995
Thomas Leese <thomas@leese.io>
2023-12-07 09:34:00 +0000

    Add QualificationRequestPolicy
    This refactors how we handle permissions for qualification requests in
    preparation for the new verification journey.

---
+++ app/policies/assessor_interface/qualification_request_policy.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AssessorInterface::QualificationRequestPolicy < ApplicationPolicy
+  def index?
+    user.assess_permission
+  end
+
+  def update?
+    user.assess_permission
+  end
+end
