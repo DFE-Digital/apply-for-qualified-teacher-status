723ced46e8c8be85aa959a60f91c97998bee9388
Thomas Leese <thomas@leese.io>
2023-12-22 11:18:28 +0000

    Simplify migration to add rules
    This has already been run in production so we don't need it to be any
    more complicated, and it's currently causing problems with deployment of
    review apps.

--- db/migrate/20220908064656_add_rules_to_application_forms.rb
+++ db/migrate/20220908064656_add_rules_to_application_forms.rb
@@ -1,28 +1,9 @@
 class AddRulesToApplicationForms < ActiveRecord::Migration[7.0]
   def change
     change_table :application_forms, bulk: true do |t|
-      t.boolean :needs_work_history
-      t.boolean :needs_written_statement
-      t.boolean :needs_registration_number
+      t.boolean :needs_work_history, null: false
+      t.boolean :needs_written_statement, null: false
+      t.boolean :needs_registration_number, null: false
     end
-
-    ApplicationForm
-      .includes(:region)
-      .find_each do |application_form|
-        region = application_form.region
-
-        application_form.update!(
-          needs_work_history:
-            region.status_check_none? || region.sanction_check_none?,
-          needs_written_statement:
-            region.status_check_online? || region.sanction_check_online?,
-          needs_registration_number:
-            region.status_check_written? || region.sanction_check_written?,
-        )
-      end
-
-    change_column_null :application_forms, :needs_work_history, false
-    change_column_null :application_forms, :needs_written_statement, false
-    change_column_null :application_forms, :needs_registration_number, false
   end
 end
