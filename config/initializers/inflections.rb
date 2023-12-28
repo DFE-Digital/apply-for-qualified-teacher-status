c64310b3a54b8d2db2571b85a090b8d66b1e600e
Thomas Leese <thomas@leese.io>
2023-12-28 17:08:33 +0000

    Add ConvertToPDF
    This adds a service which handles the conversion of an upload to a PDF.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym "DfE"
   inflect.acronym "DQT"
+  inflect.acronym "DfE"
+  inflect.acronym "PDF"
   inflect.acronym "QTS"
   inflect.acronym "TRN"
   inflect.uncountable %w[staff]
