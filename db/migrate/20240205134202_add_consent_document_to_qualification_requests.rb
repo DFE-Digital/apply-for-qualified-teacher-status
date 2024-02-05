5d6d36efcff35168066bb1ef7224d03eaa4f6297
Thomas Leese <thomas@leese.io>
2024-02-05 13:44:28 +0000

    Add unsigned_consent_document_downloaded column
    This adds a new column to qualification requests for capturing whether
    an applicant has downloaded the unsigned consent document.

---
+++ db/migrate/20240205134202_add_consent_document_to_qualification_requests.rb
@@ -0,0 +1,9 @@
+class AddConsentDocumentToQualificationRequests < ActiveRecord::Migration[7.1]
+  def change
+    add_column :qualification_requests,
+               :unsigned_consent_document_downloaded,
+               :boolean,
+               default: false,
+               null: false
+  end
+end
--- spec/models/qualification_request_spec.rb
+++ spec/models/qualification_request_spec.rb
@@ -4,21 +4,22 @@
 #
 # Table name: qualification_requests
 #
-#  id               :bigint           not null, primary key
-#  expired_at       :datetime
-#  location_note    :text             default(""), not null
-#  received_at      :datetime
-#  requested_at     :datetime
-#  review_note      :string           default(""), not null
-#  review_passed    :boolean
-#  reviewed_at      :datetime
-#  verified_at      :datetime
-#  verify_note      :text             default(""), not null
-#  verify_passed    :boolean
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  assessment_id    :bigint           not null
-#  qualification_id :bigint           not null
+#  id                                   :bigint           not null, primary key
+#  expired_at                           :datetime
+#  location_note                        :text             default(""), not null
+#  received_at                          :datetime
+#  requested_at                         :datetime
+#  review_note                          :string           default(""), not null
+#  review_passed                        :boolean
+#  reviewed_at                          :datetime
+#  unsigned_consent_document_downloaded :boolean          default(FALSE), not null
+#  verified_at                          :datetime
+#  verify_note                          :text             default(""), not null
+#  verify_passed                        :boolean
+#  created_at                           :datetime         not null
+#  updated_at                           :datetime         not null
+#  assessment_id                        :bigint           not null
+#  qualification_id                     :bigint           not null
 #
 # Indexes
 #
