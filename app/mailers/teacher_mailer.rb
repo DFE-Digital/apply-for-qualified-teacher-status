913bbcb738d078efce53e3a1316e4b21ffba7040
Thomas Leese <thomas@leese.io>
2024-02-13 09:44:50 +0000

    Add consent requested email
    This adds an email which is sent to applicants when consent has been
    requested from them.

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -56,6 +56,17 @@ class TeacherMailer < ApplicationMailer
     )
   end
 
+  def consent_requested
+    @expires_at =
+      assessment.qualification_requests.consent_required.map(&:expires_at).max
+
+    view_mail(
+      GOVUK_NOTIFY_TEMPLATE_ID,
+      to: teacher.email,
+      subject: I18n.t("mailer.teacher.consent_requested.subject"),
+    )
+  end
+
   def further_information_received
     view_mail(
       GOVUK_NOTIFY_TEMPLATE_ID,
