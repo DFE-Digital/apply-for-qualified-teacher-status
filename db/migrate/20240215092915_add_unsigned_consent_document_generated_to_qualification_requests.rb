ffbd583a2fcf9f64e71952f55ad41fe72e1d63af
Thomas Leese <thomas@leese.io>
2024-02-15 10:30:48 +0100

    Add unsigned_consent_document_generated column
    This adds a column which records when the assessor has generated the
    unsigned consent document.

---
+++ db/migrate/20240215092915_add_unsigned_consent_document_generated_to_qualification_requests.rb
@@ -0,0 +1,11 @@
+class AddUnsignedConsentDocumentGeneratedToQualificationRequests < ActiveRecord::Migration[
+  7.1
+]
+  def change
+    add_column :qualification_requests,
+               :unsigned_consent_document_generated,
+               :boolean,
+               default: false,
+               null: false
+  end
+end
