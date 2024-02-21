2a84157ff9a1679c96f1e3123790725ba157a512
Thomas Leese <thomas@leese.io>
2024-02-21 12:15:20 +0100

    Rename UnsignedConsentDocumentForm
    To GenerateUnsignedConsentDocumentForm to make it clear that it's
    related to generating the document as opposed to uploading one.

--- app/forms/assessor_interface/generate_unsigned_consent_document_form.rb
+++ app/forms/assessor_interface/generate_unsigned_consent_document_form.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AssessorInterface::UnsignedConsentDocumentForm
+class AssessorInterface::GenerateUnsignedConsentDocumentForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
