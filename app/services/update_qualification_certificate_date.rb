983ed862fa8e14e417751b1fbdbb15098f448fdb
Thomas Leese <thomas@leese.io>
2024-04-25 08:55:49 +0100

    Add UpdateQualificationCertificateDate
    This adds a service that makes it possible to change the qualification
    certificate date of an application after it has been submitted.

---
+++ app/services/update_qualification_certificate_date.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class UpdateQualificationCertificateDate
+  include ServicePattern
+
+  def initialize(qualification:, user:, certificate_date:)
+    @qualification = qualification
+    @user = user
+    @certificate_date = certificate_date
+  end
+
+  def call
+    old_certificate_date = qualification.certificate_date
+
+    ActiveRecord::Base.transaction do
+      qualification.update!(certificate_date:)
+      create_timeline_event(old_certificate_date:)
+    end
+  end
+
+  private
+
+  attr_reader :qualification, :user, :certificate_date
+
+  delegate :application_form, to: :qualification
+
+  def create_timeline_event(old_certificate_date:)
+    CreateTimelineEvent.call(
+      "information_changed",
+      application_form:,
+      user:,
+      qualification:,
+      column_name: "certificate_date",
+      old_value: old_certificate_date.strftime("%B %Y").strip,
+      new_value: certificate_date.strftime("%B %Y").strip,
+    )
+  end
+end
