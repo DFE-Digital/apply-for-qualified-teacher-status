a5e9ce1c88eb751f649b3d515ba6be8e7ece587d
Thomas Leese <thomas@leese.io>
2024-05-18 13:04:13 +0100

    Ignore Rails/NotNullColumn
    This migration has already been run so we don't need to worry about it
    running when there is no data.

--- db/migrate/20220908064656_add_rules_to_application_forms.rb
+++ db/migrate/20220908064656_add_rules_to_application_forms.rb
@@ -1,9 +1,11 @@
 class AddRulesToApplicationForms < ActiveRecord::Migration[7.0]
   def change
     change_table :application_forms, bulk: true do |t|
+      # rubocop:disable Rails/NotNullColumn
       t.boolean :needs_work_history, null: false
       t.boolean :needs_written_statement, null: false
       t.boolean :needs_registration_number, null: false
+      # rubocop:enable Rails/NotNullColumn
     end
   end
 end
