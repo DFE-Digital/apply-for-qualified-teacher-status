74758c7b76f56f7339099d1bcf32a8770a59d349
Shujat Khalid <khalidshujat@gmail.com>
2024-05-31 15:03:01 +0100

    removed redundant link and amended tests

--- spec/system/teacher_interface/authentication_spec.rb
+++ spec/system/teacher_interface/authentication_spec.rb
@@ -182,7 +182,9 @@ RSpec.describe "Teacher authentication", type: :system do
 
   def when_i_sign_in_using_magic_link
     message = ActionMailer::Base.deliveries.last
-    uri = URI.parse(message.body.raw_source.lines.fifth.chomp)
+    link_line = message.body.raw_source.lines.fifth.chomp
+    url = link_line.match(/\((http[^)]+)\)/)[1]
+    uri = URI.parse(url)
     expect(uri.path).to eq("/teacher/magic_link")
     expect(uri.query).to include("token")
     visit "#{uri.path}?#{uri.query}"
