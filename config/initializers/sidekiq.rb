8d6d1816c244c90c7669f29d7f690b85dca7bf9b
Thomas Leese <thomas@leese.io>
2024-07-03 14:22:16 +0100

    Revert "Set Sidekiq log level to warning"
    This reverts commit a8b0889b51eb1970e553398dd7477176229a641c.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,6 @@
 
 url = ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: }
-  config.logger.level = Logger::WARN
-end
+Sidekiq.configure_server { |config| config.redis = { url: } }
 
 Sidekiq.configure_client { |config| config.redis = { url: } }
