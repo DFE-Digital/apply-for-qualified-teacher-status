9de9079cb306e37fbebf4e70880f1fa5e081bca0
Thomas Leese <thomas@leese.io>
2024-07-17 14:47:58 +0100

    Add ability to edit suitability records
    This adds the ability to edit existing suitability records by filling in
    a similar form to the one when suitability records are created.

---
+++ spec/support/autoload/page_objects/assessor_interface/suitability_record_form_page.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module AssessorInterface
+    class SuitabilityRecordFormPage < SitePrism::Page
+      section :form, "form" do
+        element :name_field,
+                "#assessor-interface-suitability-record-form-name-field"
+        element :alias_field,
+                "#assessor-interface-suitability-record-form-aliases-field"
+        element :location_field,
+                "#assessor-interface-suitability-record-form-location-field"
+        element :date_of_birth_day_field,
+                "#assessor_interface_suitability_record_form_date_of_birth_3i"
+        element :date_of_birth_month_field,
+                "#assessor_interface_suitability_record_form_date_of_birth_2i"
+        element :date_of_birth_year_field,
+                "#assessor_interface_suitability_record_form_date_of_birth_1i"
+        element :email_field,
+                "#assessor-interface-suitability-record-form-emails-field"
+        element :reference_field,
+                "#assessor-interface-suitability-record-form-references-field"
+        element :note_field,
+                "#assessor-interface-suitability-record-form-note-field"
+        element :submit_button, ".govuk-button:not(.govuk-button--secondary)"
+      end
+    end
+  end
+end
