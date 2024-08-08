6a0ab775a87cc8ce59ce98db973eebb8219348c1
Shujat Khalid <khalidshujat@gmail.com>
2024-08-08 14:39:23 +0100

    Able to pull through email info through header

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,4 +8,23 @@ class ApplicationMailer < Mail::Notify::Mailer
       "GOVUK_NOTIFY_TEMPLATE_ID_APPLICATION",
       "95adafaf-0920-4623-bddc-340853c047af",
     )
+
+    rescue_from Notifications::Client::RequestError do
+      # WARNING: this needs to be a block, otherwise the exception will not be
+      # re-raised and we will not be notified via Sentry, and the job will not retry.
+      #
+      # @see https://github.com/rails/rails/issues/39018
+      if respond_to?(:headers)
+        binding.break
+        email = Email.find(headers['email-log-id'].to_s)
+        email.update!(delivery_status: 'notify_error')
+      end
+  
+      raise
+    end
+
+    def notify_email(headers)
+      headers = headers.merge(rails_mailer: mailer_name, rails_mail_template: action_name)
+      view_mail(GOVUK_NOTIFY_TEMPLATE_ID, headers)
+    end
 end
