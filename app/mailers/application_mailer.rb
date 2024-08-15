55f0780d8c75b25e2fee45ad21d88d5fb5cfc276
Shujat Khalid <khalidshujat@gmail.com>
2024-08-15 15:25:55 +0100

    linting fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -9,23 +9,22 @@ class ApplicationMailer < Mail::Notify::Mailer
       "95adafaf-0920-4623-bddc-340853c047af",
     )
 
-    rescue_from Notifications::Client::RequestError do
-      # WARNING: this needs to be a block, otherwise the exception will not be
-      # re-raised and we will not be notified via Sentry, and the job will not retry.
-      #
-      # @see https://github.com/rails/rails/issues/39018
-      if respond_to?(:headers)
-        recipient_email = headers['To'].value
-        MailDeliveryFailure.create!(
-          recipient_email: recipient_email
-        )
-      end
-  
-      raise
+  rescue_from Notifications::Client::RequestError do
+    # WARNING: this needs to be a block, otherwise the exception will not be
+    # re-raised and we will not be notified via Sentry, and the job will not retry.
+    #
+    # @see https://github.com/rails/rails/issues/39018
+    if respond_to?(:headers)
+      recipient_email = headers["To"].value
+      MailDeliveryFailure.create!(recipient_email:)
     end
 
-    def notify_email(headers)
-      headers = headers.merge(rails_mailer: mailer_name, rails_mail_template: action_name)
-      view_mail(GOVUK_NOTIFY_TEMPLATE_ID, headers)
-    end
+    raise
+  end
+
+  def notify_email(headers)
+    headers =
+      headers.merge(rails_mailer: mailer_name, rails_mail_template: action_name)
+    view_mail(GOVUK_NOTIFY_TEMPLATE_ID, headers)
+  end
 end
