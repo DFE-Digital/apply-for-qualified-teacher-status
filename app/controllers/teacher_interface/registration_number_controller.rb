351944b060f499f851d9f08fae7b2598b8b5ce8c
Thomas Leese <thomas@leese.io>
2024-08-23 19:43:50 +0100

    Add separate registration number view for Ghana
    We need a separate view as the interface will be different.

--- app/controllers/teacher_interface/registration_number_controller.rb
+++ app/controllers/teacher_interface/registration_number_controller.rb
@@ -14,21 +14,34 @@ module TeacherInterface
           application_form:,
           registration_number: application_form.registration_number,
         )
+
+      render view_name
     end
 
     def update
       @form = RegistrationNumberForm.new(form_params.merge(application_form:))
 
-      handle_application_form_section(form: @form)
+      handle_application_form_section(
+        form: @form,
+        if_failure_then_render: view_name,
+      )
     end
 
     private
+
+    def ghana?
+      CountryCode.ghana?(application_form.country.code)
+    end
 
     def form_params
       params.require(:teacher_interface_registration_number_form).permit(
         :registration_number,
         license_number_parts: [],
       )
+    end
+
+    def view_name
+      ghana? ? :edit_ghana : :edit
     end
   end
 end
