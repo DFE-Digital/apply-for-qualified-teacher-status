91d655a9014aafb6559a8c27334ae7004ffca5c1
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-10-14 21:37:30 +0100

    wip

---
+++ terraform/application/dfe_analytics.tf
@@ -0,0 +1,24 @@
+#   "gcp_project": "apply-for-qts-in-england",
+#   "gcp_project_number": "385922361840",
+#   "gcp_taxonomy_id": 5456044749211275650,
+#   "gcp_policy_tag_id": 2399328962407973209,
+#   "gcp_table_deletion_protection": false
+
+
+module dfe_analytics {
+  source = "./vendor/modules/dfe-terraform-modules//aks/dfe_analytics"
+
+  azure_resource_prefix = var.azure_resource_prefix
+  cluster = var.cluster
+  namespace = var.namespace
+  service_short = var.service_short
+  environment = "${var.app_environment}${var.app_suffix}"
+  gcp_dataset = "events_${var.app_environment}"
+  gcp_keyring = "afqts-key-ring"
+  gcp_key = "afqts-key"
+  gcp_project_id = "apply-for-qts-in-england"
+  gcp_project_number = "385922361840"
+  gcp_taxonomy_id = 5456044749211275650
+  gcp_policy_tag_id = 2399328962407973209
+  gcp_table_deletion_protection = false
+}
