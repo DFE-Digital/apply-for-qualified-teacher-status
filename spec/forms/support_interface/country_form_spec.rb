219a93d6696200d750651da886d5fa656ab3d3d0
Shujat Khalid <khalidshujat@gmail.com>
2024-11-01 14:30:09 +0000

    Add validation tests to spec file

--- spec/forms/support_interface/country_form_spec.rb
+++ spec/forms/support_interface/country_form_spec.rb
@@ -3,5 +3,19 @@
 require "rails_helper"
 
 RSpec.describe SupportInterface::CountryForm, type: :model do
+  describe "#valid?" do
+    subject(:form) do
+      described_class.new(
+        eligibility_enabled: true,
+        eligibility_route: "standard",
+        has_regions: true,
+        region_names: "Madrid",
+      )
+    end
 
+    it { is_expected.to validate_inclusion_of(:eligibility_enabled).in_array([true, false]) }
+    it { is_expected.to validate_inclusion_of(:eligibility_route).in_array(%w[expanded reduced standard]) }
+    it { is_expected.to validate_inclusion_of(:has_regions).in_array([true, false]) }
+    it { is_expected.to validate_presence_of(:region_names) }
+  end
 end
