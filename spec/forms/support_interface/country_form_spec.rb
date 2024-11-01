68b809bb95f07e1f3888fa7a61121fb8ddee7805
Shujat Khalid <khalidshujat@gmail.com>
2024-11-01 20:17:53 +0000

    Added with-message to resolve customer validation issue

--- spec/forms/support_interface/country_form_spec.rb
+++ spec/forms/support_interface/country_form_spec.rb
@@ -4,8 +4,6 @@ require "rails_helper"
 
 RSpec.describe SupportInterface::CountryForm, type: :model do
   describe "#valid?" do
-    let(:country) { create(:country) }
-
     subject(:form) do
       described_class.new(
         country:,
@@ -16,7 +14,15 @@ RSpec.describe SupportInterface::CountryForm, type: :model do
       )
     end
 
-    it { is_expected.to validate_presence_of(:eligibility_enabled) }
+    let(:country) { create(:country) }
+
+    it do
+      expect(subject).to validate_presence_of(
+        :eligibility_enabled,
+      ).with_message(
+        "You must select whether or not we accept applications from this country",
+      )
+    end
 
     it do
       expect(subject).to validate_inclusion_of(:eligibility_route).in_array(
@@ -24,7 +30,11 @@ RSpec.describe SupportInterface::CountryForm, type: :model do
       )
     end
 
-    it { is_expected.to validate_presence_of(:has_regions) }
+    it do
+      expect(subject).to validate_presence_of(:has_regions).with_message(
+        "You must select whether or not there are regions within in this country that have a different teaching authority or service journey",
+      )
+    end
 
     it { is_expected.to validate_presence_of(:region_names) }
 
