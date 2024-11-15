ef3044b9052a47f63ef96a948e1d35f16322b3d0
Shujat Khalid <khalidshujat@gmail.com>
2024-11-15 15:31:10 +0000

    all_sections_necessary validation tests

--- spec/forms/support_interface/region_form_spec.rb
+++ spec/forms/support_interface/region_form_spec.rb
@@ -5,14 +5,19 @@ require "rails_helper"
 RSpec.describe SupportInterface::RegionForm, type: :model do
   describe "#valid?" do
     subject(:valid) { form.valid? }
-
+    
+    let(:teaching_authority_requires_submission_email) { true }
+    let(:teaching_authority_emails_string) {"test@test.com, test2@test.com"}
+    let(:work_history_section_to_omit) { nil }
+    let(:all_sections_necessary) { true }
     let(:region) { create(:region) }
+    let(:save!) { form.save! }
     let(:form) do
       described_class.new(
         teaching_authority_emails_string:,
         region:,
         teaching_authority_requires_submission_email:,
-        all_sections_necessary: true,
+        all_sections_necessary:,
         requires_preliminary_check: false,
         sanction_check: "none",
         status_check: "none",
@@ -21,8 +26,29 @@ RSpec.describe SupportInterface::RegionForm, type: :model do
       )
     end
 
+    context "when applicants need to complete all sections" do
+
+      context "when yes is selected" do
+        it do
+          is_expected.to be_truthy
+        end
+      end
+    end
+
+    context "when applicants do not need to complete all sections" do
+
+      context "when no is selected" do
+        let(:all_sections_necessary) { false }
+        let(:work_history_section_to_omit) { "whole_section" }
+
+        it do 
+          save!
+          expect(form).to validate_inclusion_of(:work_history_section_to_omit).in_array(%w[whole_section contact_details])
+        end
+      end
+    end
+
     context "when teaching authority requires submission email" do
-      let(:teaching_authority_requires_submission_email) { true }
 
       context "when teaching authority emails string is blank" do
         let(:teaching_authority_emails_string) { "" }
@@ -38,9 +64,6 @@ RSpec.describe SupportInterface::RegionForm, type: :model do
       end
 
       context "when teaching authority emails string is not blank" do
-        let(:teaching_authority_emails_string) do
-          "test@test.com, test2@test.com"
-        end
 
         it { is_expected.to be_truthy }
       end
