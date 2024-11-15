7cfb7af1c93fe05dda2d834c8d8bfcc91161e2f3
Shujat Khalid <khalidshujat@gmail.com>
2024-11-15 16:06:12 +0000

    just saving where I've got to before changing my approach

--- spec/forms/support_interface/region_form_spec.rb
+++ spec/forms/support_interface/region_form_spec.rb
@@ -6,23 +6,45 @@ RSpec.describe SupportInterface::RegionForm, type: :model do
   describe "#valid?" do
     subject(:valid) { form.valid? }
     
+    let(:teaching_qualification_information) { "teaching_qualification_information" }
+    let(:sanction_information) { "sanction_information" }
+    let(:status_information) { "status_information" }
+    let(:teaching_authority_online_checker_url) { "teaching_authority_online_checker_url" }
+    let(:teaching_authority_certificate) { "teaching_authority_certificate" }
+    let(:other_information) { "other_information" }
+    let(:teaching_authority_name) { "test_name" }
+    let(:teaching_authority_address) { "test_address" }
+    let(:teaching_authority_websites_string) { "test.com" }
     let(:teaching_authority_requires_submission_email) { true }
     let(:teaching_authority_emails_string) {"test@test.com, test2@test.com"}
     let(:work_history_section_to_omit) { nil }
+    let(:sanction_check) { "none" }
+    let(:status_check) { "none" }
+    let(:teaching_authority_provides_written_statement) { false }
+    let(:written_statement_optional) { true }
     let(:all_sections_necessary) { true }
     let(:region) { create(:region) }
     let(:save!) { form.save! }
     let(:form) do
       described_class.new(
+        teaching_qualification_information:,
+        sanction_information:,
+        status_information:,
+        teaching_authority_online_checker_url:,
+        teaching_authority_certificate:,
+        other_information:,
+        teaching_authority_name:,
+        teaching_authority_address:,
+        teaching_authority_websites_string:,
         teaching_authority_emails_string:,
         region:,
         teaching_authority_requires_submission_email:,
         all_sections_necessary:,
         requires_preliminary_check: false,
-        sanction_check: "none",
-        status_check: "none",
-        teaching_authority_provides_written_statement: false,
-        written_statement_optional: true,
+        sanction_check:,
+        status_check:,
+        teaching_authority_provides_written_statement:,
+        written_statement_optional:,
       )
     end
 
@@ -40,8 +62,8 @@ RSpec.describe SupportInterface::RegionForm, type: :model do
       context "when no is selected" do
         let(:all_sections_necessary) { false }
         let(:work_history_section_to_omit) { "whole_section" }
-
-        it do 
+        
+        it do
           save!
           expect(form).to validate_inclusion_of(:work_history_section_to_omit).in_array(%w[whole_section contact_details])
         end
