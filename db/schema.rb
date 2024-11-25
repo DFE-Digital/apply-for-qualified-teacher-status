5b0c6e09d9ea15194d6ec9b8d54bd4489cb1fbe7
Hassan Mir <hassanmir92@gmail.com>
2024-11-25 15:24:35 +0000

    Adding new address and website columns to work_histories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_152315) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_25_151731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -609,6 +609,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_152315) do
     t.string "contact_job", default: "", null: false
     t.text "canonical_contact_email", default: "", null: false
     t.text "contact_email_domain", default: "", null: false
+    t.string "address_line1"
+    t.string "address_line2"
+    t.string "school_website"
     t.index ["application_form_id"], name: "index_work_histories_on_application_form_id"
     t.index ["canonical_contact_email"], name: "index_work_histories_on_canonical_contact_email"
   end
