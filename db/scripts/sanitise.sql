a7631049caced12c972bfdb7edd9965ed708671e
Hassan Mir <hassanmir92@gmail.com>
2024-12-03 13:12:24 +0000

    Fix sanitise script

--- db/scripts/sanitise.sql
+++ db/scripts/sanitise.sql
@@ -126,6 +126,6 @@ SET
   address_line2 = CASE WHEN address_line2 IS NULL THEN NULL ELSE '[sanitised]' END,
   city  = '[sanitised]',
   job  = '[sanitised]',
-  school_website  = CASE WHEN address_line1 IS NULL THEN NULL ELSE '[sanitised]' END,
+  school_website  = CASE WHEN school_website IS NULL THEN NULL ELSE '[sanitised]' END,
   contact_email  = 'sanitised@example.com',
   contact_name  = '[sanitised]';
