69f2587faf8f4092424cd82a1852cb83e741243a
Hassan Mir <hassanmir92@gmail.com>
2024-12-09 15:01:13 +0000

    New V3 endpoint client to make a POST request for TRN generation

---
+++ spec/lib/trs/client/v3/create_trn_request_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TRS::Client::V3::CreateTRNRequest do
+  subject(:call) { described_class.call(request_id:, application_form:) }
+
+  let(:request_id) { "request-id" }
+  let(:application_form) { create(:application_form) }
+
+  before do
+    allow(TRS::V3::TRNRequestParams).to receive(:call).with(
+      request_id:,
+      application_form:,
+    ).and_return("body")
+  end
+
+  context "with a successful response" do
+    before do
+      stub_request(
+        :post,
+        "https://test-teacher-qualifications-api.education.gov.uk/v3/trn-requests",
+      ).with(
+        body: "body",
+        headers: {
+          "X-Api-Version" => "Next",
+          "Authorization" => "Bearer test-api-key",
+          "Content-Type" => "application/json",
+        },
+      ).to_return(
+        status: 200,
+        body:
+          '{"requestId":"72888c5d-db14-4222-829b-7db9c2ec0dc3","status":"Completed",' \
+            '"trn":"1234567"}',
+        headers: {
+          "Content-Type" => "application/json",
+        },
+      )
+    end
+
+    it do
+      expect(subject).to eq(
+        {
+          request_id: "72888c5d-db14-4222-829b-7db9c2ec0dc3",
+          status: "Completed",
+          trn: "1234567",
+        },
+      )
+    end
+  end
+
+  context "with a failure response" do
+    before do
+      stub_request(
+        :post,
+        "https://test-teacher-qualifications-api.education.gov.uk/v3/trn-requests",
+      ).with(
+        body: "body",
+        headers: {
+          "X-Api-Version" => "Next",
+          "Authorization" => "Bearer test-api-key",
+          "Content-Type" => "application/json",
+        },
+      ).to_return(
+        status: 400,
+        body: nil,
+        headers: {
+          "Content-Type" => "application/json",
+        },
+      )
+    end
+
+    it "raises an error" do
+      expect { call }.to raise_error(Faraday::BadRequestError)
+    end
+  end
+end
