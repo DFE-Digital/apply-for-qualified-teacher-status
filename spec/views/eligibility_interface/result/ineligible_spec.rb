fbb3f02a0526c2f0e71ca114d4c3d619058823cd
Shujat Khalid <khalidshujat@gmail.com>
2024-12-11 15:12:03 +0000

    added different adaptation content for before and after 1st Jan 2025

--- spec/views/eligibility_interface/result/ineligible_spec.rb
+++ spec/views/eligibility_interface/result/ineligible_spec.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "active_support/testing/time_helpers"
 
 RSpec.describe "eligibility_interface/result/ineligible.html.erb",
                type: :view do
+  include ActiveSupport::Testing::TimeHelpers
   subject { render }
 
   let(:region) { create(:region) }
@@ -13,7 +15,7 @@ RSpec.describe "eligibility_interface/result/ineligible.html.erb",
 
   it do
     expect(subject).to match(
-      /You’re not eligible to apply for qualified teacher status/,
+      "You’re not eligible to apply for qualified teacher status",
     )
   end
 
@@ -22,18 +24,33 @@ RSpec.describe "eligibility_interface/result/ineligible.html.erb",
       create(:eligibility_check, region:, work_experience: "under_9_months")
     end
 
-    it "displays work experience ineligible reason" do
+    it do
       expect(subject).to match(
         "To apply for QTS, you’ll need at least 9 months of teaching work " \
           "experience. This can be gained in any country but must be from after you qualified as a teacher.",
       )
     end
 
-    it "displays adaptation experience message" do
-      expect(subject).to match(
-        "If you’re a citizen of Iceland, Norway, or Liechtenstein you can gain " \
-          "more teaching experience during an adaptation period.",
-      )
+    context "when the date is before 2025" do
+      it do
+        travel_to Date.new(2024, 12, 30) do
+          expect(subject).to match(
+            "If you’re a citizen of Iceland, Norway, or Liechtenstein you can gain " \
+              "more teaching experience during an adaptation period.",
+          )
+        end
+      end
+    end
+
+    context "when the date is 2025 or later" do
+      it do
+        travel_to Date.new(2025, 1, 1) do
+          expect(subject).to match(
+            "If you’re a citizen of Iceland, Liechtenstein, Norway, or Switzerland " \
+              "you can gain more teaching experience during an adaptation period.",
+          )
+        end
+      end
     end
   end
 end
