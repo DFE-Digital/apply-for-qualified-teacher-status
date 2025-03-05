06c0ac724d46519420e3d8f84c2a50435b4ca945
Shujat Khalid <khalidshujat@gmail.com>
2025-03-05 14:12:33 +0000

    fix: resolve issues with passport expiry date and suitability specs

--- spec/support/autoload/page_objects/teacher_interface/passport_expiry_date.rb
+++ spec/support/autoload/page_objects/teacher_interface/passport_expiry_date.rb
@@ -9,7 +9,7 @@ module PageObjects
 
       section :form, "form" do
         element :passport_country_of_issue_code,
-                'input[name="passport_country_of_issue_autocomplete"]'
+                "#teacher-interface-passport-expiry-date-form-passport-country-of-issue-code-field"
         element :passport_expiry_date_day_field,
                 "#teacher_interface_passport_expiry_date_form_passport_expiry_date_3i"
         element :passport_expiry_date_month_field,
