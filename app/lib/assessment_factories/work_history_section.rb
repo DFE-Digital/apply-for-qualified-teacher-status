e1be62695f1da925ba8c9e413cf11a4f2c7d18d9
Hassan Mir <hassanmir92@gmail.com>
2025-03-05 11:54:00 +0000

    Assessment factory refactor
    This allows us to separate the logic for each of the sections into their own class. This way we can utilise these methods independently for future use when running migrations

---
+++ app/lib/assessment_factories/work_history_section.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module AssessmentFactories
+  class WorkHistorySection
+    def initialize(application_form:)
+      @application_form = application_form
+    end
+
+    def checks
+      %i[verify_school_details work_history_references]
+    end
+
+    def failure_reasons
+      reasons = [
+        FailureReasons::WORK_HISTORY_BREAK,
+        FailureReasons::SCHOOL_DETAILS_CANNOT_BE_VERIFIED,
+        FailureReasons::UNRECOGNISED_REFERENCES,
+        FailureReasons::WORK_HISTORY_DURATION,
+      ]
+
+      if suitability_active?
+        reasons += [
+          FailureReasons::SUITABILITY,
+          FailureReasons::SUITABILITY_PREVIOUSLY_DECLINED,
+          FailureReasons::FRAUD,
+        ]
+      end
+
+      reasons
+    end
+
+    private
+
+    attr_reader :application_form
+
+    def suitability_active?
+      @suitability_active ||= FeatureFlags::FeatureFlag.active?(:suitability)
+    end
+  end
+end
