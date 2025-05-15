a5e2cba529135591d88e4cb92b9553801b6ee787
Shujat Khalid <khalidshujat@gmail.com>
2025-05-15 23:49:00 +0100

    Fix linting

--- spec/helpers/format_time_helper_spec.rb
+++ spec/helpers/format_time_helper_spec.rb
@@ -5,16 +5,16 @@ require "rails_helper"
 RSpec.describe FormatTimeHelper do
   describe "#format_human_readable_uk_time" do
     subject(:formatted_time) { helper.format_human_readable_uk_time(time) }
-    
+
     context "when time is nil" do
       let(:time) { nil }
-      
+
       it { is_expected.to eq("") }
     end
 
     context "during British Summer Time" do
       let(:time) { Time.utc(2025, 5, 15, 8, 37, 39) }
-      
+
       it "formats time with BST adjustment" do
         travel_to(Time.zone.local(2025, 6, 1, 12, 0, 0)) do
           expect(formatted_time).to eq("15 May 2025 at 9:37 am")
@@ -24,7 +24,7 @@ RSpec.describe FormatTimeHelper do
 
     context "during Greenwich Mean Time" do
       let(:time) { Time.utc(2025, 1, 15, 14, 30, 0) }
-      
+
       it "formats time with GMT" do
         travel_to(Time.zone.local(2025, 12, 1, 12, 0, 0)) do
           expect(formatted_time).to eq("15 January 2025 at 2:30 pm")
