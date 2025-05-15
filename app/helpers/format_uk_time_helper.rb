f54a89cabbbd7f78b3e7a106d55cae5a618bea0c
Shujat Khalid <khalidshujat@gmail.com>
2025-05-15 12:26:11 +0100

    Futher changes to time formating to match designs

--- app/helpers/format_uk_time_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module FormatUkTimeHelper
-  def format_uk_time(time)
-    return "" if time.nil?
-    time.in_time_zone("London").strftime("%Y-%m-%d %H:%M:%S")
-  end
-end
