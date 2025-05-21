48662281036b02d8280ce596b29178a791f67406
RMcVelia <rick.mcvelia@gmail.com>
2025-05-21 13:30:54 +0100

    Add ingress rate limit

--- terraform/domains/environment_domains/variables.tf
+++ terraform/domains/environment_domains/variables.tf
@@ -35,6 +35,19 @@ variable "origin_hostname" {
   description = "Origin endpoint url"
 }
 
+variable "rate_limit" {
+  type = list(object({
+    agent        = optional(string)
+    priority     = optional(number)
+    duration     = optional(number)
+    limit        = optional(number)
+    selector     = optional(string)
+    operator     = optional(string)
+    match_values = optional(string)
+  }))
+  default = null
+}
+
 locals {
   hostname = "${var.domains[0]}.${var.zone}"
 }
