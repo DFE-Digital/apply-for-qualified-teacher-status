7727f5de845d3380870a18a87a752adb0e3ccfe4
Hassan Mir <hassanmir92@gmail.com>
2025-09-18 09:54:01 +0100

    Introduce validation for public email domain for FI responses to update referee details

--- app/forms/teacher_interface/further_information_request_item_work_history_contact_form.rb
+++ app/forms/teacher_interface/further_information_request_item_work_history_contact_form.rb
@@ -11,12 +11,30 @@ module TeacherInterface
     validates :contact_name, :contact_job, presence: true
     validates :contact_email, presence: true, valid_for_notify: true
 
+    validate :contact_email_has_private_email_domain
+
     def update_model
       further_information_request_item.update(
         contact_name:,
         contact_job:,
         contact_email:,
       )
+    end
+
+    delegate :application_form, to: :further_information_request_item
+
+    private
+
+    def parsed_contact_email
+      EmailAddress.new(contact_email)
+    end
+
+    def contact_email_has_private_email_domain
+      return unless application_form.requires_private_email_for_referee?
+
+      if EmailDomain.public?(parsed_contact_email.host_name)
+        errors.add(:contact_email, :invalid_email_domain)
+      end
     end
   end
 end
