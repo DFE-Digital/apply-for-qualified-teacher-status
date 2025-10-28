aabcf127f35430ad0bbf4f5eea30c741b1f14d17
Shujat Khalid <khalidshujat@gmail.com>
2025-10-28 15:33:25 +0000

    sentry test lint

--- app/controllers/test_sentry_controller.rb
+++ app/controllers/test_sentry_controller.rb
@@ -2,6 +2,6 @@
 
 class TestSentryController < ApplicationController
   def error
-    raise 'This is a test error for Sentry integration'
+    raise "This is a test error for Sentry integration"
   end
 end
