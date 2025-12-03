90457904a13ee00fc5a2bcd1c2b3dda5aa1642cd
Hassan Mir <hassanmir92@gmail.com>
2025-12-03 14:09:41 +0000

    Add custom length validators for string and text related fields

---
+++ app/validators/string_length_validator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class StringLengthValidator < ActiveModel::EachValidator
+  MAX_LENGTH = 1000
+
+  def validate_each(record, attribute, value)
+    return if value.nil?
+
+    validate_value_does_not_exceed_max_length(record, attribute, value)
+  end
+
+  def validate_value_does_not_exceed_max_length(record, attribute, value)
+    if value.length > MAX_LENGTH
+      record.errors.add attribute, :exceeds_string_max_length
+    end
+  end
+end
