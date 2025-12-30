5247e745c0328f04580138ae9cffca65926e9e23
Hassan Mir <hassanmir92@gmail.com>
2025-12-30 13:22:57 +0000

    New table/model for support requests by users

---
+++ app/models/support_request.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: support_requests
+#
+#  id                        :bigint           not null, primary key
+#  application_reference     :string
+#  comment                   :text
+#  email                     :string
+#  enquiry_type              :string
+#  has_application_reference :boolean
+#  name                      :string
+#  submitted_at              :datetime
+#  user_type                 :string
+#  zendesk_ticket_created_at :datetime
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#  zendesk_ticket_id         :string
+#
+class SupportRequest < ApplicationRecord
+end
