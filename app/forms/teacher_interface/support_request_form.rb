3e7f20179c9a2b65613ec0055eddc4eae07c0858
Hassan Mir <hassanmir92@gmail.com>
2026-01-16 15:26:22 +0000

    Add validation errors to support request form

--- app/forms/teacher_interface/support_request_form.rb
+++ app/forms/teacher_interface/support_request_form.rb
@@ -8,6 +8,12 @@ class TeacherInterface::SupportRequestForm < TeacherInterface::BaseForm
   attribute :application_enquiry_type, :string
   attribute :application_reference, :string
 
+  validates :name, :email, :category_type, :comment, presence: true
+  validates :application_reference,
+            :application_enquiry_type,
+            presence: true,
+            if: :application_submitted_category?
+
   def update_model
     SupportRequest.create!(
       email:,
@@ -19,5 +25,11 @@ class TeacherInterface::SupportRequestForm < TeacherInterface::BaseForm
     )
 
     Zendesk.create_ticket!(name:, email:, comment:)
+  end
+
+  private
+
+  def application_submitted_category?
+    category_type == "applicantion_submitted"
   end
 end
