d66d0d81faf8846f7856ca3039ec278fe6fff507
Shujat Khalid <khalidshujat@gmail.com>
2026-02-11 09:33:46 +0000

    Add custom error message

--- app/forms/eligibility_interface/qualification_form.rb
+++ app/forms/eligibility_interface/qualification_form.rb
@@ -7,8 +7,19 @@ class EligibilityInterface::QualificationForm
   attr_accessor :eligibility_check
   attribute :qualification, :boolean
 
+
   validates :eligibility_check, presence: true
-  validates :qualification, inclusion: { in: [true, false] }
+  validate :qualification_inclusion_with_country
+
+  def qualification_inclusion_with_country
+    unless [true, false].include?(qualification)
+      errors.add(
+        :qualification,
+        :inclusion,
+        country_name: CountryName.from_eligibility_check(eligibility_check)
+      )
+    end
+  end
 
   def save
     return false unless valid?
