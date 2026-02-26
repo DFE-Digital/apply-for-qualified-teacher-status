5b19095fd1aad4414b86573947e5a70c045163c6
Hassan Mir <hassanmir92@gmail.com>
2026-02-26 13:22:59 +0000

    Leaving a note for our temporary path to be removed once/if a fix on the mail-notify has been released

--- config/initializers/mail_notify_delivery_method_with_response.rb
+++ config/initializers/mail_notify_delivery_method_with_response.rb
@@ -5,6 +5,11 @@
 #
 # Notify returns a ResponseNotification.
 # https://www.rubydoc.info/gems/notifications-ruby-client/2.2.0
+#
+# TODO: We have an open PR https://github.com/dxw/mail-notify/pull/200
+# for the mail-notify gem to handle this. Once this has been released
+# we can remove this patch and re-test to ensure our funtionality for
+# GOV.UK Notify delivery status tracking still works.
 module CaptureNotifyResponsePatch
   def deliver!(message)
     response = super
