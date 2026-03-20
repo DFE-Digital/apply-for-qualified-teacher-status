28ed84a1abcb04204c779658ea92dbbe51a508cf
Hassan Mir <hassanmir92@gmail.com>
2026-03-20 11:47:17 +0000

    Use new stimulus controller for work history country input

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -24,12 +24,6 @@ initAll();
 var loadCountryAutoComplete = () => {
   var locationPicker =
     document.getElementById(
-      "teacher-interface-work-history-school-form-country-location-field"
-    ) ??
-    document.getElementById(
-      "teacher-interface-work-history-school-form-country-location-field-error"
-    ) ??
-    document.getElementById(
       "teacher-interface-other-england-work-history-school-form-country-location-field"
     ) ??
     document.getElementById(
