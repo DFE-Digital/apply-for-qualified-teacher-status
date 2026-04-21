9b3dbd80aa8fca921ea809a9ab28f4f6b5c09f5c
Shujat Khalid <khalidshujat@gmail.com>
2026-04-21 18:14:00 +0100

    Remove save method from private

--- app/forms/assessor_interface/application_form_date_of_birth_form.rb
+++ app/forms/assessor_interface/application_form_date_of_birth_form.rb
@@ -13,19 +13,6 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
   validates :date_of_birth, date: true
   validate :date_of_birth_valid
 
-  private
-
-  def date_of_birth_valid
-    date = DateValidator.parse(date_of_birth)
-    return if date.nil?
-
-    if date > 18.years.ago
-      errors.add(:date_of_birth, :too_young)
-    elsif date < 100.years.ago
-      errors.add(:date_of_birth, :too_old)
-    end
-  end
-
   def save
     return false if invalid?
 
@@ -38,5 +25,18 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
     )
 
     true
+  end
+
+  private
+
+  def date_of_birth_valid
+    date = DateValidator.parse(date_of_birth)
+    return if date.nil?
+
+    if date > 18.years.ago
+      errors.add(:date_of_birth, :too_young)
+    elsif date < 100.years.ago
+      errors.add(:date_of_birth, :too_old)
+    end
   end
 end
