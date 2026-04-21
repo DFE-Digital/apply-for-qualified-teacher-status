c572b07f4ba1ddbf69c333971967c08ad0fc68a1
Shujat Khalid <khalidshujat@gmail.com>
2026-04-21 17:46:18 +0100

    Make methods private

--- app/forms/assessor_interface/application_form_date_of_birth_form.rb
+++ app/forms/assessor_interface/application_form_date_of_birth_form.rb
@@ -4,6 +4,7 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
   include ActiveModel::Model
   include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
+  include SanitizeDates
 
   attr_accessor :application_form, :user
   attribute :date_of_birth
@@ -11,6 +12,8 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
   validates :application_form, :user, presence: true
   validates :date_of_birth, date: true
   validate :date_of_birth_valid
+
+  private
 
   def date_of_birth_valid
     date = DateValidator.parse(date_of_birth)
@@ -26,10 +29,12 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
   def save
     return false if invalid?
 
+    sanitize_dates!(date_of_birth)
+
     UpdateApplicationFormPersonalInformation.call(
       application_form: application_form,
       user: user,
-      date_of_birth: DateValidator.parse(date_of_birth),
+      date_of_birth: date_of_birth,
     )
 
     true
