d99eeaacf6ab3cd9810b67290d2c85783f3a9739
Shujat Khalid <khalidshujat@gmail.com>
2026-04-21 16:25:17 +0100

    Improve validation and error messages

--- app/forms/assessor_interface/application_form_date_of_birth_form.rb
+++ app/forms/assessor_interface/application_form_date_of_birth_form.rb
@@ -4,12 +4,25 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
   include ActiveModel::Model
   include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
+  include SanitizeDates
 
   attr_accessor :application_form, :user
-  attribute :date_of_birth, :date
+  attribute :date_of_birth
 
   validates :application_form, :user, presence: true
-  validates :date_of_birth, presence: true
+  validates :date_of_birth, date: true
+  validate :date_of_birth_valid
+
+  def date_of_birth_valid
+    date = DateValidator.parse(date_of_birth)
+    return if date.nil?
+
+    if date > 18.years.ago
+      errors.add(:date_of_birth, :too_young)
+    elsif date < 100.years.ago
+      errors.add(:date_of_birth, :too_old)
+    end
+  end
 
   def save
     return false if invalid?
@@ -17,7 +30,6 @@ class AssessorInterface::ApplicationFormDateOfBirthForm
     UpdateApplicationFormPersonalInformation.call(
       application_form: application_form,
       user: user,
-      date_of_birth: date_of_birth,
     )
 
     true
