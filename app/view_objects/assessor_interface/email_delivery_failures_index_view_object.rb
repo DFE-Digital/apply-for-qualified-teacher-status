80e588145dd1b3aa4bfb01ba400fdaae977cf1f4
Hassan Mir <hassanmir92@gmail.com>
2026-06-18 15:09:50 +0100

    Remove unnecessary module inclusion in view object

--- app/view_objects/assessor_interface/email_delivery_failures_index_view_object.rb
+++ app/view_objects/assessor_interface/email_delivery_failures_index_view_object.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class AssessorInterface::EmailDeliveryFailuresIndexViewObject
-  include ActionView::Helpers::FormOptionsHelper
   include Pagy::Backend
 
   def initialize(params:)
