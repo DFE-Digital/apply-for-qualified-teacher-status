6bcfdb619d3cd00bd4f5876ed5ed7d6738caa94c
Shujat Khalid <khalidshujat@gmail.com>
2026-07-07 15:44:44 +0100

    Remove the need to register mail delivery failures

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -11,25 +11,6 @@ class ApplicationMailer < Mail::Notify::Mailer
       "7f036b52-9e08-40c9-8b52-0bb527d70f4a",
     )
 
-  rescue_from Notifications::Client::RequestError do
-    # WARNING: this needs to be a block, otherwise the exception will not be
-    # re-raised and we will not be notified via Sentry, and the job will not retry.
-    #
-    # @see https://github.com/rails/rails/issues/39018
-    if respond_to?(:headers)
-      email_address = headers["To"].value
-      mailer_action_method = action_name
-      mailer_class = mailer_name
-      MailDeliveryFailure.create!(
-        email_address:,
-        mailer_action_method:,
-        mailer_class:,
-      )
-    end
-
-    raise
-  end
-
   private
 
   def enqueue_email_delivery_audit
