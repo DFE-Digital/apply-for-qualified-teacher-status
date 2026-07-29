2771efdcb64ef17e26c7286f19beccf67e1267f0
Hassan Mir <hassanmir92@gmail.com>
2026-07-29 16:08:24 +0100

    Specific job for clearing SolidQueue finished jobs to ensure it runs on SolidQueue adapter and not Sidekiq since the global config has not yet been updated

---
+++ app/jobs/clear_solid_queue_finished_jobs_job.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ClearSolidQueueFinishedJobsJob < SolidQueueApplicationJob
+  def perform
+    SolidQueue::Job.clear_finished_in_batches(sleep_between_batches: 0.3)
+  end
+end
