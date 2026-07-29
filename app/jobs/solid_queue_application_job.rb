4d051202b160dc882afeaedc3d26717803c5afc3
Hassan Mir <hassanmir92@gmail.com>
2026-07-29 14:29:10 +0100

    Adding queue_adapter on SolidQueueApplicationJob with comment around temporary use of this class

--- app/jobs/solid_queue_application_job.rb
+++ app/jobs/solid_queue_application_job.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+# TODO: Temporarily having a Solid Queue specific ApplicationJob
+# Once we have fully moved all of our jobs to Solid Queue,
+# this can be renamed to ApplicationJob with the self.queue_adapter removed
+# since it will be set in production.rb
 class SolidQueueApplicationJob < ApplicationJob
+  self.queue_adapter = :solid_queue
+
   # Automatically retry jobs that encountered an error
   retry_on StandardError, wait: :polynomially_longer, attempts: 20
 
