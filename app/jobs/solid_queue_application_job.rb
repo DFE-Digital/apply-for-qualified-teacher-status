54144664a22a878ccaf152b161f6fc5fc9d4bddf
Hassan Mir <hassanmir92@gmail.com>
2026-07-30 15:28:51 +0100

    Reducing retry attempts for SolidQueue errors

--- app/jobs/solid_queue_application_job.rb
+++ app/jobs/solid_queue_application_job.rb
@@ -8,7 +8,11 @@ class SolidQueueApplicationJob < ApplicationJob
   self.queue_adapter = :solid_queue unless Rails.env.test?
 
   # Automatically retry jobs that encountered an error
-  retry_on StandardError, wait: :polynomially_longer, attempts: 20
+  # Note that `retry_on` rescues the exception to reschedule the job,
+  # which stops it going into Sentry until all attempts are exhausted.
+  # The job then sits in the "Failed jobs" list and will need manual action.
+  # 10 attempts equates to ~4 hours in total.
+  retry_on StandardError, wait: :polynomially_longer, attempts: 10
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
