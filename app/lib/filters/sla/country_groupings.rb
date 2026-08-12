32dd0938c68cc442927124ecb21ebb6af6a8471c
Hassan Mir <hassanmir92@gmail.com>
2026-08-12 16:03:02 +0100

    Implement the country grouping filter

--- app/lib/filters/sla/country_groupings.rb
+++ app/lib/filters/sla/country_groupings.rb
@@ -2,7 +2,24 @@
 
 class Filters::SLA::CountryGroupings < Filters::Base
   def apply
-    scope
+    return scope if country_groupings.nil?
+
+    codes = []
+
+    codes +=
+      Country::CODES_IN_UK +
+        [Country::GIBRALTAR_CODE] if country_groupings.include?(
+      "uk_and_gibraltar",
+    )
+    codes += Country::CODES_IN_EU if country_groupings.include?("eu")
+    codes += Country::CODES_IN_EFTA if country_groupings.include?("efta")
+    codes += Country::CODES_IN_REST_OF_WORLD if country_groupings.include?(
+      "rest_of_world",
+    )
+
+    return scope if codes.empty?
+
+    scope.joins(region: :country).where(regions: { countries: { code: codes } })
   end
 
   private
