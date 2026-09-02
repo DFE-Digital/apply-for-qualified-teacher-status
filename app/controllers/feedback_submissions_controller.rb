6ef31b5c5537dae313c7d73fff4cc5677aa1badc
Shujat Khalid <khalidshujat@gmail.com>
2026-09-02 01:32:41 +0100

    Add feedback submission page flow

--- app/controllers/feedback_submissions_controller.rb
+++ app/controllers/feedback_submissions_controller.rb
@@ -2,8 +2,33 @@
 
 class FeedbackSubmissionsController < ApplicationController
   def new
+    @form = FeedbackSubmissionForm.new
+  end
+
+  def create
+    @form = FeedbackSubmissionForm.new(form_params)
+
+    if @form.save
+      redirect_to confirmation_feedback_submissions_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def confirmation
+  end
+
+  private
+
+  def form_params
+    params.require(:feedback_submission_form).permit(
+      :application_status,
+      :overall_experience,
+      :comment,
+    )
+  end
+
+  def current_namespace
+    current_teacher ? "teacher" : "eligibility"
   end
 end
