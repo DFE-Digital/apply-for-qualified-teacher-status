a506dacdaa2f1c089d4c7c166ea5de1e1b9c3638
Philip Hope <philip.hope@mobilise.cloud>
2026-09-11 17:04:42 +0100

    2923 - review app config stripped out and test enabled

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -14,12 +14,11 @@ DfE::Analytics.configure do |config|
       ENV.fetch("BIGQUERY_DISABLE", disabled_by_default.to_s) != "true"
     end
 
-  config.enable_airbyte =
-    proc do
-      disabled_by_default = Rails.env.development?
-      ENV.fetch("BIGQUERY_DISABLE", disabled_by_default.to_s) != "true"
-    end
-  config.airbyte_stream_config_path = "terraform/aks/workspace-variables/airbyte_stream_config.json"
+  if %w[test].include?(HostingEnvironment)
+    config.airbyte_stream_config_path = "terraform/aks/workspace-variables/airbyte_stream_config.json"
+    # Perform airbyte checks on startup and allow airbyte config generation
+    config.airbyte_enabled = ENV["BIGQUERY_AIRBYTE_DATASET"].present?
+  end
 
-  config.azure_federated_auth = ENV.include? "GOOGLE_CLOUD_CREDENTIALS"
+  config.azure_federated_auth = ENV.include?("GOOGLE_CLOUD_CREDENTIALS")
 end
